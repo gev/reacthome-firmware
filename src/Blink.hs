e8791ed2fb0f4396f17240fb897378ede9adbf20
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-11-17 16:33:20 +0300

    GPIO module

--- src/Blink.hs
+++ src/Blink.hs
@@ -1,16 +1,27 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators     #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TypeOperators      #-}
 
 module Blink (compileBlink) where
 
 import           Ivory.Compile.C.CmdlineFrontend
-import           Ivory.Language
+import           Ivory.Language                  (Def, Ix (..), Module,
+                                                  Proc ((:->)), Sint32, body,
+                                                  forever, incl, package, proc,
+                                                  ret, times)
 import           Support.Device.GD32F3x0
 
 main :: Def ('[] :-> Sint32)
 main = proc "main" $ body $ do
   enablePeriphClock RCU_GPIOA
+  setMode GPIOA GPIO_MODE_OUTPUT GPIO_PUPD_NONE GPIO_PIN_15
+  setOutputOptions GPIOA GPIO_OTYPE_PP GPIO_SPEED_50MHZ GPIO_PIN_15
+  forever $ do
+    setBit GPIOA GPIO_PIN_15
+    times (ix :: Ix 10_000_000) (const $ setBit GPIOA GPIO_PIN_15)
+    resetBit GPIOA GPIO_PIN_15
+
   ret 0
 
 blinkModule :: Module
