a4589233f735420819c0026df4962f22007e88b3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-11-17 16:32:29 +0300

    some formating

--- src/Support/Device/GD32F3x0/RCU.hs
+++ src/Support/Device/GD32F3x0/RCU.hs
@@ -14,7 +14,8 @@ import           Ivory.Language.Proc
 import           Ivory.Language.Syntax
 
 data RCU_PERIPH
-  = RCU_GPIOA deriving (Show, Enum, Bounded)
+  = RCU_GPIOA 
+  deriving (Show, Enum, Bounded)
 
 enablePeriphClock :: RCU_PERIPH -> Ivory eff ()
 enablePeriphClock = call_ rcuPeriphClockEnable . extConst
