7c08cfb8a9a0a100b7533e80a4185b30e6912c47
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2022-11-20 01:00:11 +0300

    test uart

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 import           Blink (compileBlink)
 import           Shake (shake)
+import           USART (compileUSART)
 
 main :: IO ()
 main = do
   compileBlink
+  compileUSART
   shake
