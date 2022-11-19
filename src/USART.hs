1ec9f26d2a2f6a6d792f6bfb6fb875e3b4cd09ea
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-11-20 02:48:52 +0300

    check is trasmiter reafy

--- src/USART.hs
+++ src/USART.hs
@@ -37,31 +37,30 @@ main = proc "main" $ body $ do
   enablePeriphClock RCU_GPIOA
   enablePeriphClock RCU_USART1
 
-  setAF GPIOA GPIO_AF_1 GPIO_PIN_2
-  setAF GPIOA GPIO_AF_1 GPIO_PIN_3
-  setMode GPIOA GPIO_MODE_AF GPIO_PUPD_PULLUP GPIO_PIN_2
-  setOutputOptions GPIOA GPIO_OTYPE_PP GPIO_OSPEED_50MHZ GPIO_PIN_2
-  setMode GPIOA GPIO_MODE_AF GPIO_PUPD_PULLUP GPIO_PIN_3
-  setOutputOptions GPIOA GPIO_OTYPE_PP GPIO_OSPEED_50MHZ GPIO_PIN_3
-  deinitUSART USART1
-  setWordLength USART1 USART_WL_8BIT
-  setStopBit USART1 USART_STB_1BIT
-  configParity USART1 USART_PM_NONE
-  setBaudrate USART1 1_000_000
-  configReceive USART1 USART_RECEIVE_ENABLE
-  configTransmit USART1 USART_TRANSMIT_ENABLE
-  enableUSART USART1
-  forever $ do
-    flag <- getFlag USART1 USART_FLAG_RBNE
-    when flag $ receiveData USART1 >>= transmitData USART1
+  setAF             GPIOA   GPIO_AF_1                           GPIO_PIN_2
+  setMode           GPIOA   GPIO_MODE_AF    GPIO_PUPD_PULLUP    GPIO_PIN_2
+  setOutputOptions  GPIOA   GPIO_OTYPE_PP   GPIO_OSPEED_50MHZ   GPIO_PIN_2
 
+  setAF             GPIOA   GPIO_AF_1                           GPIO_PIN_3
+  setMode           GPIOA   GPIO_MODE_AF    GPIO_PUPD_PULLUP    GPIO_PIN_3
+  setOutputOptions  GPIOA   GPIO_OTYPE_PP   GPIO_OSPEED_50MHZ   GPIO_PIN_3
+
+  deinitUSART       USART1
+  setWordLength     USART1  USART_WL_8BIT
+  setStopBit        USART1  USART_STB_1BIT
+  configParity      USART1  USART_PM_NONE
+  setBaudrate       USART1  1_000_000
+  configReceive     USART1  USART_RECEIVE_ENABLE
+  configTransmit    USART1  USART_TRANSMIT_ENABLE
+  enableUSART       USART1
+
+  forever $ do
+    isDataRecived <- getFlag USART1 USART_FLAG_RBNE
+    when isDataRecived $ do
+      b <- receiveData USART1
+      forever $ do
+        isBusy <- getFlag USART1 USART_FLAG_TBE
+        unless isBusy breakOut
+        transmitData USART1 b
 
   ret 0
-
-
-delay :: Def ('[Ix 1_000_000_000] :-> ())
-delay = proc "delay" $ \n -> body $ do
-  n `times` const (call_ nop)
-
-nop :: Def ('[] :-> ())
-nop = proc "nop" $ body retVoid
