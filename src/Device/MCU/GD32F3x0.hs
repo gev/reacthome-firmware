f29c9463de7aba031613a6aa1c63694bcb1eddac
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-11-27 20:45:52 +0300

    add some combinators

--- src/Device/MCU/GD32F3x0.hs
+++ src/Device/MCU/GD32F3x0.hs
@@ -24,19 +24,28 @@ data MODE
 pa :: GPIO_PIN -> MODE -> PORT
 pa = PORT RCU_GPIOA GPIOA
 
-usart :: USART_PERIPH -> RCU_PERIPH -> (MODE -> PORT) -> MCU_GPIO
-usart u r p = USART u r . p $ AF GPIO_AF_1
+io :: GPIO_MODE -> (MODE -> PORT) -> MCU_GPIO
+io m p = PIO . p $ MF m
+
+input :: (MODE -> PORT) -> MCU_GPIO
+input = io GPIO_MODE_INPUT
+
+output :: (MODE -> PORT) -> MCU_GPIO
+output = io GPIO_MODE_OUTPUT
+
+usart :: USART_PERIPH -> RCU_PERIPH -> GPIO_AF -> (MODE -> PORT) -> MCU_GPIO
+usart u r m p  = USART u r . p $ AF m
 
 pa_2 = pa GPIO_PIN_2
 pa_3 = pa GPIO_PIN_3
+pa_15 :: MODE -> PORT
 pa_15 = pa GPIO_PIN_15
 
-pa_15_out = PIO . pa_15 $ MF GPIO_MODE_OUTPUT
-pa_15_in  = PIO . pa_15 $ MF GPIO_MODE_INPUT
+pa_15_out = output pa_15
+pa_15_in  = input pa_15
 
 
-usart_1 = usart USART1 RCU_USART1
+usart_1 = usart USART1 RCU_USART1 GPIO_AF_1
 
 usart_1_rx = usart_1 pa_2
 usart_1_tx = usart_1 pa_3
-
