d86902cb5c6e63f9511a28348795cd6bf5695816
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-12-10 21:47:37 +0300

    test system timer

--- src/Formula/Blink.hs
+++ src/Formula/Blink.hs
@@ -1,6 +1,7 @@
 module Formula.Blink where
 
 import           Device.GD32F3x0.GPIO
+import           Device.GD32F3x0.SystemTimer   (systemTimer)
 import           Device.GD32F3x0.Timer
 import           Device.GD32F3x0.USART         (usart_1)
 import           Feature
@@ -10,14 +11,15 @@ import           Formula
 import           Support.Device.GD32F3x0.Timer
 
 
+{-
+    TODO:  use frequency instead timer params
+-}
 blink :: Formula
-blink = Formula { systemClock = timer_2_irq
-                                {-
-                                  TODO:  use frequency instead timer params
-                                -}
-                                timerParam { timerPrescaler = 0
-                                           , timerPeriod = 83999
-                                           }
+blink = Formula { systemClock = systemTimer 84000
+                                -- timer_2_irq
+                                -- timerParam { timerPrescaler = 0
+                                --            , timerPeriod = 83999
+                                --            }
                 , features = [ Feature $ Blink 1 out_pa_15
                              , Feature $ Blink 2 out_pa_4
                              , Feature $ USART 1 usart_1
