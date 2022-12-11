acd16f2e8583e4d7fe86bc456c8e246a2cd87031
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2022-12-11 23:07:52 +0300

    tick new version

--- support/CMSIS/src/dwt.c
+++ support/CMSIS/src/dwt.c
@@ -10,36 +10,31 @@ void dwt_delay(uint32_t ticks) {
   while (DWT->CYCCNT - start_tick < ticks);
 }
 
-inline void delay_us(uint32_t time){
-  uint32_t ticks = 84 * time;
-  uint32_t t, dt;
-  //SysTick->VAL = 0;
-  // SysTick->LOAD = 0xFFFFFF;
-  // SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-  uint32_t start_tick = SysTick->VAL;
-  do{
-    t = SysTick->VAL;
-    dt = t < start_tick ? start_tick - t : SysTick->LOAD - t + start_tick;
-  }
-  while (dt < ticks);
-  // while(SysTick->VAL < ticks );
+
+//-----------------------
+
+void delay_us(uint32_t time){
+  uint32_t start_tick = TIMER_CNT(TIMER1);
+  while (TIMER_CNT(TIMER1) - start_tick < time);
 }
 
+void timer_config(void) {
 
-int main() {
-  rcu_periph_clock_enable(RCU_GPIOA);                                  
-  gpio_mode_set(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_4);  // re/de
-  gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
-  gpio_bit_reset(GPIOA, GPIO_PIN_4);
-  // SysTick_Config(0xFFFFFF);
+  timer_parameter_struct timer_initpara;
 
-  SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk;    
-  SysTick->LOAD = 83999;
-  SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-  while (1) {
-    gpio_bit_reset(GPIOA, GPIO_PIN_4);
-    delay_us(999);
-    gpio_bit_set(GPIOA, GPIO_PIN_4);
-    delay_us(999);
-  }
+  /* enable the peripherals clock */
+  rcu_periph_clock_enable(RCU_TIMER1);
+
+  /* deinit a TIMER */
+  timer_deinit(TIMER1);
+  /* initialize TIMER init parameter struct */
+  timer_struct_para_init(&timer_initpara);
+  /* TIMER1 configuration */
+  timer_initpara.prescaler = 83;
+  //   timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;
+  timer_initpara.counterdirection = TIMER_COUNTER_UP;
+  timer_initpara.period = 0xFFFFFFFF;
+  //   timer_initpara.clockdivision     = TIMER_CKDIV_DIV1;
+  timer_init(TIMER1, &timer_initpara);
+  timer_enable(TIMER1);
 }
