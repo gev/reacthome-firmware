19627205cbeced9b67d2efebfb818b97b8b6f0d1
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2022-12-16 22:56:56 +0300

    usart dma example

--- src/Feature/USART.hs
+++ src/Feature/USART.hs
@@ -89,30 +89,59 @@ buff' = addrOf . buff''
 
 {-
 #include "gd32f3x0.h"
-#include "gd32f3x0_gpio.h"
-#include "gd32f3x0_usart.h"
-#include <stdint.h>
-#include <string.h>
+#include <stdio.h>
 
-uint8_t transmitter_buffer[12];
-uint8_t receiver_buffer[12];
-uint8_t transfersize = 12;
-uint8_t receivesize = 12;
-uint8_t txcount = 0;
-uint16_t rxcount = 0;
 
-void uart_init(void);
+uint8_t rxbuffer[10];
+uint8_t txbuffer[] = "\n\rUSART DMA receive and transmit example, please input 10 bytes:\n\r";
+#define ARRAYNUM(arr_nanme)      (uint32_t)(sizeof(arr_nanme) / sizeof(*(arr_nanme)))
 
+void usart1_gpio_config(void);
+void usart1_config(void);
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
 int main(void)
 {
-    /* USART interrupt configuration */
-    nvic_irq_enable(USART1_IRQn, 0, 0);
+    dma_parameter_struct dma_init_struct;
+    /* enable DMA clock */
+    rcu_periph_clock_enable(RCU_DMA);
 
     /* initilize the com */
-    uart_init();
-    usart_interrupt_enable(USART1, USART_INT_RBNE);
+    usart1_gpio_config();
+    usart1_config();
 
-    while(1);
+    dma_struct_para_init(&dma_init_struct);
+    dma_init_struct.direction = DMA_MEMORY_TO_PERIPHERAL;
+    dma_init_struct.memory_addr = (uint32_t)txbuffer;
+    dma_init_struct.memory_inc = DMA_MEMORY_INCREASE_ENABLE;
+    dma_init_struct.memory_width = DMA_MEMORY_WIDTH_8BIT;
+    dma_init_struct.number = ARRAYNUM(txbuffer);
+    dma_init_struct.periph_addr = (uint32_t) &USART_TDATA(USART1);
+    dma_init_struct.periph_inc = DMA_PERIPH_INCREASE_DISABLE;
+    dma_init_struct.periph_width = DMA_PERIPHERAL_WIDTH_8BIT;
+    dma_init_struct.priority = DMA_PRIORITY_ULTRA_HIGH;
+    while(1) {
+    /* deinitialize DMA channel1 */
+    dma_deinit(DMA_CH3);
+
+    dma_init(DMA_CH3, &dma_init_struct);
+    /* configure DMA mode */
+    dma_circulation_disable(DMA_CH3);
+    dma_memory_to_memory_disable(DMA_CH3);
+    /* enable DMA channel1 */
+    dma_channel_enable(DMA_CH3);
+    
+
+        /* USART DMA enable for transmission and reception */
+        usart_dma_transmit_config(USART1, USART_DENT_ENABLE);
+        /* wait DMA Channel transfer complete */
+        while(RESET == dma_flag_get(DMA_CH3, DMA_FLAG_FTF));
+    }
 }
 
 /*!
@@ -121,14 +150,10 @@ int main(void)
     \param[out] none
     \retval     none
 */
-void uart_init(void){
-	rcu_periph_clock_enable(RCU_GPIOA);
-	rcu_periph_clock_enable(RCU_USART1);
-
-	/*RE_DE*/
-	gpio_mode_set(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_4);
-	gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
-	gpio_bit_reset(GPIOA, GPIO_PIN_4);
+void usart1_gpio_config(void)
+{
+    /* enable COM GPIO clock */
+    rcu_periph_clock_enable(RCU_GPIOA);
 
     /* connect port to USARTx_Tx */
     gpio_af_set(GPIOA, GPIO_AF_1, GPIO_PIN_2);
@@ -143,43 +168,26 @@ void uart_init(void){
     /* configure USART Rx as alternate function push-pull */
     gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_PIN_3);
     gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_10MHZ, GPIO_PIN_3);
+}
 
-
+/*!
+    \brief      configure the USART1
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void usart1_config(void)
+{
+    /* enable USART clock */
+    rcu_periph_clock_enable(RCU_USART1);
 
     /* USART configure */
     usart_deinit(USART1);
-    usart_word_length_set(USART1, USART_WL_8BIT);
-    usart_stop_bit_set(USART1, USART_STB_1BIT);
-    usart_parity_config(USART1, USART_PM_NONE);
     usart_baudrate_set(USART1, 1000000U);
     usart_receive_config(USART1, USART_RECEIVE_ENABLE);
     usart_transmit_config(USART1, USART_TRANSMIT_ENABLE);
-
     usart_enable(USART1);
-
 }
 
-void USART1_IRQHandler(void){
-    if(usart_interrupt_flag_get(USART1, USART_INT_FLAG_RBNE)) {
-        /* receive data */
-        receiver_buffer[rxcount++] = usart_data_receive(USART1);
-        if(rxcount == receivesize) {
-            rxcount = 0;
-            memcpy(transmitter_buffer, receiver_buffer, receivesize);
-            usart_interrupt_disable(USART1, USART_INT_RBNE);
-            usart_interrupt_enable(USART1, USART_INT_TBE);
-        }
-    }
-
-    else if (usart_interrupt_flag_get(USART1, USART_INT_FLAG_TBE)) {
-        /* transmit data */
-        usart_data_transmit(USART1, transmitter_buffer[txcount++]);
-        if(txcount == transfersize) {
-            txcount = 0;
-            usart_interrupt_disable(USART1, USART_INT_TBE);
-            usart_interrupt_enable(USART1, USART_INT_RBNE);
-        }
-    }
-}
 
 -}
