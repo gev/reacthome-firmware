edf13146f161863b3e7cc444d71dc51ada502096
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-12-22 22:52:27 +0300

    stylish

--- src/Util/Data/Buffer.hs
+++ src/Util/Data/Buffer.hs
@@ -7,7 +7,7 @@ module Util.Data.Buffer where
 import           Ivory.Language
 
 
-class Buffer b n t  where
+class Buffer b n t where
   storeItem     :: b n t -> Ix n -> t -> Ivory eff ()
   loadItem      :: b n t -> Ix n -> Ivory eff t
   processBuffer :: b n t
