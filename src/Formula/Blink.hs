3577e68ba992eb68c71d4d689dbcca1aae554ecf
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-12-29 13:04:18 +0300

    rbus and relay

--- src/Formula/Blink.hs
+++ src/Formula/Blink.hs
@@ -4,14 +4,14 @@ import           Device.GD32F3x0.GPIO
 import           Device.GD32F3x0.SystemClock
 import           Device.GD32F3x0.USART       (usart_1)
 import           Feature.Blink               as B
-import           Feature.RS485
+import           Feature.RBUS
 import           Formula
-import           Interface.RS485             as I
+import           Interface.RS485
 
 
 blink :: Formula
 blink = Formula { clock    = systemClock
                 , features = [ B.blink 1 out_pa_15
-                             , rs485   1 $ I.RS485 usart_1 out_pa_4
+                             , rbus    1 $ RS485 usart_1 out_pa_4
                              ]
                 }
