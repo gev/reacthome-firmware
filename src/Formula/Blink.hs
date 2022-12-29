a9f55b2cc766fe3d64e97b8c573aa49830451dd4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-12-29 13:06:09 +0300

    stylish

--- src/Formula/Blink.hs
+++ src/Formula/Blink.hs
@@ -3,7 +3,7 @@ module Formula.Blink where
 import           Device.GD32F3x0.GPIO
 import           Device.GD32F3x0.SystemClock
 import           Device.GD32F3x0.USART       (usart_1)
-import           Feature.Blink               as B
+import           Feature.Blink               as F
 import           Feature.RBUS
 import           Formula
 import           Interface.RS485
@@ -11,7 +11,7 @@ import           Interface.RS485
 
 blink :: Formula
 blink = Formula { clock    = systemClock
-                , features = [ B.blink 1 out_pa_15
+                , features = [ F.blink 1 out_pa_15
                              , rbus    1 $ RS485 usart_1 out_pa_4
                              ]
                 }
