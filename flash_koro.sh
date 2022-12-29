dfd01ee33f3cd78f96bc8987680cc9ac828111d4
Evgeny Gazdovsky <gazdovsky@gmail.com>
2022-12-29 12:29:21 +0300

    switch programmer

--- flash_koro.sh
+++ flash_koro.sh
@@ -1,3 +1,3 @@
-#JLinkExe ip tunnel:69610692::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
-# JLinkExe ip tunnel:69667069::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
-JLinkExe ip tunnel:69669681::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
+# JLinkExe ip tunnel:69610692::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
+JLinkExe ip tunnel:69667069::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
+# JLinkExe ip tunnel:69669681::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
