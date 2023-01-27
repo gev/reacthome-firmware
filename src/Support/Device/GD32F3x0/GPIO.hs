240eb36a5b79ae131362ec136a6fd41969430d89
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-01-27 16:12:00 +0300

    added port A pins

--- src/Support/Device/GD32F3x0/GPIO.hs
+++ src/Support/Device/GD32F3x0/GPIO.hs
@@ -58,9 +58,21 @@ data GPIO_OTYPE
 instance ExtDef GPIO_OTYPE Uint32
 
 data GPIO_PIN
-    = GPIO_PIN_2
+    = GPIO_PIN_0
+    | GPIO_PIN_1
+    | GPIO_PIN_2
     | GPIO_PIN_3
     | GPIO_PIN_4
+    | GPIO_PIN_5
+    | GPIO_PIN_6
+    | GPIO_PIN_7
+    | GPIO_PIN_8
+    | GPIO_PIN_9
+    | GPIO_PIN_10
+    | GPIO_PIN_11
+    | GPIO_PIN_12
+    | GPIO_PIN_13
+    | GPIO_PIN_14
     | GPIO_PIN_15
     deriving (Show, Enum, Bounded)
 instance ExtDef GPIO_PIN Uint32
