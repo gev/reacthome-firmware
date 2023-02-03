b672df9f4e5ca06742e1374ea21283dae64fde84
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-02-03 23:15:22 +0300

    added order relay

--- src/Formula/Relay_10.hs
+++ src/Formula/Relay_10.hs
@@ -12,8 +12,12 @@ import           Interface.RS485
 relay10 :: Formula
 relay10 = Formula { clock    = systemClock
                   , features = [ rbus   1 $ RS485 1 usart_1 out_pa_4
-                               , relays [ out_pa_1
-                                        , out_pa_2
+                               , relays [ out_pa_6
+                                        , out_pa_5
+                                        , out_pa_8
+                                        , out_pa_7
+                                        , out_pa_13
+                                        , out_pa_14
                                         ]
                                ]
                   }
