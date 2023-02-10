216d3c48efba61235cc72a71d89c39347116508a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-02-10 21:32:09 +0300

    Mac interface

---
+++ src/Interface/Mac.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns        #-}
+{-# LANGUAGE RankNTypes            #-}
+
+module Interface.Mac (Mac, mac) where
+
+import           GHC.TypeNats
+import           Include
+import           Initialize
+import           Ivory.Language
+import           Ivory.Language.Module
+import           Util.Data.Buffer
+import           Util.Data.Class
+
+
+data Mac n t = Mac
+    { includeMac    :: ModuleM ()
+    , initializeMac :: Buffer n t -> forall eff. Ivory eff ()
+    , buff          :: Buffer n t
+    }
+
+
+mac :: ModuleM () -> (Buffer 6 Uint8 -> forall eff. Ivory eff ()) -> Mac 6 Uint8
+mac include initialize = Mac
+    { includeMac    = include
+    , initializeMac = initialize
+    , buff          = buffer "mac"
+    }
+
+
+instance Include (Mac n t) where
+    include = includeMac >> include . buff
+
+
+instance Initialize (Mac n t) where
+    initialize (Mac {initializeMac, buff})= [proc "mac_init" $ body $ initializeMac buff]
+
+
+instance (IvoryStore t, KnownNat n) => Buff Mac n t where
+  getBuffer = getBuffer . buff
