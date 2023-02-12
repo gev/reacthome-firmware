f054276e34ee760cd3fced05e466c9f63657a0d0
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-02-12 21:54:04 +0300

    stylish

--- src/Device/GD32F3x0/Mac.hs
+++ src/Device/GD32F3x0/Mac.hs
@@ -4,15 +4,16 @@ module Device.GD32F3x0.Mac where
 
 
 import           Control.Monad
+import           GHC.TypeNats
 import           Interface.Mac
 import           Ivory.Language
 import           Support.Device.GD32F3x0.DBG
 import           Util.ByteSplit
 import           Util.Data.Class
 
-systemMac :: Mac 6 Uint8
+systemMac :: Mac
 systemMac = mac inclDBG $ \buff -> do
     id <- getID
     zipWithM_ (setItem buff)
-              [0, 1, 2, 3, 4, 5]
-            $ [0x01, 0x4a] <> bytes id
+              (iterate (+1) 0)
+            $ 0x01 : 0x4a : bytes id
