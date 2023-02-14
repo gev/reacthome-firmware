7981db8a234a94ce07c4102a9385ceca10e5a166
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-02-14 21:58:20 +0300

    simplify a formula's version

--- src/Formula.hs
+++ src/Formula.hs
@@ -14,46 +14,46 @@ import           Ivory.Language
 import           Ivory.Language.Module
 import           Scheduler             (schedule, scheduler)
 import           Util.Data.Record
-import           Util.Version
+import qualified Util.Version          as V
 
 
 data Formula where
     Formula :: MCU mcu
-           => { version  :: Version
+           => { version  :: (Uint8, Uint8)
               , mcu      :: mcu
               , features :: [Reader mcu Feature]
               } -> Formula
 
 cook :: Formula -> ModuleM ()
-cook (Formula version mcu features) = do
+cook (Formula (major, minor) mcu features) = do
 
-    let fts       = (`runReader` mcu) <$> features
-    let sch       = scheduler (systemClock mcu) $ concatMap tasks fts
+    let fts    = (`runReader` mcu) <$> features
+    let sch    = scheduler (systemClock mcu) $ concatMap tasks fts
 
-    let inits     = initialize sch
-                 <> initialize (mac mcu)
-                 <> (initialize =<< fts)
+    let inits  = initialize sch
+              <> initialize (mac mcu)
+              <> (initialize =<< fts)
 
-    let init      = proc "init"
-                  $ body
-                  $ mapM_ call_ inits
-                 :: Def ('[] :-> ())
+    let init   = proc "init"
+               $ body
+               $ mapM_ call_ inits
+              :: Def ('[] :-> ())
 
-    let loop      = schedule sch
+    let loop   = schedule sch
 
-    let main      = proc "main"
-                  $ body
-                  $ call_ init
-                 >> call_ loop
-                 >> ret 0
-                 :: Def ('[] :-> Sint32)
+    let main   = proc "main"
+               $ body
+               $ call_ init
+              >> call_ loop
+              >> ret 0
+              :: Def ('[] :-> Sint32)
 
-    include      version
-    include      (mac mcu)
-    include      sch
-    traverse_    incl inits
-    traverse_    include fts
+    include   (V.version major minor)
+    include   (mac mcu)
+    include   sch
 
+    traverse_ incl inits
+    traverse_ include fts
 
     incl init
     incl loop
--- src/Formula/Blink.hs
+++ src/Formula/Blink.hs
@@ -6,10 +6,9 @@ import           Feature.Blink   as F
 import           Feature.RBUS
 import           Formula
 import           Interface.RS485
-import qualified Util.Version    as V
 
 blink :: Formula
-blink = Formula { version   = V.version 1 0
+blink = Formula { version   = (1, 0)
                 , mcu       = gd32f3x0
                 , features  = [ F.blink 1 out_pa_15
                               ]
