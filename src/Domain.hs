6a3e4af27c9c3e0e99f91bf547bb8823c8f7262f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-02-15 21:19:12 +0300

    add mac to the domain

--- src/Domain.hs
+++ src/Domain.hs
@@ -1,13 +1,19 @@
+{-# LANGUAGE DataKinds #-}
+
 module Domain where
 
-import           Interface.SystemClock (SystemClock)
+import           Interface.SystemClock
 import           Ivory.Language
+import           Util.Data.Buffer
 import           Util.Data.Record
 import           Util.Version
+
+
 
 data Domain mcu = Domain
     { model   :: Uint8
     , version :: Record Version
-    , clock   :: SystemClock
     , mcu     :: mcu
+    , clock   :: SystemClock
+    , mac     :: Buffer 6 Uint8
     }
