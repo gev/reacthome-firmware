3e819cdf3c88a605a5f3f21aa5b22e301b676286
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-02-20 23:20:14 +0300

    add relay message payload

--- src/Endpoint/Relay.hs
+++ src/Endpoint/Relay.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds      #-}
 {-# LANGUAGE GADTs          #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
@@ -5,6 +6,7 @@ module Endpoint.Relay where
 
 import           Core.Include
 import           Core.Initialize
+import           Data.Buffer
 import           Data.Class
 import           Data.Function
 import           Data.Value
@@ -14,27 +16,35 @@ import           Ivory.Language
 
 
 data Relay = forall a. (OUT a) => Relay
- { out   :: a
- , state :: Value IBool
- }
+    { n       :: Int
+    , name    :: String
+    , out     :: a
+    , state   :: Value IBool
+    , payload :: Buffer 8 Uint8
+    }
 
 
 relay :: OUT o => Int -> o -> Relay
 relay n out = Relay
-    { out   = out
-    , state = value (name <> "_state") false
+    { n       = n
+    , name    = name
+    , out     = out
+    , state   = value (name <> "_state") false
+    , payload = buffer (name <> "_payload")
     } where name = "relay_" <> show n
 
 
 
 turnOn :: Relay -> Ivory eff ()
-turnOn (Relay {state}) = setValue state true
+turnOn (Relay {state, payload}) =
+    setValue state true >> setItem payload 2 1
 
 turnOff :: Relay -> Ivory eff ()
-turnOff (Relay {state}) = setValue state false
+turnOff (Relay {state, payload}) = do
+    setValue state false >> setItem payload 2 0
 
 manage :: Relay -> Ivory eff ()
-manage (Relay out state) = do
+manage (Relay {out, state}) = do
     s <- getValue state
     ifte_ s (set   out)
             (reset out)
@@ -42,7 +52,21 @@ manage (Relay out state) = do
 
 
 instance Include Relay where
-    include (Relay {out, state}) = include state >> include out
+    include (Relay {out, state, payload}) = do
+        include out
+        include state
+        include payload
 
 instance Initialize Relay where
-    initialize (Relay {out}) = initialize out
+    initialize (Relay {n, name, out, payload}) =
+        initialize out <> [
+            proc (name <> "_payload_init") $ body $ do
+                setItem payload 0 0
+                setItem payload 1 $ fromIntegral n
+                setItem payload 2 0
+                setItem payload 3 $ fromIntegral n
+                setItem payload 4 0
+                setItem payload 5 0
+                setItem payload 6 0
+                setItem payload 7 0
+        ]
