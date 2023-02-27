454c7916340de096c06da34136161799587a0bb2
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-02-28 01:00:44 +0300

    Clarify  GPiOs classes

--- src/Interface/GPIO.hs
+++ src/Interface/GPIO.hs
@@ -5,7 +5,7 @@ import           Core.Initialize
 import           Ivory.Language
 import           Ivory.Language.Module
 
-class (Include a, Initialize a) => Input  a where
+class (Include a, Initialize a) => Input a where
     get   :: a -> Ivory eff IBool
 
 class (Include a, Initialize a) => Output a where
--- src/Interface/GPIOs.hs
+++ src/Interface/GPIOs.hs
@@ -1,11 +1,15 @@
 module Interface.GPIOs where
 
+import           Core.Include
+import           Core.Initialize
 import           Ivory.Language
 import           Ivory.Language.Module
 
-class Inputs  a where
-    get   :: a -> Ivory eff IBool
+class (Include a, Initialize a) => Inputs a where
+    get   :: a -> Ix n -> Ivory eff IBool
 
-class Outputs a where
-    reset :: a -> Uint8 -> Ivory eff ()
-    set   :: a -> Uint8 -> Ivory eff ()
+class (Include a, Initialize a) => Outputs a where
+    reset :: a -> Ix n -> Ivory eff ()
+    set   :: a -> Ix n -> Ivory eff ()
+
+уточнять
