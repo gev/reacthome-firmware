67d980827c7653330587da3ad01ed6204d4f3c58
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-01 02:47:13 +0300

    relays endpoint

---
+++ src/Endpoint/Relays.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE DataKinds      #-}
+{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE QuasiQuotes    #-}
+
+module Endpoint.Relays where
+
+import           Core.Include
+import           Core.Initialize
+import           Data.Buffer
+import           Data.Function
+import           Data.Value
+import           Interface.GPIO
+import           Interface.Timer
+import           Ivory.Language
+
+
+
+type RelayStruct = "relay_struct"
+
+[ivory|
+    struct relay_struct
+    { state     :: IBool
+    ; delay     :: Uint32
+    ; timestamp :: Uint32
+    }
+|]
+
+
+
+data Relays =  Relays
+    { runRelays: 
+    }
+
+
+relay :: Output o => Int -> o -> Relay
+relay n out = Relay
+    { n       = n
+    , name    = name
+    , out     = out
+    , state   = value (name <> "_state") false
+    , payload = buffer (name <> "_payload")
+    } where name = "relay_" <> show n
+
+
+
+turnOn :: Relay -> Ivory eff ()
+turnOn (Relay {state, payload}) =
+    store (addrOf state) true >> store (addrOf payload ! 2) 1
+
+turnOff :: Relay -> Ivory eff ()
+turnOff (Relay {state, payload}) = do
+    store (addrOf state) false >> store (addrOf payload ! 2) 0
+
+manage :: Relay -> Ivory eff ()
+manage (Relay {out, state}) = do
+    s <- deref $ addrOf state
+    ifte_ s (set   out)
+            (reset out)
+
+
+
+instance Include Relay where
+    include (Relay {out, state, payload}) = do
+        include out
+        include state
+        include payload
+
+instance Initialize Relay where
+    initialize (Relay {n, name, out, payload}) =
+        initialize out <> [
+            proc (name <> "_payload_init") $ body $ do
+                let payload' = addrOf payload
+                store (payload' ! 0) 0
+                store (payload' ! 1) $ fromIntegral n
+                store (payload' ! 2) 0
+                store (payload' ! 3) $ fromIntegral n
+                store (payload' ! 4) 0
+                store (payload' ! 5) 0
+                store (payload' ! 6) 0
+                store (payload' ! 7) 0
+        ]
