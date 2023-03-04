183319b1861eb1259ba720901773c1df6c4e75aa
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-03-05 00:38:41 +0300

    fix functions

--- src/Support/Serialize.hs
+++ src/Support/Serialize.hs
@@ -24,29 +24,53 @@ unpack_sint8 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> S
 unpack_sint8 = importProc "unpack_sint8" "serialize.h"
 
 
-pack_uint16 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint16] :-> ())
-pack_uint16 = importProc "pack_uint16" "serialize.h"
+pack_uint16_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint16] :-> ())
+pack_uint16_be = importProc "pack_uint16_be" "serialize.h"
 
-unpack_uint16 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint16)
-unpack_uint16 = importProc "unpack_uint16" "serialize.h"
+unpack_uint16_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint16)
+unpack_uint16_be = importProc "unpack_uint16_be" "serialize.h"
 
+pack_uint16_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint16] :-> ())
+pack_uint16_le = importProc "pack_uint16_le" "serialize.h"
 
-pack_sint16 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint16] :-> ())
-pack_sint16 = importProc "pack_sint16" "serialize.h"
-
-unpack_sint16 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint16)
-unpack_sint16 = importProc "unpack_sint16" "serialize.h"
-
-
-pack_uint32 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint32] :-> ())
-pack_uint32 = importProc "pack_uint32" "serialize.h"
-
-unpack_uint32 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint32)
-unpack_uint32 = importProc "unpack_uint32" "serialize.h"
+unpack_uint16_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint16)
+unpack_uint16_le = importProc "unpack_uint16_le" "serialize.h"
 
 
-pack_sint32 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint32] :-> ())
-pack_sint32 = importProc "pack_sint32" "serialize.h"
+pack_sint16_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint16] :-> ())
+pack_sint16_be = importProc "pack_sint16_be" "serialize.h"
 
-unpack_sint32 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint32)
-unpack_sint32 = importProc "unpack_sint32" "serialize.h"
+unpack_sint16_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint16)
+unpack_sint16_be = importProc "unpack_sint16_be" "serialize.h"
+
+pack_sint16_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint16] :-> ())
+pack_sint16_le = importProc "pack_sint16_le" "serialize.h"
+
+unpack_sint16_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint16)
+unpack_sint16_le = importProc "unpack_sint16_le" "serialize.h"
+
+
+pack_uint32_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint32] :-> ())
+pack_uint32_be = importProc "pack_uint32_be" "serialize.h"
+
+unpack_uint32_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint32)
+unpack_uint32_be = importProc "unpack_uint32_be" "serialize.h"
+
+pack_uint32_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint32] :-> ())
+pack_uint32_le = importProc "pack_uint32_le" "serialize.h"
+
+unpack_uint32_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint32)
+unpack_uint32_le = importProc "unpack_uint32_le" "serialize.h"
+
+
+pack_sint32_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint32] :-> ())
+pack_sint32_be = importProc "pack_sint32_be" "serialize.h"
+
+unpack_sint32_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint32)
+unpack_sint32_be = importProc "unpack_sint32_be" "serialize.h"
+
+pack_sint32_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint32] :-> ())
+pack_sint32_le = importProc "pack_sint32_be" "serialize.h"
+
+unpack_sint32_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint32)
+unpack_sint32_le = importProc "unpack_sint32_le" "serialize.h"
