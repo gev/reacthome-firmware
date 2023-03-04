39ccb7aa6724a189d8c74fb6411613dd5fa9742b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-05 00:40:40 +0300

    stylish

--- src/Support/Serialize.hs
+++ src/Support/Serialize.hs
@@ -10,6 +10,7 @@ import           Ivory.Language
 import           Ivory.Language.Module
 
 
+
 pack_uint8 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint8] :-> ())
 pack_uint8 = importProc "pack_uint8" "serialize.h"
 
@@ -17,11 +18,13 @@ unpack_uint8 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> U
 unpack_uint8 = importProc "unpack_uint8" "serialize.h"
 
 
+
 pack_sint8 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint8] :-> ())
 pack_sint8 = importProc "pack_sint8" "serialize.h"
 
 unpack_sint8 :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Sint8)
 unpack_sint8 = importProc "unpack_sint8" "serialize.h"
+
 
 
 pack_uint16_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint16] :-> ())
@@ -37,6 +40,7 @@ unpack_uint16_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :
 unpack_uint16_le = importProc "unpack_uint16_le" "serialize.h"
 
 
+
 pack_sint16_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint16] :-> ())
 pack_sint16_be = importProc "pack_sint16_be" "serialize.h"
 
@@ -50,6 +54,7 @@ unpack_sint16_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :
 unpack_sint16_le = importProc "unpack_sint16_le" "serialize.h"
 
 
+
 pack_uint32_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Uint32] :-> ())
 pack_uint32_be = importProc "pack_uint32_be" "serialize.h"
 
@@ -61,6 +66,7 @@ pack_uint32_le = importProc "pack_uint32_le" "serialize.h"
 
 unpack_uint32_le :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n] :-> Uint32)
 unpack_uint32_le = importProc "unpack_uint32_le" "serialize.h"
+
 
 
 pack_sint32_be :: KnownNat n => Def ('[Ref s (Array n (Stored Uint8)), Ix n, Sint32] :-> ())
