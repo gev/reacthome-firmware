4323ad4158d6470944e63f84737e7d74939ad4b0
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-03-04 23:47:06 +0300

    added serialize

--- src/Support/Serialize.hs
+++ src/Support/Serialize.hs
@@ -1,1 +1,51 @@
-module Support.Serialize
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+
+module Support.Serialize where
+
+import           Ivory.Language
+import           Ivory.Language.Module
+
+
+pack_uint8 :: Def ('[Ref s (Array n (Store Uint8)), Uint16, Uint8] :-> ())
+pack_uint8 = importProc "pack_uint8" "serialize.h"
+
+unpack_uint8 :: Def ('[Ref s (Array n (Store Uint8)), Uint16] :-> Uint8)
+unpack_uint8 = importProc "unpack_uint8" "serialize.h"
+
+
+pack_int8 :: Def ('[Ref s (Array n (Store Uint8)), Uint16, Int8] :-> ())
+pack_int8 = importProc "pack_int8" "serialize.h"
+
+unpack_int8 :: Def ('[Ref s (Array n (Store Uint8)), Uint16] :-> Int8)
+unpack_int8 = importProc "unpack_int8" "serialize.h"
+
+
+pack_uint16 :: Def ('[Ref s (Array n (Store Uint8)), Uint16, Uint16] :-> ())
+pack_uint16 = importProc "pack_uint16" "serialize.h"
+
+unpack_uint16 :: Def ('[Ref s (Array n (Store Uint8)), Uint16] :-> Uint16)
+unpack_uint16 = importProc "unpack_uint16" "serialize.h"
+
+
+pack_int16 :: Def ('[Ref s (Array n (Store Uint8)), Uint16, Int16] :-> ())
+pack_int16 = importProc "pack_int16" "serialize.h"
+
+unpack_int16 :: Def ('[Ref s (Array n (Store Uint8)), Uint16] :-> Int16)
+unpack_int16 = importProc "unpack_int16" "serialize.h"
+
+
+pack_uint32 :: Def ('[Ref s (Array n (Store Uint8)), Uint16, Uint32] :-> ())
+pack_uint32 = importProc "pack_uint32" "serialize.h"
+
+unpack_uint32 :: Def ('[Ref s (Array n (Store Uint8)), Uint16] :-> Uint32)
+unpack_uint32 = importProc "unpack_uint32" "serialize.h"
+
+
+pack_int32 :: Def ('[Ref s (Array n (Store Uint8)), Uint16, Int32] :-> ())
+pack_int32 = importProc "pack_int32" "serialize.h"
+
+unpack_int32 :: Def ('[Ref s (Array n (Store Uint8)), Uint16] :-> Int32)
+unpack_int32 = importProc "unpack_int32" "serialize.h"
--- support/inc/serialize.h
+++ support/inc/serialize.h
@@ -1,5 +1,3 @@
-
-
 #include <stdint.h>
 
 
@@ -12,9 +10,6 @@ inline uint8_t unpack_uint8 (uint8_t *a, uint16_t i) {
 }
 
 
-
-
-
 inline void pack_int8 (uint8_t *a, uint16_t i, int8_t v) {
     pack_uint8(a, i, (uint8_t) v);
 }
@@ -25,8 +20,78 @@ inline int8_t unpack_int8 (uint8_t *a, uint16_t i) {
 
 
 
-pack_uint16_be
-unpack_uint16_be
+inline void pack_uint16_be (uint8_t *a, uint16_t i, int16_t v) {
+    a[i]   = v >> 8;
+    a[i+1] = v;
+}
 
-pack_uint16_le
-unpack_uint16_le
+inline uint16_t unpack_uint16_be (uint8_t *a, uint16_t i) {
+    return (uint16_t) ((a[i] << 8) | a[i+1]);
+}
+
+inline void pack_uint16_le (uint8_t *a, uint16_t i, int16_t v) {
+    a[i]   = v;
+    a[i+1] = v >> 8;
+}
+
+inline uint16_t unpack_uint16_le (uint8_t *a, uint16_t i) {
+    return (uint16_t) ((a[i+1] << 8) | a[i]);
+}
+
+
+inline void pack_int16_be (uint8_t *a, uint16_t i, int16_t v) {
+    pack_uint16_be(a, i, v);
+}
+
+inline uint16_t unpack_int16_be (uint8_t *a, uint16_t i) {
+    return (uint16_t) unpack_uint16_be (a, i);
+}
+
+inline void pack_int16_le (uint8_t *a, uint16_t i, int16_t v) {
+    pack_uint16_le(a, i, v);
+}
+
+inline uint16_t unpack_int16_le (uint8_t *a, uint16_t i) {
+    return (uint16_t) unpack_uint16_le (a, i);
+}
+
+
+
+inline void pack_uint32_be (uint8_t *a, uint16_t i, int32_t v) {
+    a[i]   = v >> 24;
+    a[i+1] = v >> 16;
+    a[i+2] = v >> 8;
+    a[i+3] = v;
+}
+
+inline uint32_t unpack_uint32_be (uint8_t *a, uint16_t i) {
+    return (uint32_t) ((a[i] << 24) | (a[i+1] << 16) | (a[i+2] << 8) | a[i+3]);
+}
+
+inline void pack_uint32_le (uint8_t *a, uint16_t i, int32_t v) {
+    a[i]   = v;
+    a[i+1] = v >> 8;
+    a[i+2] = v >> 16;
+    a[i+3] = v >> 24;
+}
+
+inline uint32_t unpack_uint32_le (uint8_t *a, uint16_t i) {
+    return (uint32_t) ((a[i] | (a[i+1] << 8) | (a[i+2] << 16) | a[i+3] << 24));
+}
+
+
+inline void pack_int32_be (uint8_t *a, uint16_t i, int32_t v) {
+    pack_uint32_be(a, i, v);
+}
+
+inline uint32_t unpack_int32_be (uint8_t *a, uint16_t i) {
+    return (uint32_t) unpack_uint32_be (a, i);
+}
+
+inline void pack_int32_le (uint8_t *a, uint16_t i, int32_t v) {
+    pack_uint32_le (a, i, v);
+}
+
+inline uint32_t unpack_int32_le (uint8_t *a, uint16_t i) {
+    return (uint32_t) unpack_uint32_le (a, i);
+}
