4e959ad8817e621673be486ade6dc79249582652
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-03-07 11:48:29 +0300

    fix startup

--- support/device/gd32f4xx/src/startup_gd32f450_470.s
+++ support/device/gd32f4xx/src/startup_gd32f450_470.s
@@ -55,7 +55,7 @@ LoopFillZerobss:
 /* Call the clock system initialization function.*/
   bl  SystemInit   
 /* Call into static constructors (C++) */
-  bl __libc_init_array
+#  bl __libc_init_array
 /* Call the application's entry point.*/
   bl  main
   bx  lr    
