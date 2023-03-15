53ea908e96f4259e5144c31c9b802b25870d8e82
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-15 19:53:13 +0300

    add output Context

---
+++ src/Core/Context.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Core.Context where
+
+import           Control.Monad.Writer  (Writer, tell)
+import           Ivory.Language
+import           Ivory.Language.Module
+
+
+data Context = Context
+    { getModule       :: ModuleM ()
+    , getInitializers :: [Def ('[] :-> ())]
+    }
+
+
+class Include a where
+    include :: a -> Writer Context ()
+
+instance Include (ModuleM ()) where
+    include a = tell $ Context a mempty
+
+instance Include [Def ('[] :-> ())] where
+    include a = tell $ Context mempty a
+
+instance Semigroup Context where
+  (Context m1 i1) <> (Context m2 i2) = Context (m1 <> m2) (i1 <> i2)
+
+instance Monoid Context where
+  mempty = Context mempty mempty
