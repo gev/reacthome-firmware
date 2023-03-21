1860c669fc19040a89151f1894b64f01f2fe4537
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-21 18:42:55 +0300

    typo

--- support/device/gd32f4xx/inc/gd32f4xx.h
+++ support/device/gd32f4xx/inc/gd32f4xx.h
@@ -43,7 +43,7 @@ OF SUCH DAMAGE.
 
 /* define GD32F4xx */
 #if !defined (GD32F450)  && !defined (GD32F405) && !defined (GD32F407) && !defined (GD32F470)  && !defined (GD32F425) && !defined (GD32F427)
- /* #define GD32F450 */ 
+  /* #define GD32F450 */ 
   /* #define GD32F405 */
   /* #define GD32F407 */
   /* #define GD32F470 */
