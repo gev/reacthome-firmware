775ee98bf2b8af48494bb7ed3e13dc6fdc82453f
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-28 23:46:53 +0300

    -O3

--- src/Build/Compiler/GCC/GD32F4xx.hs
+++ src/Build/Compiler/GCC/GD32F4xx.hs
@@ -35,7 +35,7 @@ instance Compiler GCC GD32F4xx where
                     -- , "-fms-extensions"
                     , "-ffunction-sections"
                     , "-Wall"
-                    , "-O0"
+                    , "-O3"
                     ]
 
         , ld      = "-Tsupport/device/gd32f4xx/gd32f450-470.ld"
