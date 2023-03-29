01d50c15da73db7262e3d9688bd0d72be2ab49d2
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-03-29 22:23:55 +0300

    remove lib in lab

--- lab/gd32f450/450_uart_dma_test/build/gd32f4xx_pmu.d
+++ lab/gd32f450/450_uart_dma_test/build/gd32f4xx_pmu.d
@@ -1,64 +1,74 @@
-build/gd32f4xx_pmu.o: lib/GD32F4xx/Source/gd32f4xx_pmu.c \
- lib/GD32F4xx/Include/gd32f4xx_pmu.h \
- lib/CMSIS/GD/GD32F4xx/Include/gd32f4xx.h lib/CMSIS/core_cm4.h \
- lib/CMSIS/core_cmInstr.h lib/CMSIS/core_cmFunc.h \
- lib/CMSIS/core_cm4_simd.h \
- lib/CMSIS/GD/GD32F4xx/Include/system_gd32f4xx.h \
- lib/GD32F4xx/Include/gd32f4xx_libopt.h \
- lib/GD32F4xx/Include/gd32f4xx_rcu.h lib/GD32F4xx/Include/gd32f4xx_adc.h \
- lib/GD32F4xx/Include/gd32f4xx_can.h lib/GD32F4xx/Include/gd32f4xx_crc.h \
- lib/GD32F4xx/Include/gd32f4xx_ctc.h lib/GD32F4xx/Include/gd32f4xx_dac.h \
- lib/GD32F4xx/Include/gd32f4xx_dbg.h lib/GD32F4xx/Include/gd32f4xx_dci.h \
- lib/GD32F4xx/Include/gd32f4xx_dma.h lib/GD32F4xx/Include/gd32f4xx_exti.h \
- lib/GD32F4xx/Include/gd32f4xx_fmc.h \
- lib/GD32F4xx/Include/gd32f4xx_fwdgt.h \
- lib/GD32F4xx/Include/gd32f4xx_gpio.h \
- lib/GD32F4xx/Include/gd32f4xx_syscfg.h \
- lib/GD32F4xx/Include/gd32f4xx_i2c.h lib/GD32F4xx/Include/gd32f4xx_iref.h \
- lib/GD32F4xx/Include/gd32f4xx_pmu.h lib/GD32F4xx/Include/gd32f4xx_rtc.h \
- lib/GD32F4xx/Include/gd32f4xx_sdio.h lib/GD32F4xx/Include/gd32f4xx_spi.h \
- lib/GD32F4xx/Include/gd32f4xx_timer.h \
- lib/GD32F4xx/Include/gd32f4xx_trng.h \
- lib/GD32F4xx/Include/gd32f4xx_usart.h \
- lib/GD32F4xx/Include/gd32f4xx_wwdgt.h \
- lib/GD32F4xx/Include/gd32f4xx_misc.h \
- lib/GD32F4xx/Include/gd32f4xx_enet.h \
- lib/GD32F4xx/Include/gd32f4xx_exmc.h lib/GD32F4xx/Include/gd32f4xx_ipa.h \
- lib/GD32F4xx/Include/gd32f4xx_tli.h
-lib/GD32F4xx/Include/gd32f4xx_pmu.h:
-lib/CMSIS/GD/GD32F4xx/Include/gd32f4xx.h:
-lib/CMSIS/core_cm4.h:
-lib/CMSIS/core_cmInstr.h:
-lib/CMSIS/core_cmFunc.h:
-lib/CMSIS/core_cm4_simd.h:
-lib/CMSIS/GD/GD32F4xx/Include/system_gd32f4xx.h:
-lib/GD32F4xx/Include/gd32f4xx_libopt.h:
-lib/GD32F4xx/Include/gd32f4xx_rcu.h:
-lib/GD32F4xx/Include/gd32f4xx_adc.h:
-lib/GD32F4xx/Include/gd32f4xx_can.h:
-lib/GD32F4xx/Include/gd32f4xx_crc.h:
-lib/GD32F4xx/Include/gd32f4xx_ctc.h:
-lib/GD32F4xx/Include/gd32f4xx_dac.h:
-lib/GD32F4xx/Include/gd32f4xx_dbg.h:
-lib/GD32F4xx/Include/gd32f4xx_dci.h:
-lib/GD32F4xx/Include/gd32f4xx_dma.h:
-lib/GD32F4xx/Include/gd32f4xx_exti.h:
-lib/GD32F4xx/Include/gd32f4xx_fmc.h:
-lib/GD32F4xx/Include/gd32f4xx_fwdgt.h:
-lib/GD32F4xx/Include/gd32f4xx_gpio.h:
-lib/GD32F4xx/Include/gd32f4xx_syscfg.h:
-lib/GD32F4xx/Include/gd32f4xx_i2c.h:
-lib/GD32F4xx/Include/gd32f4xx_iref.h:
-lib/GD32F4xx/Include/gd32f4xx_pmu.h:
-lib/GD32F4xx/Include/gd32f4xx_rtc.h:
-lib/GD32F4xx/Include/gd32f4xx_sdio.h:
-lib/GD32F4xx/Include/gd32f4xx_spi.h:
-lib/GD32F4xx/Include/gd32f4xx_timer.h:
-lib/GD32F4xx/Include/gd32f4xx_trng.h:
-lib/GD32F4xx/Include/gd32f4xx_usart.h:
-lib/GD32F4xx/Include/gd32f4xx_wwdgt.h:
-lib/GD32F4xx/Include/gd32f4xx_misc.h:
-lib/GD32F4xx/Include/gd32f4xx_enet.h:
-lib/GD32F4xx/Include/gd32f4xx_exmc.h:
-lib/GD32F4xx/Include/gd32f4xx_ipa.h:
-lib/GD32F4xx/Include/gd32f4xx_tli.h:
+build/gd32f4xx_pmu.o: \
+ ../../../support/device/gd32f4xx/peripherals/src/gd32f4xx_pmu.c \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_pmu.h \
+ ../../../support/device/gd32f4xx/inc/gd32f4xx.h \
+ ../../../support/CMSIS/inc/core_cm4.h \
+ ../../../support/CMSIS/inc/core_cmInstr.h \
+ ../../../support/CMSIS/inc/core_cmFunc.h \
+ ../../../support/CMSIS/inc/core_cm4_simd.h \
+ ../../../support/device/gd32f4xx/inc/system_gd32f4xx.h \
+ ../../../support/device/gd32f4xx/inc/gd32f4xx_libopt.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_rcu.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_adc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_can.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_crc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_ctc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dac.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dbg.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dci.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dma.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_exti.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_fmc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_fwdgt.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_gpio.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_syscfg.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_i2c.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_iref.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_rtc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_sdio.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_spi.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_timer.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_trng.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_usart.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_wwdgt.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_misc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_enet.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_exmc.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_ipa.h \
+ ../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_tli.h
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_pmu.h:
+../../../support/device/gd32f4xx/inc/gd32f4xx.h:
+../../../support/CMSIS/inc/core_cm4.h:
+../../../support/CMSIS/inc/core_cmInstr.h:
+../../../support/CMSIS/inc/core_cmFunc.h:
+../../../support/CMSIS/inc/core_cm4_simd.h:
+../../../support/device/gd32f4xx/inc/system_gd32f4xx.h:
+../../../support/device/gd32f4xx/inc/gd32f4xx_libopt.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_rcu.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_adc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_can.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_crc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_ctc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dac.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dbg.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dci.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_dma.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_exti.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_fmc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_fwdgt.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_gpio.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_syscfg.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_i2c.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_iref.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_rtc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_sdio.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_spi.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_timer.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_trng.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_usart.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_wwdgt.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_misc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_enet.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_exmc.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_ipa.h:
+../../../support/device/gd32f4xx/peripherals/inc/gd32f4xx_tli.h:
