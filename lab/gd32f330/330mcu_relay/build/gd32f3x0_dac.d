15e7195fd851ed9dfded6ff672064c4d7f658dbf
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-29 23:00:40 +0300

    cleanup

--- lab/gd32f330/330mcu_relay/build/gd32f3x0_dac.d
+++
@@ -1,2 +0,0 @@
-build/gd32f3x0_dac.o: \
- ../../../support/device/gd32f3x0/peripherals/src/gd32f3x0_dac.c
--- lab/gd32f330/gd32_led/build/gd32f3x0_cec.d
+++
@@ -1,2 +0,0 @@
-build/gd32f3x0_cec.o: \
- ../../../support/device/gd32f3x0/peripherals/src/gd32f3x0_cec.c
