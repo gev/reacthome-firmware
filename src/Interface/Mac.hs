fb7f546633d385cf914ca609dbe71ee3c176227a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-31 22:59:04 +0300

    The Mac is Values now

--- src/Interface/Mac.hs
+++ src/Interface/Mac.hs
@@ -9,21 +9,21 @@ module Interface.Mac where
 
 import           Control.Monad.Writer
 import           Core.Context
-import           Data.Buffer
+import           Data.Value
 import           GHC.TypeNats
 import           Ivory.Language
 import           Ivory.Language.Module
 
 
-type Mac = Buffer 6 Uint8
+type Mac = Values 6 Uint8
 
 
 makeMac :: MonadWriter Context m
-        => (Buffer 6 Uint8 -> forall eff. Ivory eff ())
+        => (Values 6 Uint8 -> forall eff. Ivory eff ())
         -> String
         -> m Mac
 makeMac initMac name = do
-    mac <- buffer name
+    mac <- values_ name
     let initMac' :: Def ('[] ':-> ())
         initMac' = proc (name <> "_init") $ body $ initMac mac
     addInit initMac'
