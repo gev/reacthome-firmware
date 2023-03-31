ebaeb3307b5d73afbdd399f0883a71fb084e6bb5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-03-31 23:26:41 +0300

    add the Versions array

--- src/Core/Version.hs
+++ src/Core/Version.hs
@@ -5,8 +5,10 @@
 
 module Core.Version
     ( Version
+    , Versions
     , version_
     , version
+    , versions_
     , major
     , minor
     ) where
@@ -14,6 +16,7 @@ module Core.Version
 import           Control.Monad.Writer
 import           Core.Context
 import           Data.Record
+import           GHC.TypeNats
 import           Ivory.Language
 
 
@@ -21,7 +24,8 @@ import           Ivory.Language
 type VersionStruct = "version_struct"
 
 
-type Version = Record VersionStruct
+type Version    = Record   VersionStruct
+type Versions n = Records n VersionStruct
 
 
 [ivory|
@@ -31,6 +35,11 @@ type Version = Record VersionStruct
     }
 |]
 
+
+versions_ :: (MonadWriter Context m, KnownNat n) => String -> m (Versions n)
+versions_ name = do
+    addStruct (Proxy :: Proxy VersionStruct)
+    records_ name
 
 version_ :: MonadWriter Context m => String -> m Version
 version_ name = do
