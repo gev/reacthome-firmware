f0b1bd3bbf2108b8e4ecfd54445ff10d6e5c0f4b
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-04-03 02:41:57 +0300

    refactor IRQ module

--- src/Support/Device/GD32F4xx/IRQ.hs
+++ src/Support/Device/GD32F4xx/IRQ.hs
@@ -1,9 +1,38 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE RankNTypes                 #-}
 
 module Support.Device.GD32F4xx.IRQ
-    ( IRQn (..)
+    ( IRQn
+    , timer1_irqn
+    , timer2_irqn
+    , usart0_irqn
+    , usart1_irqn
+    , usart2_irqn
+    , usart5_irqn
+    , uart3_irqn
+    , uart4_irqn
+    , uart6_irqn
+    , uart7_irqn
+    , dma0_channel0_irqn
+    , dma0_channel1_irqn
+    , dma0_channel2_irqn
+    , dma0_channel3_irqn
+    , dma0_channel4_irqn
+    , dma0_channel5_irqn
+    , dma0_channel6_irqn
+    , dma0_channel7_irqn
+    , dma1_channel0_irqn
+    , dma1_channel1_irqn
+    , dma1_channel2_irqn
+    , dma1_channel3_irqn
+    , dma1_channel4_irqn
+    , dma1_channel5_irqn
+    , dma1_channel6_irqn
+    , dma1_channel7_irqn
+
     , makeIRQHandler
+
     , inclIRQ
     ) where
 
@@ -15,43 +44,70 @@ import           Ivory.Support
 import           Ivory.Support.Device.GD32F4xx
 
 
-data IRQn
-    = TIMER1_IRQn
-    | TIMER2_IRQn
-    | USART0_IRQn
-    | USART1_IRQn
-    | USART2_IRQn
-    | USART5_IRQn
-    | UART3_IRQn
-    | UART4_IRQn
-    | UART6_IRQn
-    | UART7_IRQn
-    | DMA0_Channel0_IRQn
-    | DMA0_Channel1_IRQn
-    | DMA0_Channel2_IRQn
-    | DMA0_Channel3_IRQn
-    | DMA0_Channel4_IRQn
-    | DMA0_Channel5_IRQn
-    | DMA0_Channel6_IRQn
-    | DMA0_Channel7_IRQn
-    | DMA1_Channel0_IRQn
-    | DMA1_Channel1_IRQn
-    | DMA1_Channel2_IRQn
-    | DMA1_Channel3_IRQn
-    | DMA1_Channel4_IRQn
-    | DMA1_Channel5_IRQn
-    | DMA1_Channel6_IRQn
-    | DMA1_Channel7_IRQn
-    deriving (Show, Enum, Bounded)
-instance ExtDef IRQn Uint8
+newtype IRQn = IRQn Uint8
+    deriving (IvoryExpr, IvoryInit, IvoryVar, IvoryType)
+
+timer1_irqn        = IRQn $ ext "TIMER1_IRQn"
+timer2_irqn        = IRQn $ ext "TIMER2_IRQn"
+usart0_irqn        = IRQn $ ext "USART0_IRQn"
+usart1_irqn        = IRQn $ ext "USART1_IRQn"
+usart2_irqn        = IRQn $ ext "USART2_IRQn"
+usart5_irqn        = IRQn $ ext "USART5_IRQn"
+uart3_irqn         = IRQn $ ext "UART3_IRQn"
+uart4_irqn         = IRQn $ ext "UART4_IRQn"
+uart6_irqn         = IRQn $ ext "UART6_IRQn"
+uart7_irqn         = IRQn $ ext "UART7_IRQn"
+dma0_channel0_irqn = IRQn $ ext "DMA0_Channel0_IRQn"
+dma0_channel1_irqn = IRQn $ ext "DMA0_Channel1_IRQn"
+dma0_channel2_irqn = IRQn $ ext "DMA0_Channel2_IRQn"
+dma0_channel3_irqn = IRQn $ ext "DMA0_Channel3_IRQn"
+dma0_channel4_irqn = IRQn $ ext "DMA0_Channel4_IRQn"
+dma0_channel5_irqn = IRQn $ ext "DMA0_Channel5_IRQn"
+dma0_channel6_irqn = IRQn $ ext "DMA0_Channel6_IRQn"
+dma0_channel7_irqn = IRQn $ ext "DMA0_Channel7_IRQn"
+dma1_channel0_irqn = IRQn $ ext "DMA1_Channel0_IRQn"
+dma1_channel1_irqn = IRQn $ ext "DMA1_Channel1_IRQn"
+dma1_channel2_irqn = IRQn $ ext "DMA1_Channel2_IRQn"
+dma1_channel3_irqn = IRQn $ ext "DMA1_Channel3_IRQn"
+dma1_channel4_irqn = IRQn $ ext "DMA1_Channel4_IRQn"
+dma1_channel5_irqn = IRQn $ ext "DMA1_Channel5_IRQn"
+dma1_channel6_irqn = IRQn $ ext "DMA1_Channel6_IRQn"
+dma1_channel7_irqn = IRQn $ ext "DMA1_Channel7_IRQn"
+
 
 
 inclIRQ :: ModuleDef
-inclIRQ = inclDef (def :: Cast IRQn Uint8)
+inclIRQ = do
+    inclSym timer1_irqn
+    inclSym timer2_irqn
+    inclSym usart0_irqn
+    inclSym usart1_irqn
+    inclSym usart2_irqn
+    inclSym usart5_irqn
+    inclSym uart3_irqn
+    inclSym uart4_irqn
+    inclSym uart6_irqn
+    inclSym uart7_irqn
+    inclSym dma0_channel0_irqn
+    inclSym dma0_channel1_irqn
+    inclSym dma0_channel2_irqn
+    inclSym dma0_channel3_irqn
+    inclSym dma0_channel4_irqn
+    inclSym dma0_channel5_irqn
+    inclSym dma0_channel6_irqn
+    inclSym dma0_channel7_irqn
+    inclSym dma1_channel0_irqn
+    inclSym dma1_channel1_irqn
+    inclSym dma1_channel2_irqn
+    inclSym dma1_channel3_irqn
+    inclSym dma1_channel4_irqn
+    inclSym dma1_channel5_irqn
+    inclSym dma1_channel6_irqn
+    inclSym dma1_channel7_irqn
 
 
-makeIRQHandler :: Show t
+makeIRQHandler :: ExtSymbol t
                => t
                -> (forall s. Ivory (ProcEffects s ()) ())
                -> ModuleDef
-makeIRQHandler t b = incl $ proc (show t <> "_IRQHandler") $ body b
+makeIRQHandler t b = incl $ proc (symbol t <> "_IRQHandler") $ body b
