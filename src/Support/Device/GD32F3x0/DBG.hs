8d45da3076c13870a752a2c1570a750ffc44d7d5
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-04-03 00:11:32 +0300

    stylish

--- src/Support/Device/GD32F3x0/DBG.hs
+++ src/Support/Device/GD32F3x0/DBG.hs
@@ -15,12 +15,14 @@ import           Ivory.Support
 import           Ivory.Support.Device.GD32F3x0
 
 
-inclDBG :: ModuleDef
-inclDBG = incl dbg_id_get
-
 
 getID :: Ivory eff Uint32
 getID = call dbg_id_get
 
 dbg_id_get :: Def ('[] :-> Uint32)
 dbg_id_get = fun "dbg_id_get"
+
+
+
+inclDBG :: ModuleDef
+inclDBG = incl dbg_id_get
