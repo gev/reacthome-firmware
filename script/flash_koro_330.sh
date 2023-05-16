7a7a51732fb48010e01177cc06afeac21b0359c8
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-05-16 15:37:12 +0300

    up script 330

--- script/flash_koro_330.sh
+++ script/flash_koro_330.sh
@@ -1,3 +1,4 @@
-# JLinkExe ip tunnel:69610692::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
-# JLinkExe ip tunnel:69667069::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/FlashMCU.jlink
+# JLinkExe ip 192.168.88.154 -Device GD32F330K8 -If SWD -Speed 1000 jlink/gd32f330/FlashMCU.jlink
+# JLinkExe ip tunnel:69610692::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/gd32f330/FlashMCU.jlink
+# JLinkExe ip tunnel:69667069::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/gd32f330/FlashMCU.jlink
 JLinkExe ip tunnel:69669681::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/gd32f330/FlashMCU.jlink
