c152f48111bf605c1790bfbebd4ba722eaa1c323
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-21 22:22:30 +0300

    some unsafe casting

--- src/Support/Cast.hs
+++ src/Support/Cast.hs
@@ -5,6 +5,7 @@
 
 module Support.Cast
     ( castArrayToUint32
+    , castFloatToUint16
     , inclCast
     ) where
 
@@ -12,11 +13,20 @@ import           Ivory.Language
 import           Ivory.Language.Module
 
 inclCast :: ModuleDef
-inclCast = incl cast_to_uint32
+inclCast = do
+    incl cast_to_uint16
+    incl cast_to_uint32
 
 
 castArrayToUint32 :: Ref r (CArray (Stored Uint16)) -> Ivory eff Uint32
 castArrayToUint32 = call cast_to_uint32
 
+castFloatToUint16 :: IFloat -> Ivory eff Uint16
+castFloatToUint16 = call cast_to_uint16
+
 cast_to_uint32 :: Def ('[Ref r (CArray (Stored Uint16))] :-> Uint32)
 cast_to_uint32 = importProc "cast_to_uint32" "cast.h"
+
+
+cast_to_uint16 :: Def ('[IFloat] :-> Uint16)
+cast_to_uint16 = importProc "cast_to_uint16" "cast.h"
