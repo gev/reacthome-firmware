f18c8de8a47882f42697590905f7850885038759
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-24 21:01:28 +0300

    code review

--- src/Interface/PWM.hs
+++ src/Interface/PWM.hs
@@ -2,17 +2,15 @@ module Interface.PWM where
 
 import           Ivory.Language
 
-type Prescaler = Uint32
-type Period = Uint32
 
-data ModePMW 
-    = FORCE_LOW
+data ModePMW
+    = DUTY_LOW
+    | DUTY_HIGH
+    | FORCE_LOW
     | FORCE_HIGH
-    | DUTY_LOW
-    | DURY_HIGH
 
 
 class PWM p where
     setDuty        :: p -> Uint16 -> Ivory eff ()
     resetCounter   :: p -> Ivory eff ()
-    setModePWM     :: p -> ModePMW -> Ivory eff ()
+    setMode        :: p -> ModePMW -> Ivory eff ()
