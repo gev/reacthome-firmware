86d2550e07e91f7345cde1d463af5bf8a32a8ded
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-05-24 20:33:50 +0300

    add interface set pwm mode

--- src/Interface/PWM.hs
+++ src/Interface/PWM.hs
@@ -5,6 +5,14 @@ import           Ivory.Language
 type Prescaler = Uint32
 type Period = Uint32
 
+data ModePMW 
+    = FORCE_LOW
+    | FORCE_HIGH
+    | DUTY_LOW
+    | DURY_HIGH
+
+
 class PWM p where
     setDuty        :: p -> Uint16 -> Ivory eff ()
     resetCounter   :: p -> Ivory eff ()
+    setModePWM     :: p -> ModePMW -> Ivory eff ()
