3f9cd53ec481505e294a21fc8b2cb7069cddeb3e
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-24 13:22:34 +0300

    add cross zero handler

--- src/Feature/DimmerAC.hs
+++ src/Feature/DimmerAC.hs
@@ -2,26 +2,31 @@
 
 module Feature.DimmerAC where
 
-import           Control.Monad.Reader
-import           Control.Monad.Writer
+import           Control.Monad.Reader (MonadReader, asks)
+import           Control.Monad.Writer (MonadWriter)
 import           Core.Context
 import           Core.Controller
-import           Core.Domain as D
+import           Core.Domain          as D
 import           Core.Feature
+import           Core.Handler
 import           Core.Transport       as T
+import           Interface.EXTI
+import           Interface.MCU
+import           Interface.PWM
 
 
 data DimmerAC = DimmerAC
 
 dimmerAC :: ( MonadWriter Context m
             , MonadReader (Domain p t) m
-            , T.Transport t
+            , T.Transport t, PWM o, Handler HandleEXTI e, EXTI e
             ) => [p -> m o] -> (p -> m e) -> m Feature
 dimmerAC pwms exti = do
     mcu <- asks D.mcu
-    e   <- exti mcu
-    let CrossZerro = mapM_ resetCounter pwms
-    addHandler e CrossZerro
+    e   <- exti $ peripherals mcu
+    os  <- mapM ($ peripherals mcu) pwms
+    let crossZero = mapM_ resetCounter os
+    addHandler $ HandleEXTI e crossZero
     pure $ Feature DimmerAC
 
 
