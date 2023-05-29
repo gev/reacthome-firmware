5a2f2c648b7b94c1346c2bc0b618ff45c59a25a2
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-05-29 16:10:16 +0300

    to be continue

---
+++ src/Data/NeoPixel/Buffer.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Data.NeoPixel.Buffer where
+
+import           GHC.TypeNats
+import           Ivory.Language
+
+
+type Px n = Ix (n * 1)
+
+
+class NeoPixelBuffer n t where
+    writeByte :: t n -> Px n -> Uint8 -> Ivory eff ()
