17b68970e1ffa8ce82a4ff28d96d2335a1966605
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-06-01 01:20:51 +0300

    The NeoPixel handler

--- src/Interface/NeoPixel.hs
+++ src/Interface/NeoPixel.hs
@@ -1,16 +1,24 @@
 {-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE RankNTypes             #-}
 
 module Interface.NeoPixel where
 
 import           Control.Monad.Writer
 import           Core.Context
+import           Core.Handler
 import           Data.NeoPixel.Buffer
 import           GHC.TypeNats
 import           Ivory.Language
 
 
-class NeoPixel p b | p -> b where
+data HandleNeoPixel p = HandleNeoPixel
+    { neoPixel :: p
+    , handle   :: forall eff. Ivory eff ()
+    }
+
+
+class Handler HandleNeoPixel p => NeoPixel p b | p -> b where
     neoPixelBuffer :: (KnownNat n, MonadWriter Context m)
                    => p -> String -> m (b n)
     transmitPixels :: KnownNat n => p -> b n -> Ivory eff ()
