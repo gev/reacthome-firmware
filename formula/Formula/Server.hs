05bc65027b8815d2eef630a399c6e61e4646ae73
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-06-08 23:00:38 +0300

    fix server indicator

--- formula/Formula/Server.hs
+++ formula/Formula/Server.hs
@@ -1,13 +1,13 @@
 module Formula.Server where
 
 import           Core.Formula
+import           Data.Color
 import           Device.GD32F4xx
+import           Feature.Indicator
 import           Feature.RS485.RBUS  as F
 import           Interface.RS485
 import           Ivory.Language
 import           Transport.UART.RBUS as U
-import           Feature.Indicator
-import           Data.Color
 
 
 server :: Formula GD32F4xx
@@ -16,7 +16,7 @@ server = Formula { name       = "server"
                  , version    = (1, 0)
                  , shouldInit = true
                  , transport  = U.rbus uart_1
-                 , features   = [ indicator npx_pwm_2 $ hsv 120 1 0.2 
+                 , features   = [ indicator npx_pwm_2 120
                                 , F.rbus [ rs485 1 uart_0 out_pd_1
                                          , rs485 2 uart_2 out_pb_15
                                          , rs485 3 uart_3 out_pc_12
