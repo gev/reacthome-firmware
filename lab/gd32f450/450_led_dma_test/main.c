c1807eefddd4f28b1c095bc7a14a272ae65a333b
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-06-08 17:53:41 +0300

    up test

--- lab/gd32f450/450_led_dma_test/main.c
+++ lab/gd32f450/450_led_dma_test/main.c
@@ -4,10 +4,10 @@
 #include "gd32f4xx.h"
 
 #define LED_NUM 5
-#define LENGHT_TRANSMIT (LED_NUM * 24)
+#define LENGHT_TRANSMIT (LED_NUM * 24 + 1)
 
-#define LED1 66
-#define LED0 33
+#define LED1 90
+#define LED0 30
 
 typedef struct {
   uint16_t g[8];
@@ -15,10 +15,10 @@ typedef struct {
   uint16_t b[8];
 } led_t;
 
-// #define TIMER0_CH0CC  ((uint32_t)0x40012c34)
-led_t buffer[LED_NUM] = {0};
 
-void timer_conf(uint16_t);
+led_t buffer[LED_NUM+1] = {0};
+
+void timer_conf();
 
 /*!
     \brief      configure the GPIO ports
@@ -29,6 +29,10 @@ void timer_conf(uint16_t);
 void gpio_config(void) {
   rcu_periph_clock_enable(RCU_GPIOB);
   rcu_periph_clock_enable(RCU_GPIOC);
+
+  gpio_mode_set(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_0);  // ch2
+  gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0);
+  gpio_af_set(GPIOB, GPIO_AF_2, GPIO_PIN_0);
 
   gpio_mode_set(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_5);  // ch1
   gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_5);
@@ -87,47 +91,56 @@ void dma_send_buf(uint16_t *buf, uint32_t size_buf, uint16_t timer_ch) {
   /* enable DMA channel5 */
   dma_channel_enable(DMA0, DMA_CH2);
 
-  timer_conf(timer_ch);
-  while (RESET == dma_flag_get(DMA0, DMA_CH2, DMA_FLAG_FTF))
-    ;
-  timer_deinit(TIMER2);
 }
 
-void timer_conf(uint16_t timer_ch) {
+void timer_conf() {
   timer_oc_parameter_struct timer_ocintpara;
   timer_parameter_struct timer_initpara;
 
   rcu_periph_clock_enable(RCU_TIMER2);
-  rcu_timer_clock_prescaler_config(RCU_TIMER_PSC_MUL4);
 
   timer_deinit(TIMER2);
 
   /* TIMER2 configuration */
-  timer_initpara.prescaler = 1;
+  timer_initpara.prescaler = 0;
   timer_initpara.alignedmode = TIMER_COUNTER_EDGE;
   timer_initpara.counterdirection = TIMER_COUNTER_UP;
-  timer_initpara.period = 100;
+  timer_initpara.period = 120;
   timer_initpara.clockdivision = TIMER_CKDIV_DIV1;
   timer_initpara.repetitioncounter = 0;
   timer_init(TIMER2, &timer_initpara);
 
-  /* CH0 configuration in PWM1 mode */
+  /* CH configuration in PWM0 mode */
   timer_ocintpara.outputstate = TIMER_CCX_ENABLE;
   timer_ocintpara.outputnstate = TIMER_CCXN_ENABLE;
   timer_ocintpara.ocpolarity = TIMER_OC_POLARITY_HIGH;
   timer_ocintpara.ocnpolarity = TIMER_OCN_POLARITY_HIGH;
   timer_ocintpara.ocidlestate = TIMER_OC_IDLE_STATE_HIGH;
   timer_ocintpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
-  timer_channel_output_config(TIMER2, timer_ch, &timer_ocintpara);
+  timer_channel_output_config(TIMER2, TIMER_CH_0, &timer_ocintpara);
+  timer_channel_output_config(TIMER2, TIMER_CH_1, &timer_ocintpara);
+  timer_channel_output_config(TIMER2, TIMER_CH_2, &timer_ocintpara);
+  timer_channel_output_config(TIMER2, TIMER_CH_3, &timer_ocintpara);
 
-  timer_channel_output_pulse_value_config(TIMER2, timer_ch, 0);
-  timer_channel_output_mode_config(TIMER2, timer_ch, TIMER_OC_MODE_PWM0);
-  timer_channel_output_shadow_config(TIMER2, timer_ch, TIMER_OC_SHADOW_DISABLE);
+  timer_channel_output_pulse_value_config(TIMER2, TIMER_CH_0, 0);
+  timer_channel_output_mode_config(TIMER2, TIMER_CH_0, TIMER_OC_MODE_PWM0);
+  timer_channel_output_shadow_config(TIMER2, TIMER_CH_0, TIMER_OC_SHADOW_DISABLE);
 
-  /* TIMER2 primary output enable */
-  // timer_primary_output_config(TIMER2, ENABLE);
+  timer_channel_output_pulse_value_config(TIMER2, TIMER_CH_1, 0);
+  timer_channel_output_mode_config(TIMER2, TIMER_CH_1, TIMER_OC_MODE_PWM0);
+  timer_channel_output_shadow_config(TIMER2, TIMER_CH_1, TIMER_OC_SHADOW_DISABLE);
 
-  /* TIMER2 update DMA request enable */
+  timer_channel_output_pulse_value_config(TIMER2, TIMER_CH_2, 0);
+  timer_channel_output_mode_config(TIMER2, TIMER_CH_2, TIMER_OC_MODE_PWM0);
+  timer_channel_output_shadow_config(TIMER2, TIMER_CH_2, TIMER_OC_SHADOW_DISABLE);
+
+  timer_channel_output_pulse_value_config(TIMER2, TIMER_CH_3, 0);
+  timer_channel_output_mode_config(TIMER2, TIMER_CH_3, TIMER_OC_MODE_PWM0);
+  timer_channel_output_shadow_config(TIMER2, TIMER_CH_3, TIMER_OC_SHADOW_DISABLE);
+
+
+
+
   timer_dma_enable(TIMER2, TIMER_DMA_UPD);
 
   /* auto-reload preload enable */
@@ -165,6 +178,20 @@ void set_color(uint8_t led, uint8_t r, uint8_t g, uint8_t b) {
 */
 int main(void) {
   gpio_config();
+  timer_conf();
+
+    for (uint32_t x = 0; x < 2000000; x++)
+      ;
+
+  set_color(0, 0, 100, 0);
+  set_color(1, 0, 0, 100);
+  set_color(2, 100, 0, 0);
+  set_color(3, 0, 100, 0);
+  set_color(4, 0, 0, 100);
+  dma_send_buf((uint16_t *)buffer, LENGHT_TRANSMIT + 1, TIMER_CH_2);
+
+    for (uint32_t x = 0; x < 2000000; x++)
+    ;
 
   while (1) {
     set_color(0, 100, 100, 100);
