d20449c36cdfa21e6adec747fe3359526e3d881a
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-06-16 15:20:21 +0300

    fix rs485 transmission

--- src/Feature/RS485/RBUS/Rx.hs
+++ src/Feature/RS485/RBUS/Rx.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE RecordWildCards    #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use for_" #-}
-{-# LANGUAGE NumericUnderscores #-}
 
 module Feature.RS485.RBUS.Rx where
 
@@ -34,22 +34,23 @@ rxTask RBUS{..} = do
             {-
                 TODO: handle 16 bit values
             -}
-            rsSize'   <- deref rsSize
-            t0        <- deref rxTimestamp
-            t1        <- getSystemTime clock
-            -- baudrate' <- deref baudrate
-            -- let dt     = 40_000 ./ baudrate' + 1 -- wait 4 bytes timeout
-            pop rxQueue $ \i -> do
-                store (rsBuff ! toIx rsSize') . castDefault =<< deref (rxBuff ! toIx i)
-                store rsSize $ rsSize' + 1
-            when (rsSize' >? 0 .&& t1 - t0 >? 1) $ do
-                lazyTransmit transport $ \transmit -> do
-                    transmit $ rsSize' + 2
-                    transmit 0xa2
-                    transmit $ fromIntegral index
-                    for (toIx rsSize') $ \ix ->
-                        transmit . castDefault =<< deref (rsBuff ! ix)
-                store rsSize 0
+            baudrate' <- deref baudrate
+            when (baudrate' >? 0) $ do
+                rsSize'   <- deref rsSize
+                t0        <- deref rxTimestamp
+                t1        <- getSystemTime clock
+                let dt     = 40_000 ./ baudrate' + 1 -- wait 4 bytes timeout
+                pop rxQueue $ \i -> do
+                    store (rsBuff ! toIx rsSize') . castDefault =<< deref (rxBuff ! toIx i)
+                    store rsSize $ rsSize' + 1
+                when (rsSize' >? 0 .&& t1 - t0 >? dt) $ do
+                    lazyTransmit transport $ \transmit -> do
+                        transmit $ rsSize' + 2
+                        transmit 0xa2
+                        transmit $ fromIntegral index
+                        for (toIx rsSize') $ \ix ->
+                            transmit . castDefault =<< deref (rsBuff ! ix)
+                    store rsSize 0
         )
 
 
@@ -58,9 +59,10 @@ rxTask RBUS{..} = do
 --}
 resetTask :: RBUS -> Ivory eff ()
 resetTask RBUS{..} = do
-    t0 <- deref rxTimestamp
-    t1 <- getSystemTime clock
-    when (t1 - t0 >? 1) $ do
+    isRBUS' <- deref isRBUS
+    t0      <- deref rxTimestamp
+    t1      <- getSystemTime clock
+    when (isRBUS' .&& t1 - t0 >? 1) $ do
         Q.clear rxQueue
         reset   protocol
         store   rxLock false
