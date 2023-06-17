62e18f64a52e9e6a91ed04f082d6e8c2d633f095
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-06-17 17:42:59 +0300

    read a data  on the error too

--- src/Device/GD32F4xx/UART.hs
+++ src/Device/GD32F4xx/UART.hs
@@ -112,17 +112,17 @@ handleReceive :: USART_PERIPH -> (Uint16 -> Ivory eff ()) -> Ivory eff ()
 handleReceive uart onReceive = do
     rbne  <- getInterruptFlag   uart usart_int_flag_rbne
     when rbne $ do
-        clearInterruptFlag      uart usart_int_flag_rbne
-        ferr         <- getFlag uart usart_flag_ferr
-        nerr         <- getFlag uart usart_flag_nerr
-        orerr        <- getFlag uart usart_flag_orerr
-        perr         <- getFlag uart usart_flag_perr
-        clearFlag               uart usart_flag_ferr
-        clearFlag               uart usart_flag_nerr
-        clearFlag               uart usart_flag_orerr
-        clearFlag               uart usart_flag_perr
-        when (iNot $ ferr .|| nerr .|| orerr .|| perr) $
-            onReceive =<< S.receiveData uart
+        clearInterruptFlag     uart usart_int_flag_rbne
+        ferr        <- getFlag uart usart_flag_ferr
+        nerr        <- getFlag uart usart_flag_nerr
+        orerr       <- getFlag uart usart_flag_orerr
+        perr        <- getFlag uart usart_flag_perr
+        clearFlag              uart usart_flag_ferr
+        clearFlag              uart usart_flag_nerr
+        clearFlag              uart usart_flag_orerr
+        clearFlag              uart usart_flag_perr
+        value <- S.receiveData uart
+        when (iNot $ ferr .|| nerr .|| orerr .|| perr) $ onReceive value
 
 handleDrain :: USART_PERIPH -> Ivory eff () -> Ivory eff ()
 handleDrain uart onDrain = do
