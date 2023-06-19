c4fc856be6c26b9e68d644a7572fe0a975c84df3
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-06-19 12:18:56 +0300

    naming

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,7 @@ import           Formula.Blink450
 import           Formula.DimmerAC12
 import           Formula.DimmerDC12
 import           Formula.Echo
-import           Formula.MixA6x6
+import           Formula.Mix6x12
 import           Formula.Relay12
 import           Formula.Server
 
@@ -21,7 +21,7 @@ main = do
     gcc gd32f330k8u6 [ relay12
                      , dimmerDC12
                      , dimmerAC12
-                     , mixA6x6
+                     , mix6x12
                     --  , blink330
                      ]
     gcc gd32f450vgt6 [ server
