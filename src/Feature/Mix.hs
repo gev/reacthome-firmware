1e3f70961810357d3ae7deccaa4f610fb4ebd9c6
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-06-26 00:34:00 +0300

    implement Mix feature logic

--- src/Feature/Mix.hs
+++ src/Feature/Mix.hs
@@ -1,30 +1,51 @@
+{-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns   #-}
+{-# LANGUAGE RankNTypes       #-}
 {-# LANGUAGE RecordWildCards  #-}
 
 module Feature.Mix where
 
-import           Control.Monad.Reader
-import           Control.Monad.Writer
+import           Control.Monad               (zipWithM_)
+import           Control.Monad.Reader        (MonadReader, asks)
+import           Control.Monad.Writer        (MonadWriter)
 import           Core.Context
 import           Core.Controller
-import           Core.Domain
+import           Core.Domain                 as D
 import           Core.Feature
 import           Core.Task
 import           Core.Transport
-import           Feature.DInputs       (DInputs, manageDInputs, mkDInputs,
-                                        syncDInputs)
-import           Feature.Relays        (Relays, manageRelays, mkRelays,
-                                        syncRelays)
+import qualified Core.Transport              as T
+import           Data.Buffer
+import           Data.Matrix
+import           Data.Serialize
+import           Data.Value
+import qualified Endpoint.DInputs            as DI
+import           Endpoint.DInputsRelaysRules
+import           Feature.DInputs             (DInputs (DInputs, getDInputs, getInputs),
+                                              manageDInputs, mkDInputs,
+                                              syncDInputs)
+import           Feature.Relays              (Relays (Relays), manageRelays,
+                                              mkRelays, offRelay, onDo, onGroup,
+                                              onInit, onRelay', syncRelays,
+                                              toggleRelay)
+import           GHC.TypeNats
 import           Interface.GPIO.Input
 import           Interface.GPIO.Output
 import           Ivory.Language
+import           Ivory.Stdlib
 
 
 
 data Mix = Mix
-    { relays  :: Relays
-    , dinputs :: DInputs
+    { relaysN    :: Int
+    , relays     :: Relays
+    , dinputs    :: DInputs
+    , dinputsN   :: Int
+    , rules      :: Rules
+    , shouldInit :: Value IBool
+    , transmit    :: forall n. KnownNat n
+                  => Buffer n Uint8 -> forall s. Ivory (ProcEffects s ()) ()
     }
 
 
@@ -34,19 +55,55 @@ mix :: ( MonadWriter Context m
        , Transport t, Output o, Input i
        ) => [p -> m i] -> [p -> m o] -> m Feature
 mix inputs outputs = do
-    relays  <- mkRelays  outputs
-    dinputs <- mkDInputs inputs
-    let mix  = Mix {relays, dinputs}
-    addTask  $ delay 10 "mix_manage" $ manage mix
-    addTask  $ yeld     "mix_sync"   $ sync   mix
-    pure $ Feature mix
+    relays       <- mkRelays outputs
+    let relaysN   = length outputs
+    dinputs      <- mkDInputs inputs
+    let dinputsN  = length inputs
+    rules        <- mkRules dinputsN relaysN
+    transport    <- asks D.transport
+    shouldInit   <- asks D.shouldInit
+    let mix       = Mix { relays
+                        , relaysN
+                        , dinputs
+                        , dinputsN
+                        , rules
+                        , shouldInit
+                        , transmit = T.transmitBuffer transport
+                        }
+    addTask $ delay 10 "mix_manage" $ manage mix
+    addTask $ yeld     "mix_sync"   $ sync   mix
+    pure    $ Feature mix
 
 
 
-manage :: Mix -> Ivory eff ()
+manage :: Mix -> Ivory ('Effects (Returns ()) r (Scope s)) ()
 manage Mix{..} = do
-    manageDInputs dinputs
-    manageRelays  relays
+    manageDInputs  dinputs
+    manageMixRules rules dinputs relays dinputsN
+    manageRelays   relays
+
+
+
+manageMixRules :: Rules -> DInputs -> Relays -> Int -> Ivory ('Effects (Returns ()) r (Scope s)) ()
+manageMixRules Rules{..} DInputs{..} relays n = zipWithM_ zip (reverse getInputs) [n, n-1 .. 1]
+    where
+        zip :: Input i => i -> Int -> Ivory ('Effects (Returns ()) r (Scope s)) ()
+        zip input i = DI.runDInputs getDInputs $ \dis -> do
+            let ix = i - 1
+            let di = addrOf dis ! fromIntegral ix
+            let run :: RunMatrix Uint8 -> Ivory ('Effects (Returns ()) r (Scope s)) ()
+                run runRules = runRules $ \rules -> arrayMap $ \jx -> do
+                    r <- deref (addrOf rules ! fromIntegral ix ! jx)
+                    cond_ [ r ==? 0 ==> offRelay relays (toIx . fromIx $ jx + 1)
+                          , r ==? 1 ==> onRelay' relays (toIx . fromIx $ jx + 1)
+                          , r ==? 2 ==> do
+                                    changed <- iNot <$> deref (di ~> DI.synced)
+                                    when changed $ toggleRelay relays (toIx . fromIx $ jx + 1)
+                          ]
+            state' <- deref $ di ~> DI.state
+            ifte_ state'
+                (run runRulesOn )
+                (run runRulesOff)
 
 
 
@@ -58,4 +115,34 @@ sync Mix{..} = do
 
 
 instance Controller Mix where
-    handle  Mix {..} = handle relays
+    handle  mix@Mix{..} buff size = do
+        shouldInit' <- deref shouldInit
+        pure [ size >=? 3 ==> do
+                action <- deref $ buff ! 0
+                cond_ [ iNot shouldInit' ==> cond_
+                      [ action ==? 0x00  ==> onDo    relays buff size
+                      , action ==? 0x02  ==> onGroup relays buff size
+                      , action ==? 0x03  ==> onRule  mix    buff size
+                      ]
+                      , action ==? 0xf2  ==> onInit  relays buff size
+                      ]
+             ]
+
+
+
+onRule :: KnownNat n => Mix -> Buffer n Uint8 -> Uint8 -> Ivory (ProcEffects s ()) ()
+onRule Mix{..} buff size = do
+    let relaysN'  = fromIntegral relaysN
+    let dinputsN' = fromIntegral dinputsN
+    i <- subtract 1 <$> deref (buff ! 1)
+    when (size ==? 2 + 2 * relaysN' .&& i <? dinputsN') $ do
+        kx <- local $ ival 2
+        let run :: (Rules -> RunMatrix Uint8) -> Ivory eff ()
+            run runRules = runRules rules $ \rs -> arrayMap $ \jx -> do
+                kx' <- deref kx
+                store (addrOf rs ! toIx i ! jx) =<< unpack buff kx'
+                store kx $ kx' + 1
+        run runRulesOff
+        run runRulesOn
+        fillPayload rules i
+        runPayload rules $ \p -> transmit . addrOf $ p
