1c331aa290b5232763681b4544bd14155a0f1aba
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-07-06 03:52:46 +0300

    up

--- src/Endpoint/ATS.hs
+++ src/Endpoint/ATS.hs
@@ -5,36 +5,79 @@
 
 module Endpoint.ATS where
 
-import           Control.Monad.Writer (MonadWriter)
+import           Control.Monad.Reader  (MonadReader, asks)
+import           Control.Monad.Writer  (MonadWriter)
 import           Core.Context
+import           Core.Domain           (Domain (mcu))
+import qualified Core.Domain           as D
 import           Data.Buffer
+import           Data.Record
 import           Data.Value
-import           Endpoint.DInputs
-import           Endpoint.Relays
+import           Endpoint.DInputs      as DI
+import           Endpoint.Relays       as R
+import           Interface.MCU
+import           Interface.SystemClock (SystemClock, getSystemTime)
 import           Ivory.Language
 import           Ivory.Stdlib
 
 
 
-modeNone  = 0x00 :: Uint8
-mode_N1_G = 0x11 :: Uint8
-mode_N2   = 0x20 :: Uint8
-mode_N2_G = 0x21 :: Uint8
+modeNone        = 0x00 :: Uint8
+mode_N1_G       = 0x11 :: Uint8
+mode_N2         = 0x20 :: Uint8
+mode_N2_G       = 0x21 :: Uint8
+
+
+stateOff        = 0x00 :: Uint8
+stateOn         = 0x01 :: Uint8
+
+stateTurningOff = 0x10 :: Uint8
+stateTurningOn  = 0x11 :: Uint8
+
+stateError      = 0xff :: Uint8
+
+maxAttempt      = 3    :: Uint8
 
 
 
 data ATS = ATS
-    { mode    :: Value Uint8
-    , payload :: Buffer 2 Uint8
+    { mode            :: Value    Uint8
+    , state           :: Value    Uint8
+    , currentSource   :: Value    Uint8
+    , selectedSource  :: Value    Uint8
+    , relayStatus     :: Values 3 IBool
+    , generatorStatus :: Value    IBool
+    , attempt         :: Value    Uint8
+    , timestamp       :: Value    Uint32
+    , clock           :: SystemClock
+    , payload         :: Buffer 2 Uint8
     }
 
 
-
-mkATS :: (MonadWriter Context m) => m ATS
+mkATS :: (MonadWriter Context m, MonadReader (Domain p t) m) => m ATS
 mkATS = do
-    mode <- value "ats_mode" modeNone
-    payload <- buffer "ats_payload"
-    pure ATS {mode, payload}
+    mcu             <- asks D.mcu
+    let clock        = systemClock mcu
+    mode            <- value  "ats_mode"             modeNone
+    state           <- value  "ats_state"            stateOff
+    currentSource   <- value  "ats_current_source"   0
+    selectedSource  <- value  "ats_selected_source"  0
+    relayStatus     <- values "ats_relay_status"     [true, true, true]
+    generatorStatus <- value  "ats_generator_status" true
+    attempt         <- value  "ats_attempt"          0
+    timestamp       <- value  "ats_timestamp"        0
+    payload         <- buffer "ats_payload"
+    pure ATS { mode
+             , state
+             , currentSource
+             , selectedSource
+             , relayStatus
+             , generatorStatus
+             , attempt
+             , timestamp
+             , clock
+             , payload
+             }
 
 
 
@@ -47,9 +90,104 @@ message ATS{..} = do
 
 
 manageATS :: ATS -> DInputs -> Relays -> Ivory eff ()
-manageATS ATS{..} inputs relays = do
-    mode' <- deref mode
-    cond_ [ mode' ==? mode_N1_G ==> pure ()
-          , mode' ==? mode_N2   ==> pure ()
-          , mode' ==? mode_N2_G ==> pure ()
-          ]
+manageATS a@ATS{..} DInputs{runDInputs} Relays{runRelays} =
+    runDInputs $ \di -> runRelays $ \r -> do
+        let di'  = addrOf di
+        let r'   = addrOf r
+        mode'   <- deref mode
+        store selectedSource 0
+        cond_ [ mode' ==? mode_N1_G ==> do manageLine      2 a (di' ! 0) (di' ! 1) (r' ! 0)
+                                           manageGenerator 1 a (di' ! 2) (di' ! 3) (r' ! 1) (r' ! 2)
+                                           manageReset       a (di' ! 4)
+
+              , mode' ==? mode_N2   ==> do manageLine      2 a (di' ! 0) (di' ! 1) (r' ! 0)
+                                           manageLine      1 a (di' ! 2) (di' ! 3) (r' ! 1)
+                                           manageReset       a (di' ! 4)
+
+              , mode' ==? mode_N2_G ==> do manageLine      3 a (di' ! 0) (di' ! 1) (r' ! 0)
+                                           manageLine      2 a (di' ! 2) (di' ! 3) (r' ! 1)
+                                           manageGenerator 1 a (di' ! 4) (di' ! 5) (r' ! 2) (r' ! 3)
+                                           manageReset       a (di' ! 6)
+              ]
+
+
+
+manageLine :: Uint8
+           -> ATS
+           -> Record DInputStruct
+           -> Record DInputStruct
+           -> Record RelayStruct
+           ->  Ivory eff ()
+manageLine n ATS{..} hasVoltage isRelayOn relay = do
+    state' <- deref state
+    ifte_ (state' ==? stateError)
+          (turnOff relay)
+          (do
+                hasVoltage'     <- deref $ hasVoltage ~> DI.state
+                isRelayOn'      <- deref $ isRelayOn ~> DI.state
+                relayState'     <- deref $ relay ~> R.state
+                ifte_ hasVoltage'
+                    (do
+                        selectedSource' <- deref selectedSource
+                        ifte_ (n >? selectedSource')
+                              (do
+                                    -- when relayState' $ store currentSource n
+                                    -- currentSource' <- deref currentSource
+                                    ifte_ (iNot relayState')
+                                        (do
+                                            store (relay ~> R.timestamp) =<< getSystemTime clock
+                                            turnOn' relay
+                                        )
+                                        (turnOn  relay)
+                                    store selectedSource n
+                              )
+                              (turnOff relay)
+                    )
+                    (turnOff relay)
+          )
+
+
+
+manageGenerator :: Int
+                -> ATS
+                -> Record DInputStruct
+                -> Record DInputStruct
+                -> Record RelayStruct
+                -> Record RelayStruct
+                ->  Ivory eff ()
+manageGenerator n ATS{..} hasVoltage isRelayOn relay start = do
+    state' <- deref state
+    when (state' ==? stateError) $ do
+        pure ()
+
+
+
+manageReset :: ATS
+            -> Record DInputStruct
+            ->  Ivory eff ()
+manageReset ATS{..} reset = do
+    state' <- deref state
+    when (state' ==? stateError) $ do
+        pure ()
+
+
+
+turnOn :: Record RelayStruct -> Ivory eff ()
+turnOn relay = do
+    store (relay ~> R.state   ) true
+    store (relay ~> R.delayOff) 0
+    store (relay ~> R.delayOn ) 0
+
+
+turnOn' :: Record RelayStruct -> Ivory eff ()
+turnOn' relay = do
+    store (relay ~> R.state   ) false
+    store (relay ~> R.delayOff) 0
+    store (relay ~> R.delayOn ) 1000
+
+
+turnOff :: Record RelayStruct -> Ivory eff ()
+turnOff relay = do
+    store (relay ~> R.state   ) false
+    store (relay ~> R.delayOff) 0
+    store (relay ~> R.delayOn ) 0
