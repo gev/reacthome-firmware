06d858b69430d410231dac8f3b805c10a64d51e5
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-09-05 22:47:41 +0300

    Revert "make Input and Output instances for the OpenDrain"
    This reverts commit 9f9f4d31dedbedeed5c840b5581facf7502df9d2.

--- src/Device/GD32F3x0/GPIO/OpenDrain.hs
+++ src/Device/GD32F3x0/GPIO/OpenDrain.hs
@@ -6,9 +6,7 @@ module Device.GD32F3x0.GPIO.OpenDrain where
 import           Control.Monad.State
 import           Core.Context
 import           Device.GD32F3x0.GPIO
-import qualified Interface.GPIO.Input         as I
-import qualified Interface.GPIO.Output        as O
-import qualified Interface.GPIO.OpenDrain        as OD
+import qualified Interface.GPIO.OpenDrain     as I
 import           Ivory.Language.Module
 import           Support.Device.GD32F3x0.GPIO as S
 
@@ -23,17 +21,7 @@ openDrain p = do
     pure $ OpenDrain port
 
 
-instance OD.OpenDrain OpenDrain where
-    OD.set   (OpenDrain Port{..}) = S.setBit       gpio pin
-    OD.get   (OpenDrain Port{..}) = S.getInputBit  gpio pin
-    OD.reset (OpenDrain Port{..}) = S.resetBit     gpio pin
-
-
-instance I.Input OpenDrain where
-    I.get  = I.get . getOpenDrain  
-
-
-instance O.Output OpenDrain where
-    O.set    = O.set   . getOpenDrain
-    O.get    = O.get   . getOpenDrain
-    O.reset  = O.reset . getOpenDrain
+instance I.OpenDrain OpenDrain where
+    set   (OpenDrain Port{..}) = S.setBit       gpio pin
+    get   (OpenDrain Port{..}) = S.getInputBit  gpio pin
+    reset (OpenDrain Port{..}) = S.resetBit     gpio pin
