89a9cdb782daa97e90238e6b0883c804ef12708a
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-09-13 18:01:20 +0300

    up

--- src/Device/GD32F3x0/OneWire.hs
+++ src/Device/GD32F3x0/OneWire.hs
@@ -6,7 +6,7 @@
 
 module Device.GD32F3x0.OneWire where
 
-import           Control.Monad.State
+import           Control.Monad.State            (MonadState)
 import           Core.Context
 import           Core.Handler
 import           Data.Buffer
@@ -16,11 +16,12 @@ import           Device.GD32F3x0.GPIO.Mode
 import           Device.GD32F3x0.GPIO.OpenDrain
 import           Device.GD32F3x0.GPIO.Port
 import           Device.GD32F3x0.Timer          (Timer)
-import qualified Interface.OneWire              as I
-import           Ivory.Language
-import qualified Interface.Timer                as I
-import           Support.CMSIS.CoreCM4
 import qualified Interface.GPIO.OpenDrain       as I
+import qualified Interface.OneWire              as I
+import qualified Interface.Timer                as I
+import           Ivory.Language
+import           Ivory.Stdlib
+import           Support.CMSIS.CoreCM4
 
 
 
@@ -40,15 +41,24 @@ mkOneWire :: MonadState Context m
          -> m OneWire
 mkOneWire cfg od = do
     port  <- od
-    timer <- cfg 1_000_000 1
+    timer <- cfg 2_000_000 10
     tmpB  <- buffer "one_wire_tmp"
     tmpQ  <- queue  "one_wire_tmp"
     tmpV  <- value_ "one_wire_tmp_value"
-    count <- value  "one_wire_count" 0
+    count <- value  "one_wire_count" 8
     let handlerOW = do
-                    I.set port
-                    nop 10
-                    I.reset port
+            count' <- deref count
+            cond_ [ count' ==? 8 
+                           ==> pop tmpQ (\i -> do store tmpV =<< deref (tmpB ! toIx i)
+                                                  store count 0)
+                  , count' <? 8 
+                           ==> do tmpV' <- deref tmpV
+                                  let bit = (tmpV' `iShiftR` count') .& 1
+                                  ifte_ (bit ==? 1)
+                                      (I.set   port)
+                                      (I.reset port)
+                                  store count $ count' + 1
+                  ]
     addHandler $ I.HandleTimer timer handlerOW
     pure $ OneWire { port, timer, tmpB, tmpQ, tmpV, count }
 
@@ -58,7 +68,7 @@ instance I.OneWire OneWire where
     read _ = pure 0
 
     write OneWire {tmpB, tmpQ} v =
-        push tmpQ $ \ix -> store (tmpB ! toIx ix) v
+        push tmpQ $ \i -> store (tmpB ! toIx i) v
 
 
 
