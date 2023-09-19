4c1a290b69eb536e15401bcd17fd19b3df31ad7c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-09-19 12:40:38 +0300

    add OneWire
    Co-authored-by: Dmitry Tyazhlov <DmitryTyazhlov@users.noreply.github.com>

--- formula/Formula/DI4.hs
+++ formula/Formula/DI4.hs
@@ -3,6 +3,7 @@ module Formula.DI4 where
 import           Core.Formula
 import           Device.GD32F3x0
 import           Feature.DInputs
+import           Feature.TestOneWire  (testOneWire)
 import           Interface.RS485
 import           Ivory.Language
 import           Transport.RS485.RBUS
@@ -13,10 +14,11 @@ di4 = Formula { name       = "di4"
               , version    = (4, 2)
               , shouldInit = false
               , transport  = rbus $ rs485 1 uart_1 out_pa_4
-              , features   = [dinputs [  in_pa_12
-                                      ,  in_pa_11
-                                      ,  in_pa_10
-                                      ,  in_pa_9
-                                      ]  
-                          ]
+              , features   = [ dinputs [  in_pa_12
+                                       ,  in_pa_11
+                                       ,  in_pa_10
+                                       ,  in_pa_9
+                                       ]
+                             , testOneWire ow_0 od_pa_8
+                             ]
               }
