5acdfc28b1cdec8a3f859de71be33cac45def0d9
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-09-27 19:07:10 +0300

    combine all master OneWire logic
    Co-authored-by: Dmitry Tyazhlov <DmitryTyazhlov@users.noreply.github.com>

--- src/Feature/DS18B20.hs
+++ src/Feature/DS18B20.hs
@@ -1,34 +1,34 @@
-{-# LANGUAGE DataKinds        #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs            #-}
-{-# LANGUAGE NamedFieldPuns   #-}
-{-# LANGUAGE RankNTypes       #-}
-{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE FlexibleContexts   #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE NamedFieldPuns     #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE RankNTypes         #-}
+{-# LANGUAGE RecordWildCards    #-}
 
 module Feature.DS18B20 where
 
-import           Control.Monad                  (replicateM_)
-import           Control.Monad.Reader           (MonadReader, asks)
-import           Control.Monad.State            (MonadState)
+import           Control.Monad            (replicateM_)
+import           Control.Monad.Reader     (MonadReader, asks)
+import           Control.Monad.State      (MonadState)
 import           Core.Context
 import           Core.Controller
-import qualified Core.Domain                    as D
+import qualified Core.Domain              as D
 import           Core.Feature
 import           Core.Task
-import qualified Core.Transport                 as T
+import qualified Core.Transport           as T
 import           Data.Buffer
 import           Data.Serialize
 import           Data.Value
 import           GHC.TypeNats
-import           Interface.GPIO.OpenDrain       (OpenDrain)
-import           Interface.MCU                  (peripherals)
+import           Interface.GPIO.OpenDrain (OpenDrain)
+import           Interface.MCU            (peripherals)
 import           Interface.OneWire
 import           Ivory.Language
-import           Ivory.Language.Uint            (Uint16 (Uint16))
+import           Ivory.Language.Uint      (Uint16 (Uint16))
 import           Ivory.Stdlib
-import           Prelude                        hiding (read)
--- import           Protocol.OneWire.Master
-import           Protocol.OneWire.Master.Search
+import           Prelude                  hiding (read)
+import           Protocol.OneWire.Master
 
 
 
@@ -59,55 +59,54 @@ ds18b20 ow od = do
                          , transmit = T.transmitBuffer transport
                          }
     onewire   <- ow mcu $ od mcu
-    -- master    <- mkOneWireMaster onewire (onData ds) (onError ds)
-    s    <- mkSearch        onewire (onData ds) (onError ds)
+    master    <- mkOneWireMaster onewire (onData ds) (onDiscovery ds) (onError ds)
 
 
-    addTask $ delay      10000     (name <> "_search") $ search s
-    -- addTask $ delay      10000     (name <> "_measure_temperature") $ measureTemperature master
-    -- addTask $ delayPhase 10000 700 (name <> "_get_temperature"    ) $ getTemperature     master
+    addTask $ delay      10_000       (name <> "_search"             ) $ search             master
+    addTask $ delayPhase 10_000 3_000 (name <> "_measure_temperature") $ measureTemperature master
+    addTask $ delayPhase 10_000 3_700 (name <> "_get_temperature"    ) $ getTemperature     master
 
     pure $ Feature ds
 
--- measureTemperature :: OneWireMaster -> Ivory eff ()
--- measureTemperature onewire = do
---     reset    onewire
---     skipROM  onewire
---     write    onewire 0x44
+measureTemperature :: OneWireMaster -> Ivory eff ()
+measureTemperature onewire = do
+    reset    onewire
+    skipROM  onewire
+    write    onewire 0x44
 
 
--- getTemperature :: OneWireMaster -> Ivory eff ()
--- getTemperature onewire = do
---     reset    onewire
---     skipROM  onewire
---     write    onewire 0xbe
---     replicateM_ 9 $ read onewire
+getTemperature :: OneWireMaster -> Ivory eff ()
+getTemperature onewire = do
+    reset    onewire
+    skipROM  onewire
+    write    onewire 0xbe
+    replicateM_ 9 $ read onewire
 
 
 
+onData :: DS18B20 -> Uint8 -> Ivory (ProcEffects s ()) ()
+onData DS18B20{..} v = do
+    count' <- deref count
+    store (rxB ! toIx count') v
+    ifte_ (count' ==? 8)
+          (do
+            {-
+                TODO: Check CRC
+            -}
+            store count 0
+            raw <- unpackLE rxB 0
+            let t = (25 * raw) `iDiv` 4 :: Sint16
+            packLE txB 9 t
+            transmit txB
+          )
+          (store count $ count' + 1)
 
--- onData :: DS18B20 -> Uint8 -> Ivory (ProcEffects s ()) ()
--- onData DS18B20{..} v = do
---     count' <- deref count
---     store (rxB ! toIx count') v
---     ifte_ (count' ==? 8)
---           (do
---             {-
---                 TODO: Check CRC
---             -}
---             store count 0
---             raw <- unpackLE rxB 0
---             let t = (25 * raw) `iDiv` 4 :: Sint16
---             packLE txB 9 t
---             transmit txB
---           )
---           (store count $ count' + 1)
 
 
-onData :: DS18B20 -> Uint8 -> Buffer 8 Uint8 -> Ivory (ProcEffects s ()) ()
-onData _ _ _ = pure ()
+onDiscovery :: DS18B20 -> Uint8 -> Buffer 8 Uint8 -> Ivory (ProcEffects s ()) ()
+onDiscovery _ _ _ = pure ()
 
-onError :: DS18B20 -> Uint8 -> Ivory eff ()
+onError :: DS18B20 -> Uint8 -> Ivory (ProcEffects s ()) ()
 onError _ _ = pure ()
 
 
