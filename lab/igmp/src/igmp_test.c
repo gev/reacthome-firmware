544fc60f18bbc2fc2f8b7044b3607d0855290358
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-10-25 19:19:53 +0300

    up
    Co-authored-by: Evgeny Gazdovsky <gazdovsky@gmail.com>

--- lab/igmp/src/igmp_test.c
+++ lab/igmp/src/igmp_test.c
@@ -9,14 +9,14 @@ void igmp_echoserver_receive_callback(void *arg, struct udp_pcb *upcb, struct pb
 {
   printf("udp_callback\n");
 
-//   /* Connect to the remote client */
-//   udp_connect(upcb, addr, port);
+  /* Connect to the remote client */
+  udp_connect(upcb, addr, port);
 
-//   /* Tell the client that we have accepted it */
-//   udp_send(upcb, p);
+  /* Tell the client that we have accepted it */
+  udp_send(upcb, p);
 
-//   /* free the UDP connection, so we can accept new clients */
-//   udp_disconnect(upcb);
+  /* free the UDP connection, so we can accept new clients */
+  udp_disconnect(upcb);
 	
   /* Free the p buffer */
   pbuf_free(p);
@@ -25,18 +25,15 @@ void igmp_echoserver_receive_callback(void *arg, struct udp_pcb *upcb, struct pb
 
 void igmp_test_init(){
 
-// igmp_input(struct pbuf *p, struct netif *inp, const ip4_addr_t *dest)
   struct udp_pcb *upcb;
   upcb = udp_new();
   err_t err;
-  ip_addr_t igmp_group;
-  IP4_ADDR(&igmp_group, 235, 1, 1, 1);
   if (upcb)
    {
      /* Bind the upcb to the UDP_PORT port */
      /* Using IP_ADDR_ANY allow the upcb to be used by any local interface */
-      err = udp_bind(upcb, IP_ADDR_ANY, 23);
-      
+      err = udp_bind(upcb, IP_ADDR_ANY, 2000);
+
       if(err == ERR_OK)
       {
         /* Set a receive callback for the upcb */
