638b8d42986aaf8aac84b1b08db30e934102a8c3
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-10-27 18:45:54 +0300

    telnet up

--- lab/Telnet/src/gd32f4xx_enet_eval.c
+++ lab/Telnet/src/gd32f4xx_enet_eval.c
@@ -72,14 +72,10 @@ void enet_system_setup(void)
         }
     }
 
-#ifdef USE_ENET_INTERRUPT
+
     enet_interrupt_enable(ENET_DMA_INT_NIE);
     enet_interrupt_enable(ENET_DMA_INT_RIE);
-#endif /* USE_ENET_INTERRUPT */
 
-#ifdef SELECT_DESCRIPTORS_ENHANCED_MODE
-    enet_desc_select_enhanced_mode();
-#endif /* SELECT_DESCRIPTORS_ENHANCED_MODE */
 
     /* configure systick clock source as HCLK */
     systick_clksource_set(SYSTICK_CLKSOURCE_HCLK);
@@ -116,29 +112,19 @@ static void enet_mac_dma_config(void)
 //  enet_initpara_config(HALFDUPLEX_OPTION, ENET_CARRIERSENSE_ENABLE|ENET_RECEIVEOWN_ENABLE|ENET_RETRYTRANSMISSION_DISABLE|ENET_BACKOFFLIMIT_10|ENET_DEFERRALCHECK_DISABLE);
 //  enet_initpara_config(DMA_OPTION, ENET_FLUSH_RXFRAME_ENABLE|ENET_SECONDFRAME_OPT_ENABLE|ENET_NORMAL_DESCRIPTOR);
 
-#ifdef CHECKSUM_BY_HARDWARE
-    enet_init_status = enet_init(ENET_AUTO_NEGOTIATION, ENET_AUTOCHECKSUM_DROP_FAILFRAMES, ENET_BROADCAST_FRAMES_PASS);
-    // enet_init_status = enet_init(ENET_AUTO_NEGOTIATION, ENET_AUTOCHECKSUM_DROP_FAILFRAMES, ENET_RECEIVEALL);
-#else
     enet_init_status = enet_init(ENET_AUTO_NEGOTIATION, ENET_NO_AUTOCHECKSUM, ENET_BROADCAST_FRAMES_PASS);
-#endif /* CHECKSUM_BY_HARDWARE */
+
 
 }
 
-#ifdef USE_ENET_INTERRUPT
-/*!
-    \brief      configures the nested vectored interrupt controller
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
+
 static void nvic_configuration(void)
 {
     nvic_vector_table_set(NVIC_VECTTAB_FLASH, 0x0);
     nvic_priority_group_set(NVIC_PRIGROUP_PRE2_SUB2);
     nvic_irq_enable(ENET_IRQn, 0, 0);
 }
-#endif /* USE_ENET_INTERRUPT */
+
 
 /*!
     \brief      configures the different GPIO ports
@@ -163,119 +149,11 @@ static void enet_gpio_config(void)
     /* enable SYSCFG clock */
     rcu_periph_clock_enable(RCU_SYSCFG);
 
-#ifdef MII_MODE
 
-#ifdef PHY_CLOCK_MCO
-    /* output HXTAL clock (25MHz) on CKOUT0 pin(PA8) to clock the PHY */
-    rcu_ckout0_config(RCU_CKOUT0SRC_HXTAL, RCU_CKOUT0_DIV1);
-#endif /* PHY_CLOCK_MCO */
-
-    syscfg_enet_phy_interface_config(SYSCFG_ENET_PHY_MII);
-
-#elif defined RMII_MODE
     /* choose DIV2 to get 50MHz from 200MHz on CKOUT0 pin (PA8) to clock the PHY */
     // rcu_ckout0_config(RCU_CKOUT0SRC_PLLP, RCU_CKOUT0_DIV4);
     syscfg_enet_phy_interface_config(SYSCFG_ENET_PHY_RMII);
 
-#endif /* MII_MODE */
-
-#ifdef MII_MODE
-
-    /* PA1: ETH_MII_RX_CLK */
-    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_1);
-    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_1);
-
-    /* PA2: ETH_MDIO */
-    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_2);
-    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_2);
-
-    /* PA7: ETH_MII_RX_DV */
-    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_7);
-    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_7);
-
-    gpio_af_set(GPIOA, GPIO_AF_11, GPIO_PIN_1);
-    gpio_af_set(GPIOA, GPIO_AF_11, GPIO_PIN_2);
-    gpio_af_set(GPIOA, GPIO_AF_11, GPIO_PIN_7);
-
-    /* PG11: ETH_MII_TX_EN */
-    gpio_mode_set(GPIOG, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_11);
-    gpio_output_options_set(GPIOG, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_11);
-
-    /* PG13: ETH_MII_TXD0 */
-    gpio_mode_set(GPIOG, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_13);
-    gpio_output_options_set(GPIOG, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_13);
-
-    /* PG14: ETH_MII_TXD1 */
-    gpio_mode_set(GPIOG, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_14);
-    gpio_output_options_set(GPIOG, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_14);
-
-    gpio_af_set(GPIOG, GPIO_AF_11, GPIO_PIN_11);
-    gpio_af_set(GPIOG, GPIO_AF_11, GPIO_PIN_13);
-    gpio_af_set(GPIOG, GPIO_AF_11, GPIO_PIN_14);
-
-    /* PC1: ETH_MDC */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_1);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_1);
-
-    /* PC2: ETH_MII_TXD2 */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_2);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_2);
-
-    /* PC3: ETH_MII_TX_CLK */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_3);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_3);
-
-    /* PC4: ETH_MII_RXD0 */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_4);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_4);
-
-    /* PC5: ETH_MII_RXD1 */
-    gpio_mode_set(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_5);
-    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_5);
-
-    gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_1);
-    gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_2);
-    gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_3);
-    gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_4);
-    gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_5);
-
-    /* PH2: ETH_MII_CRS */
-    gpio_mode_set(GPIOH, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_2);
-    gpio_output_options_set(GPIOH, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_2);
-
-    /* PH3: ETH_MII_COL */
-    gpio_mode_set(GPIOH, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_3);
-    gpio_output_options_set(GPIOH, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_3);
-
-    /* PH6: ETH_MII_RXD2 */
-    gpio_mode_set(GPIOH, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_6);
-    gpio_output_options_set(GPIOH, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_6);
-
-    /* PH7: ETH_MII_RXD3 */
-    gpio_mode_set(GPIOH, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_7);
-    gpio_output_options_set(GPIOH, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_7);
-
-    gpio_af_set(GPIOH, GPIO_AF_11, GPIO_PIN_2);
-    gpio_af_set(GPIOH, GPIO_AF_11, GPIO_PIN_3);
-    gpio_af_set(GPIOH, GPIO_AF_11, GPIO_PIN_6);
-    gpio_af_set(GPIOH, GPIO_AF_11, GPIO_PIN_7);
-
-    /* PI8: ETH_INT */
-    gpio_mode_set(GPIOI, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_PIN_8);
-
-    /* PI10: ETH_MII_RX_ER */
-    gpio_mode_set(GPIOI, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_10);
-    gpio_output_options_set(GPIOI, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_10);
-
-    gpio_af_set(GPIOI, GPIO_AF_11, GPIO_PIN_10);
-
-    /* PB8: ETH_MII_TXD3 */
-    gpio_mode_set(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_8);
-    gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_MAX, GPIO_PIN_8);
-
-    gpio_af_set(GPIOB, GPIO_AF_11, GPIO_PIN_8);
-
-#elif defined RMII_MODE
 
     /* PA1: ETH_RMII_REF_CLK */
     gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_1);
@@ -324,6 +202,5 @@ static void enet_gpio_config(void)
     gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_1);
     gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_4);
     gpio_af_set(GPIOC, GPIO_AF_11, GPIO_PIN_5);
-#endif /* MII_MODE */
 
 }
