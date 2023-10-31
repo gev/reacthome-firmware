80efe83934b883b90a87a9425d7eda1e0f6e0e55
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-10-31 15:06:54 +0300

    update the modules graph script

--- script/graph.sh
+++ script/graph.sh
@@ -1,1 +1,8 @@
-find ./app/* ./formula ./src/* -name '*.hs' | xargs graphmod --no-cabal  > modules.dot 
+rm -rf graph
+go () {
+    mkdir -p $(dirname graph/$1)
+    find $1 -name '*.hs' | xargs graphmod --no-cabal > graph/$1.dot 
+}
+export -f go
+find app formula src -type d -exec bash -c 'go "$@"' bash {} ';'
+find ./app/* ./formula ./src/* -name '*.hs' | xargs graphmod --no-cabal  > graph/modules.dot 
