9cb36392dcf8c055ac18c6e11936f611164b09b6
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-11-09 12:41:43 +0300

    add ip_addr_any

--- src/Support/Lwip/IP_addr.hs
+++ src/Support/Lwip/IP_addr.hs
@@ -12,6 +12,9 @@ module Support.Lwip.IP_addr
     , inclIP_addr
     ) where
 
+
+
+
 import           Ivory.Language
 import           Ivory.Support
 import           Ivory.Language.Proc   (ProcType)
@@ -22,14 +25,22 @@ fun :: ProcType f => Sym -> Def f
 fun = funFrom "ip_addr.h"
 
 
+
 type IP_ADDR_4_T = "ip4_addr"
 type IP_ADDR_4 s = Ref s (Struct IP_ADDR_4_T)
 
 [ivory|
-    abstract struct ip4_addr "ip_addr.h"
+    struct ip4_addr 
+        { addr :: Stored Uint32 }
 |]
 
--- IP_ADDR_4 s $ ext "IP_ADDR_ANY"
+
+ip_addr_any :: [InitStruct  IP_ADDR_4_T]
+             -> Init (Struct IP_ADDR_4_T)
+ip_addr_any p =
+    istruct $ p <+> [ addr .= ival 0 ]
+
+
 
 createIpAddr4 :: IP_ADDR_4 s -> Uint8 -> Uint8 -> Uint8 -> Uint8 -> Ivory eff ()
 createIpAddr4 = call_ ip_addr4
