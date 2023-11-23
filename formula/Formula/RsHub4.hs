6c503567dd9b66db05bcbd9632bdefea7d885342
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-11-23 14:10:55 +0300

    stylish

--- formula/Formula/RsHub4.hs
+++ formula/Formula/RsHub4.hs
@@ -12,26 +12,26 @@ import           Transport.UART.RBUS as U
 
 
 rsHub4 :: Formula GD32F4xx
-rsHub4 = Formula { name       = "rs_hub4"
-                 , model      = 0xc0
-                 , version    = (3, 0)
-                 , shouldInit = true
-                 , transport  = U.rbus uart_1
-                 , features   = [ indicator npx_pwm_2 60
-                                , F.server [ rs485 1 uart_5 out_pb_14
-                                           , rs485 2 uart_3 out_pc_12
-                                           , rs485 3 uart_2 out_pb_15
-                                           , rs485 4 uart_0 out_pd_1
-                                           ]
-                                           [ pwm_0
-                                           , pwm_1
-                                           , pwm_2
-                                           ]
-                                           [ in_pa_8 
-                                           , in_pb_4 
-                                           , in_pb_10
-                                           , in_pa_15
-                                           ]
-                                , ds18b20 ow_0 od_pb_3
-                                ]
-                 }
+rsHub4 =  Formula { name       = "rs_hub4"
+                  , model      = 0xc0
+                  , version    = (3, 0)
+                  , shouldInit = true
+                  , transport  = U.rbus uart_1
+                  , features   = [ indicator npx_pwm_2 60
+                                 , F.server [ rs485 1 uart_5 out_pb_14
+                                            , rs485 2 uart_3 out_pc_12
+                                            , rs485 3 uart_2 out_pb_15
+                                            , rs485 4 uart_0 out_pd_1
+                                            ]
+                                            [ pwm_0
+                                            , pwm_1
+                                            , pwm_2
+                                            ]
+                                            [ in_pa_8
+                                            , in_pb_4
+                                            , in_pb_10
+                                            , in_pa_15
+                                            ]
+                                 , ds18b20 ow_0 od_pb_3
+                                 ]
+                  }
