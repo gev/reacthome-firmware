3c85fc64f4f71a874250066c6b7d02e0982bda97
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-12-06 11:52:56 +0300

    up

--- lab/gd32f330/save_data_after_poweroff/main.c
+++ lab/gd32f330/save_data_after_poweroff/main.c
@@ -4,15 +4,31 @@
 #include "sk6812.h"
 
 #define PAGE_ADDR 0x800fc00
+uint32_t flash_data;
 
 void leds_on(void);
+void set_pin_output(uint32_t port, uint32_t pin);
 
 int main(){
+  flash_data = *((uint32_t*)PAGE_ADDR) + 0x01010101;
   rcu_periph_clock_enable(RCU_GPIOA);
 
 	gpio_mode_set(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_11);
 	gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_11);
   gpio_bit_reset(GPIOA, GPIO_PIN_11);
+
+  set_pin_output(GPIOA, GPIO_PIN_0);
+  set_pin_output(GPIOA, GPIO_PIN_1);
+  set_pin_output(GPIOA, GPIO_PIN_2);
+  set_pin_output(GPIOA, GPIO_PIN_3);
+  set_pin_output(GPIOA, GPIO_PIN_6);
+  set_pin_output(GPIOA, GPIO_PIN_7);
+  set_pin_output(GPIOA, GPIO_PIN_8);
+  set_pin_output(GPIOA, GPIO_PIN_9);
+  set_pin_output(GPIOA, GPIO_PIN_10);
+  set_pin_output(GPIOA, GPIO_PIN_11);
+  set_pin_output(GPIOB, GPIO_PIN_0);
+  set_pin_output(GPIOB, GPIO_PIN_1);
   
   /* configure NVIC */
   nvic_priority_group_set(NVIC_PRIGROUP_PRE0_SUB4);
@@ -28,12 +44,7 @@ int main(){
 
   leds_on();
 
-    // erase page
-  // fmc_unlock();
-  // fmc_flag_clear(FMC_FLAG_END | FMC_FLAG_WPERR | FMC_FLAG_PGERR);
-  // fmc_page_erase(PAGE_ADDR);
-  // fmc_flag_clear(FMC_FLAG_END | FMC_FLAG_WPERR | FMC_FLAG_PGERR);
-  // fmc_lock();
+
 
   while(1){
 
@@ -51,13 +62,24 @@ void LVD_IRQHandler(void)
 {
     if(RESET != exti_interrupt_flag_get(EXTI_16)) {
         exti_interrupt_flag_clear(EXTI_16);
-        gpio_bit_set(GPIOA, GPIO_PIN_11);
+
+      // gpio_bit_set(GPIOA, GPIO_PIN_11);
+
+      gpio_deinit(GPIOA);
+      gpio_deinit(GPIOB);
+
+       // erase page
+      fmc_unlock();
+      fmc_flag_clear(FMC_FLAG_END | FMC_FLAG_WPERR | FMC_FLAG_PGERR);
+      fmc_page_erase(PAGE_ADDR);
+      fmc_flag_clear(FMC_FLAG_END | FMC_FLAG_WPERR | FMC_FLAG_PGERR);
+      fmc_lock();
           
           // write 32 bit data
       fmc_unlock();
       fmc_flag_clear(FMC_FLAG_END | FMC_FLAG_WPERR | FMC_FLAG_PGERR);
       for (uint32_t x = 0; x < 200; x++) {
-        fmc_word_program(PAGE_ADDR + (x * 4), 0x21542154);
+        fmc_word_program(PAGE_ADDR + (x * 4), flash_data);
         fmc_flag_clear(FMC_FLAG_END | FMC_FLAG_WPERR | FMC_FLAG_PGERR);
       }
       fmc_lock();
@@ -84,6 +106,13 @@ void leds_on(void){
   }
 
 
+}
+
+
+void set_pin_output(uint32_t port, uint32_t pin){
+  gpio_mode_set(port, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, pin);
+  gpio_output_options_set(port, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, pin);
+  gpio_bit_set(port, pin);
 }
 
 // #define PAGE_ADDR 0x8002000
