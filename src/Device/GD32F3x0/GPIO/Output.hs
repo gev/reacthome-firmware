b44f6e7d081a6047b7ab5aca2f9bfd95b114235f
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-12-19 16:58:01 +0300

    add pupd

--- src/Device/GD32F3x0/GPIO/Output.hs
+++ src/Device/GD32F3x0/GPIO/Output.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards  #-}
+{-# OPTIONS_GHC -Wno-missing-methods #-}
 
 module Device.GD32F3x0.GPIO.Output where
 
--- src/Interface/GPIO/Input.hs
+++ src/Interface/GPIO/Input.hs
@@ -3,5 +3,10 @@ module Interface.GPIO.Input where
 import           Ivory.Language
 import           Ivory.Language.Module
 
+data Pull = PullUp 
+          | PullDown 
+          | PullNone
+
 class Input a where
-    get :: a -> Ivory eff IBool
+    get     :: a -> Ivory eff IBool
+    setPUPD :: a -> Pull -> Ivory eff ()
