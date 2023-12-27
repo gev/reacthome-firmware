5fe715b7e47f88ed8e4bcb911ee08fdd9fe3b7a9
Evgeny Gazdovsky <gazdovsky@gmail.com>
2023-12-27 19:38:39 +0300

    uart transport discovery

--- src/Transport/UART/RBUS.hs
+++ src/Transport/UART/RBUS.hs
@@ -3,21 +3,24 @@
 {-# LANGUAGE NamedFieldPuns     #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE RankNTypes         #-}
+{-# LANGUAGE RecordWildCards    #-}
 
 module Transport.UART.RBUS    where
 
 import           Control.Monad.Reader     (MonadReader, asks)
 import           Control.Monad.State      (MonadState)
+import           Core.Actions             (actionDiscovery)
 import           Core.Context
 import           Core.Dispatcher
 import qualified Core.Domain              as D
 import           Core.Handler
 import           Core.Task
 import           Core.Transport
+import           Core.Version
 import           Data.Buffer
 import           Data.Concurrent.Queue
 import           Data.Value
-import           Interface.MCU            (MCU (peripherals, systemClock), mac)
+import qualified Interface.MCU            as I
 import           Interface.UART           (HandleUART (HandleUART),
                                            Parity (None), StopBit (SB_1b),
                                            UART (configUART),
@@ -37,10 +40,14 @@ rbus uart' = do
     mcu           <- asks D.mcu
     features      <- asks D.features
 
-    let name       = "transport_uart_rbus"
-    let clock      = systemClock mcu
+    model         <- asks D.model
+    version       <- asks D.version
+    let mac        = I.mac mcu
 
-    uart          <- uart' $ peripherals mcu
+    let name       = "transport_uart_rbus"
+    let clock      = I.systemClock mcu
+
+    uart          <- uart' $ I.peripherals mcu
     rxBuff        <- buffer (name <> "_rx"          )
     rxQueue       <- queue  (name <> "_rx"          )
     msgOffset     <- buffer (name <> "_msg_offset"  )
@@ -49,6 +56,7 @@ rbus uart' = do
     msgBuff       <- buffer (name <> "_msg"         )
     msgIndex      <- value  (name <> "_msg_index"   ) 0
     txBuff        <- buffer (name <> "_tx"          )
+    discoveryBuff <- buffer (name <> "_discovery"   )
     txLock        <- value  (name <> "_tx_lock"     ) false
     rxTimestamp   <- value  (name <> "_timestamp_rx") 0
 
@@ -59,28 +67,45 @@ rbus uart' = do
 
     protocol <- U.rbus name onMessage
 
-    let rbus = RBUS { name, clock, uart, protocol
+    let rbus = RBUS { name
+                    , model, version, mac
+                    , clock, uart, protocol
                     , rxBuff, rxQueue
                     , msgOffset, msgSize, msgQueue, msgBuff, msgIndex
                     , txBuff
                     , txLock
+                    , discoveryBuff
                     , rxTimestamp
                     }
 
     addHandler $ HandleUART uart (rxHandle rbus) (txHandle rbus) Nothing
 
-    addInit name $ configUART uart 1_000_000 WL_8b SB_1b None
+    addInit name $ initialize rbus
 
-    addTask $ yeld (name <> "_rx"   ) $ rxTask    rbus
-    addTask $ yeld (name <> "_tx"   ) $ txTask    rbus
-    addTask $ yeld (name <> "_reset") $ resetTask rbus
+    addTask $ yeld        (name <> "_rx"       ) $ rxTask        rbus
+    addTask $ yeld        (name <> "_tx"       ) $ txTask        rbus
+    addTask $ yeld        (name <> "_reset"    ) $ resetTask     rbus
+    addTask $ delay 1_000 (name <> "_discovery") $ discoveryTask rbus
 
     pure rbus
 
 
 
+initialize RBUS{..} = do
+    store (discoveryBuff ! 0) actionDiscovery
+    arrayMap $ \ix -> do
+        let jx = toIx $ 1 + fromIx ix
+        store (discoveryBuff ! jx) =<< deref (mac ! ix)
+    store (discoveryBuff ! 7) =<< deref model
+    store (discoveryBuff ! 8) =<< deref (version ~> major)
+    store (discoveryBuff ! 9) =<< deref (version ~> minor)
+    configUART uart 1_000_000 WL_8b SB_1b None
+
+
+
 instance Transport RBUS where
     transmitBuffer = toQueue
+
 
 instance LazyTransport RBUS where
     lazyTransmit = toQueue'
--- src/Transport/UART/RBUS/Data.hs
+++ src/Transport/UART/RBUS/Data.hs
@@ -3,9 +3,12 @@
 
 module Transport.UART.RBUS.Data where
 
+import           Core.Version          (Version)
 import           Data.Buffer
 import           Data.Concurrent.Queue
 import           Data.Value
+import           Interface.Mac
+import           Interface.MCU         (MCU (mac))
 import           Interface.SystemClock
 import           Interface.UART
 import           Ivory.Language
@@ -15,6 +18,9 @@ import qualified Protocol.UART.RBUS    as U
 data RBUS where
      RBUS :: UART u
           => { name          :: String
+             , model         :: Value       Uint8
+             , version       :: Version
+             , mac           :: Mac
              , clock         :: SystemClock
              , uart          :: u
              , protocol      :: U.RBUS  255
@@ -25,7 +31,8 @@ data RBUS where
              , msgQueue      :: Queue   256
              , msgBuff       :: Buffer 4096 Uint16
              , msgIndex      :: Value       Uint16
-             , txBuff        :: Buffer  512 Uint16
+             , txBuff        :: Buffer  300 Uint16
+             , discoveryBuff :: Buffer   10 Uint8
              , txLock        :: Value       IBool
              , rxTimestamp   :: Value       Uint32
              } -> RBUS
