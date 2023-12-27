d7be43f9fb390aa0baedbfdff37e35f0000c7d1f
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2023-12-27 16:32:33 +0300

    add sht21 feature to smart_top  formula

--- formula/Formula/Smart/Top/TopA6P.hs
+++ formula/Formula/Smart/Top/TopA6P.hs
@@ -4,6 +4,7 @@ import           Core.Formula
 import           Core.Models
 import           Device.GD32F3x0
 import           Feature.DInputs
+import           Feature.Sht21 
 import           Ivory.Language
 import           Transport.UART.RBUS
 
@@ -13,7 +14,8 @@ smartTopA6P =  Formula { name       = "smart-top-a6p"
                        , version    = (1, 0)
                        , shouldInit = false
                        , transport  = rbus uart_1
-                       , features   = [ dinputs [ in_pa_4
+                       , features   = [ sht21 i2c_0 0x80
+                                      , dinputs [ in_pa_4
                                                 , in_pb_2
                                                 , in_pa_5
                                                 , in_pb_1
