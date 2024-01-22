b6402e4caffc79669b39bbe9143d7556a20436af
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-01-22 17:32:52 +0300

    stylish

--- formula/Formula/Smart/Bottom/Bottom1.hs
+++ formula/Formula/Smart/Bottom/Bottom1.hs
@@ -20,10 +20,10 @@ smartBottom1 =  Formula { name           = "smart_bottom_1"
                         , transport      = rbus $ rs485 uart_1 out_pa_4
                         , implementation = bottom1 (top uart_0 in_pb_4)
                                                    (dinputs [ in_pb_1
-                                                              , in_pb_0
-                                                              , in_pa_6
-                                                              , in_pa_5
-                                                              ]
+                                                            , in_pb_0
+                                                            , in_pa_6
+                                                            , in_pa_5
+                                                            ]
                                                    )
                                                    (ds18b20 ow_0 od_pa_15)
                         }
