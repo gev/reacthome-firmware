9437c2fae73ad706a799781f9de40859db254ec7
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-01-26 17:44:30 +0300

    fix sequence

--- formula/Formula/Smart/Top/TopA6P.hs
+++ formula/Formula/Smart/Top/TopA6P.hs
@@ -24,5 +24,5 @@ smartTopA6P =  Formula { name           = "smart_top_a6p"
                                                        ]
                                               )
                                               (sht21 i2c_0)
-                                            -- npx_pwm_0 [bt1, bt3, bt5, bt6, bt4, bt2]
+                                            -- npx_pwm_0 [led1, led6, led2, led5, led3, led4]
                        }
