941a7596078e47a99cd828047201e594202696bd
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-03-12 11:50:29 +0300

    add formula doppler1 and doppler5

---
+++ formula/Formula/Doppler1.hs
@@ -0,0 +1,26 @@
+module Formula.Doppler1 where
+
+import           Core.Formula
+import           Core.Models
+import           Device.GD32F3x0
+import           Feature.DInputs
+import           Feature.Dopplers
+import           Implementation.Doppler hiding (dinputs, dopplers)
+import           Interface.RS485
+import           Ivory.Language
+import           Transport.RS485.RBUS
+
+doppler1 :: Formula GD32F3x0
+doppler1 = Formula { name           = "doppler1"
+                   , model          = deviceTypeDoppler
+                   , version        = (1, 0)
+                   , shouldInit     = false
+                   , transport      = rbus $ rs485 uart_1 out_pa_4
+                   , implementation = doppler  (dopplers adc_pa_7)
+                                               (dinputs  [ in_pa_12
+                                                         , in_pa_11
+                                                         , in_pa_10
+                                                         , in_pa_9
+                                                         ]
+                                               )
+                    }
--- formula/Formula/Doppler5.hs
+++ formula/Formula/Doppler5.hs
@@ -10,17 +10,23 @@ import           Interface.RS485
 import           Ivory.Language
 import           Transport.RS485.RBUS
 
-doppler5 :: Formula GD32F3x0
-doppler5 = Formula { name           = "doppler5"
-                    , model          = 0xff
-                    , version        = (1, 0)
-                    , shouldInit     = false
-                    , transport      = rbus $ rs485 uart_1 out_pa_4
-                    , implementation = doppler (dopplers [adc_pa_0])
-                                               (dinputs  [ in_pa_12
-                                                         , in_pa_11
-                                                         , in_pa_10
-                                                         , in_pa_9
-                                                         ]
-                                               )
-                    }
+-- doppler5 :: Formula GD32F3x0
+-- doppler5 = Formula { name           = "doppler5"
+--                     , model          = 0xff
+--                     , version        = (1, 0)
+--                     , shouldInit     = false
+--                     , transport      = rbus $ rs485 uart_1 out_pa_4
+--                     , implementation = doppler (dopplers [adc_pa_0
+--                                                          , adc_pa_1
+--                                                          , adc_pa_5
+--                                                          , adc_pa_6
+--                                                          , adc_pa_7
+--                                                          ]
+--                                                )
+--                                                (dinputs  [ in_pa_12
+--                                                          , in_pa_11
+--                                                          , in_pa_10
+--                                                          , in_pa_9
+--                                                          ]
+--                                                )
+--                     }
