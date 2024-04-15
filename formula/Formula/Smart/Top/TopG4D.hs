ccc4283a608de421a39fc3726d0d228262762823
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-04-15 19:27:21 +0300

    add touch power management

--- formula/Formula/Smart/Top/TopG4D.hs
+++ formula/Formula/Smart/Top/TopG4D.hs
@@ -3,12 +3,13 @@ module Formula.Smart.Top.TopG4D where
 import           Core.Formula
 import           Core.Models
 import           Device.GD32F3x0
-import           Feature.DInputs            (dinputs)
-import           Feature.Sht21              (sht21)
-import           Feature.Smart.Top.Vibro    (vibro)
-import           Implementation.Smart.TopGD (topGD)
+import           Feature.DInputs              (dinputs)
+import           Feature.Sht21                (sht21)
+import           Feature.Smart.Top.PowerTouch (powerTouch)
+import           Feature.Smart.Top.Vibro      (vibro)
+import           Implementation.Smart.TopGD   (topGD)
 import           Ivory.Language
-import           Transport.UART.RBUS        (rbus)
+import           Transport.UART.RBUS          (rbus)
 
 smartTopG4D :: Formula GD32F3x0
 smartTopG4D =  Formula { name           = "smart_top_g4d"
@@ -23,6 +24,7 @@ smartTopG4D =  Formula { name           = "smart_top_g4d"
                                                          ]
                                                 )
                                                 (vibro out_pa_1)
+                                                (powerTouch out_pa_8)
                                                 (sht21 i2c_0)
                                                 npx_pwm_1
                        }
--- src/Implementation/Smart/TopGD.hs
+++ src/Implementation/Smart/TopGD.hs
@@ -7,31 +7,33 @@
 
 module Implementation.Smart.TopGD where
 
-import           Control.Monad.Reader      (MonadReader, asks)
-import           Control.Monad.State       (MonadState)
+import           Control.Monad.Reader         (MonadReader, asks)
+import           Control.Monad.State          (MonadState)
 import           Core.Actions
 import           Core.Context
 import           Core.Controller
-import qualified Core.Domain               as D
-import           Core.Handler              (addHandler)
+import qualified Core.Domain                  as D
+import           Core.Handler                 (addHandler)
 import           Core.Task
 import           Core.Transport
 import           Data.Buffer
 import           Data.Value
-import           Device.GD32F3x0.ADC       (ADC (buff))
-import           Endpoint.DInputs          as E (DInputs)
-import           Feature.DInputs           as DI (DInputs (getDInputs),
-                                                  forceSyncDInputs)
-import           Feature.RS485.RBUS.Data   (RBUS (shouldConfirm))
-import           Feature.Sht21             (SHT21)
+import           Device.GD32F3x0.ADC          (ADC (buff))
+import           Endpoint.DInputs             as E (DInputs)
+import           Feature.DInputs              as DI (DInputs (getDInputs),
+                                                     forceSyncDInputs)
+import           Feature.RS485.RBUS.Data      (RBUS (shouldConfirm))
+import           Feature.Sht21                (SHT21)
 import           Feature.Smart.Top.Buttons
-import           Feature.Smart.Top.LEDs    (LEDs, mkLeds, onDim, onDo, onImage,
-                                            onInitColors, onSetColor, render,
-                                            updateLeds)
-import           Feature.Smart.Top.Vibro   (Vibro, onInitVibro, onVibro, vibro)
+import           Feature.Smart.Top.LEDs       (LEDs, mkLeds, onDim, onDo,
+                                               onImage, onInitColors,
+                                               onSetColor, render, updateLeds)
+import           Feature.Smart.Top.PowerTouch (PowerTouch)
+import           Feature.Smart.Top.Vibro      (Vibro, onInitVibro, onVibro,
+                                               vibro)
 import           GHC.TypeNats
-import           Interface.Display         (Display, Render (Render))
-import           Interface.MCU             (peripherals)
+import           Interface.Display            (Display, Render (Render))
+import           Interface.MCU                (peripherals)
 import           Ivory.Language
 import           Ivory.Stdlib
 
@@ -64,15 +66,20 @@ data Top = Top
 -}
 
 topGD :: (MonadState Context m , MonadReader (D.Domain p c) m, Transport t, LazyTransport t, Display d)
-      => m t -> (Bool -> t -> m DI.DInputs) -> (E.DInputs -> t -> m Vibro) -> (t -> m SHT21) -> (p -> m d) -> m Top
-topGD transport' dinputs' vibro' sht21' display' = do
+      => m t -> (Bool -> t -> m DI.DInputs) -> (E.DInputs -> t -> m Vibro) -> m PowerTouch -> (t -> m SHT21) -> (p -> m d) -> m Top
+topGD transport' dinputs' vibro' touch' sht21' display' = do
     transport          <- transport'
     shouldInit         <- asks D.shouldInit
     mcu                <- asks D.mcu
     display            <- display' $ peripherals mcu
     dinputs            <- dinputs' True transport
+
     let runFrameBuffer  = runValues "top_frame_buffer" $ replicate 204 0
+
     vibro              <- vibro' (getDInputs dinputs) transport
+
+    touch'
+
     leds               <- mkLeds runFrameBuffer [  6,  7,      0,  1
                                                 ,  5,  4,      3,  2
 
@@ -92,9 +99,13 @@ topGD transport' dinputs' vibro' sht21' display' = do
                                                 ,     14,     19,     27,     32,     40,     46,     54
                                                 ,     13,     18, 25, 26,     31, 38, 39, 44, 45, 52, 53
                                                 ] transport
+
     buttons            <- mkButtons leds (getDInputs dinputs) 2 transport
+
     sht21              <- sht21' transport
+
     initBuff           <- values "top_init_buffer" [actionInitialize]
+
     let top             = Top { dinputs, leds, vibro, buttons, sht21
                               , initBuff, shouldInit
                               , transmit = transmitBuffer transport
