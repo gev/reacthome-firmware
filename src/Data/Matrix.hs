cdd11a6d43cea98ca4428388d0a86cbf3526e6e1
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-04-23 18:42:56 +0300

    remove runMatrix

--- src/Data/Matrix.hs
+++ src/Data/Matrix.hs
@@ -2,16 +2,14 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE ImpredicativeTypes  #-}
+{-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Matrix
     ( Matrix
-    , Matrix'
-    , RunMatrix
     , matrix_
+    , matrix'
     , matrix
-    , runMatrix_
-    , runMatrix
     ) where
 
 import           Control.Monad.State
@@ -19,11 +17,10 @@ import           Core.Context
 import           Data.Area
 import           GHC.TypeNats
 import           Ivory.Language
+import           Ivory.Language.Proxy (NatType, aNat)
 
 
-type Matrix  n m t = Ref Global (Array  n (Array m (Stored t)))
-type Matrix' n m t = MemArea    (Array  n (Array m (Stored t)))
-type RunMatrix   t = forall a.  (forall n m. (KnownNat n, KnownNat m) => Matrix' n m t -> a) -> a
+type Matrix  (n:: Nat) (m :: Nat) t = Ref Global (Array n (Array m (Stored t)))
 
 
 matrix_ :: (MonadState Context w, IvoryZeroVal t, KnownNat n, KnownNat m)
@@ -31,23 +28,15 @@ matrix_ :: (MonadState Context w, IvoryZeroVal t, KnownNat n, KnownNat m)
 matrix_ id = mkArea id Nothing
 
 
+matrix' :: forall n m t w. (KnownNat n, KnownNat m, MonadState Context w, IvoryZeroVal t, IvoryInit t)
+        => String -> t -> w (Matrix n m t)
+matrix' id v = do
+    let n' = fromTypeNat (aNat :: NatType n)
+    let m' = fromTypeNat (aNat :: NatType m)
+    matrix id . fill n' . fill m' $ v
+    where fill = replicate . fromIntegral
+
+
 matrix :: (MonadState Context w, IvoryZeroVal t, IvoryInit t, KnownNat n, KnownNat m)
        => String -> [[t]] -> w (Matrix n m t)
 matrix id v = mkArea id . Just . iarray $ (\r -> iarray $ ival <$> r) <$> v
-
-
-
-
-runMatrix_ :: (IvoryInit t, IvoryZeroVal t)
-           => String -> Int -> Int -> RunMatrix t
-runMatrix_ id = run' (area id Nothing)
-
-runMatrix :: (IvoryInit t, IvoryZeroVal t)
-          => String -> t -> Int -> Int -> RunMatrix t
-runMatrix id v n m = run' (area id . Just . iarray . replicate n . iarray . replicate m . ival $ v) n m
-
-
-
-run' :: forall t. (forall n m. (KnownNat n, KnownNat m) => Matrix' n m t) -> Int -> Int -> RunMatrix t
-run' v n m f = go (someNatVal . fromIntegral $ n) (someNatVal . fromIntegral $ m)
-    where go (SomeNat (p :: Proxy p)) (SomeNat (q :: Proxy q)) = f (v :: Matrix' p q t)
