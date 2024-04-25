3024404664bee03d07f9f16b76e8ad7b00c008fc
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-04-25 19:48:36 +0300

    add the records' constructor

--- src/Data/Record.hs
+++ src/Data/Record.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -7,10 +8,10 @@
 module Data.Record
     ( Record
     , Records
-    , Records'
     , record_
     , record
     , records_
+    , records'
     , records
     ) where
 
@@ -19,12 +20,12 @@ import           Core.Context
 import           Data.Area
 import           GHC.TypeNats
 import           Ivory.Language
+import           Ivory.Language.Proxy
 
 
 
-type Record     t = Ref Global (Struct t)
-type Records  n t = Ref Global (Array n (Struct t))
-type Records' n t = MemArea    (Array n (Struct t))
+type Record              t = Ref Global (Struct t)
+type Records  (n :: Nat) t = Ref Global (Array n (Struct t))
 
 
 record_ :: (MonadState Context m, IvoryStruct t)
@@ -40,6 +41,12 @@ record id r = mkArea id . Just $ istruct r
 records_ :: (MonadState Context m, KnownNat n, IvoryStruct t)
          => String -> m (Records n t)
 records_ id = mkArea id Nothing
+
+records' :: forall m n t. (MonadState Context m, KnownNat n, IvoryStruct t)
+         => String -> [InitStruct t] -> m (Records n t)
+records' id r = do
+    let n = fromIntegral $ natVal (aNat :: NatType n)
+    records id $ replicate n r
 
 records :: (MonadState Context m, KnownNat n, IvoryStruct t)
         => String -> [[InitStruct t]] -> m (Records n t)
