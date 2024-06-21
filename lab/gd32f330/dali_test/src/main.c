7e08f39fcc9c06c12abb74c506ef0947f820721d
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-06-21 18:50:59 +0300

    lab dali up

--- lab/gd32f330/dali_test/src/main.c
+++ lab/gd32f330/dali_test/src/main.c
@@ -6,21 +6,38 @@
 #include "gd32f3x0_rcu.h"
 #include "gpio.h"
 
-#define DALI_TIME_BIT_US  833 
+#define DALI_TIME_BIT_US       833 
 #define DALI_HALF_TIME_BIT_US  (DALI_TIME_BIT_US / 2)
+#define DALI_0_75_TIME_BIT_US  (DALI_TIME_BIT_US * 3 / 4)
 
 #define DALI_START_BIT        2
 #define DALI_DOUBLE_STOP_BIT  3
 
 #define dali_pin_reset pin_reset
-#define dali_pin_set pin_set
+#define dali_pin_set   pin_set
+#define dali_pin_get   pin_get
 
 
 pin dali_rx = {GPIOA, GPIO_PIN_0};
 pin dali_tx = {GPIOA, GPIO_PIN_1};
 
+uint8_t byte_read = 0;
+ 
+uint8_t read_byte() {
+  while (dali_pin_get(dali_rx) == 0);
+  delay_us(DALI_TIME_BIT_US + DALI_0_75_TIME_BIT_US);
+  uint8_t result = 0;
+  for (int i = 0; i < 8; i++) {
+    if (dali_pin_get(dali_rx) == 0) {
+      result |= (0x80 >> i);
+    }
+    delay_us(DALI_TIME_BIT_US);
+  }
+  return result;
+}
 
 void dali_send_frame(uint8_t, uint8_t);
+volatile uint8_t res;
 
 int main() {
   rcu_periph_clock_enable(RCU_GPIOA);
@@ -34,9 +51,11 @@ int main() {
 
 
   while (1) {
-    dali_send_frame(0xFE, 0x00);
-    delay_us(2000000);
     dali_send_frame(0xFE, 0xf0);
+    delay_us(2000000);
+    dali_send_frame(0xFF, 0x91);
+    res = 0;
+    res = read_byte();
     delay_us(2000000);
   }
 }
@@ -78,6 +97,21 @@ void dali_send_frame(uint8_t address, uint8_t data) {
   }
   for(int i = 0; i < 8; i++) {
     dali_send_bit(((data << i) & 0x80) >> 7);
+  }
+
+  dali_send_bit(DALI_DOUBLE_STOP_BIT);
+}
+void dali_send_double_frame(uint8_t address, uint8_t byte1, uint8_t byte2) {
+  dali_send_bit(DALI_START_BIT);
+
+  for(int i = 0; i < 8; i++) {
+    dali_send_bit(((address << i) & 0x80) >> 7);
+  }
+  for(int i = 0; i < 8; i++) {
+    dali_send_bit(((byte1 << i) & 0x80) >> 7);
+  }
+  for(int i = 0; i < 8; i++) {
+    dali_send_bit(((byte2 << i) & 0x80) >> 7);
   }
 
   dali_send_bit(DALI_DOUBLE_STOP_BIT);
