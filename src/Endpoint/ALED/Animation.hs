1b456e800acaadb97cc205db0ce43a637956bd27
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-09-09 14:19:31 +0300

    up

--- src/Endpoint/ALED/Animation.hs
+++ src/Endpoint/ALED/Animation.hs
@@ -54,8 +54,8 @@ renderColor random animation segment segmentSize pixel pixelSize subpixel value 
             cond [ 0x00 --> renderFade      subpixel value
                  , 0x10 --> renderSpectrumT subpixel
                  , 0x11 --> renderSpectrumX segmentSize pixel subpixel
-                 , 0x30 --> renderRandomT   subpixel value random
-                 , 0x31 --> renderRandomX   value random
+                 , 0x20 --> renderRandomT   subpixel value random
+                 , 0x21 --> renderRandomX   value random
                  , true ==> pure value
                  ]
          ) $ pure value
@@ -68,46 +68,45 @@ renderMask :: Random Uint8
             -> Sint32
             -> Ivory (AllowBreak (ProcEffects s ())) IFloat
 renderMask random animation segment segmentSize pixel = do
-    t <- local $ ival 0
-    time' <- deref $ animation ~> time
-    phase' <- deref $ animation ~> phase
-    let phase = safeCast segment * phase'
-    ifte_ (time' <? 0)
-          (store t $ time' - phase)
-          (do
-               let t' = time' - phase
-               ifte_ (t' <? 0)
-                     (store t $ t' - floorF t')
-                     (store t t')
+     t <- local $ ival 0
+     time' <- deref $ animation ~> time
+     phase' <- deref $ animation ~> phase
+     let phase = safeCast segment * phase'
+     store t $ time' - phase
+     t' <- deref t
+     kind' <- deref $ animation ~> kind
+     animationState' <- deref $ animation ~> animationState
+     ifte animationState'
+          (ifte (t' <? 0)
+                (ifte (kind' .& 0xf0 ==? 0x10)
+                      (pure 0)
+                      (pure 1)
+                )
+                (do
+                   let (-->) p r = kind' ==? p ==> r animation
+                   cond [ 0x01 --> renderRandomOff   pixel random
+                        , 0x02 --> renderSlideOff    segmentSize pixel
+                        , 0x03 --> renderSlideOff'   segmentSize pixel
+                        , 0x04 --> renderSlideOffIn  segmentSize pixel
+                        , 0x05 --> renderSlideOffOut segmentSize pixel
+
+                        , 0x11 --> renderRandomOn    pixel random
+                        , 0x12 --> renderSlideOn     segmentSize pixel t'
+                        , 0x13 --> renderSlideOn'    segmentSize pixel
+                        , 0x14 --> renderSlideOnIn   segmentSize pixel
+                        , 0x15 --> renderSlideOnOut  segmentSize pixel
+
+                        , 0x21 --> renderRandom      random
+                        , 0x22 --> renderEiffel      random
+                        , 0x23 --> renderSlide       segmentSize pixel
+                        , 0x24 --> renderSlide'      segmentSize pixel
+                        , 0x25 --> renderSlide''     segmentSize pixel
+                        , 0xff --> renderConst
+                        , true ==> pure 1
+                        ]
+                )
           )
-    t' <- deref t
-    kind' <- deref $ animation ~> kind
-    animationState' <- deref $ animation ~> animationState
-    ifte (animationState' .&& t' >=? 0)
-         (do
-            let (-->) p r = kind' ==? p ==> r animation
-            cond [ 0x01 --> renderRandomOff   pixel random
-                 , 0x02 --> renderSlideOff    segmentSize pixel
-                 , 0x03 --> renderSlideOff'   segmentSize pixel
-                 , 0x04 --> renderSlideOffIn  segmentSize pixel
-                 , 0x05 --> renderSlideOffOut segmentSize pixel
-
-                 , 0x11 --> renderRandomOn    pixel random
-                 , 0x12 --> renderSlideOn     segmentSize pixel t'
-                 , 0x13 --> renderSlideOn'    segmentSize pixel
-                 , 0x14 --> renderSlideOnIn   segmentSize pixel
-                 , 0x15 --> renderSlideOnOut  segmentSize pixel
-
-                 , 0x21 --> renderRandom      random
-                 , 0x22 --> renderEiffel      random
-                 , 0x23 --> renderSlide       segmentSize pixel
-                 , 0x24 --> renderSlide'      segmentSize pixel
-                 , 0x25 --> renderSlide''     segmentSize pixel
-
-                 , 0xff --> renderConst
-
-                 , true ==> pure 1
-                 ]
-         ) $ ifte (kind' .& 0xf0 ==? 0x00)
-                  (pure 0)
-                  (pure 1)
+          (ifte (kind' .& 0xf0 ==? 0x00)
+                (pure 0)
+                (pure 1)
+          )
