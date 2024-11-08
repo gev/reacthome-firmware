dda0cd33daabf2369037ceb1e3f24c69c5c49e45
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-11-08 10:41:30 +0300

    up

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import           Device.GD32F3x0
 import           Device.GD32F4xx
 import           Formula.Blink330             (blink330)
 import           Formula.DI4
+import           Formula.DI4LA
 import           Formula.DI4RSM
 import           Formula.DimmerAC12
 import           Formula.DimmerDC12
@@ -31,30 +32,29 @@ import           Formula.Smart.Top.TopG6
 
 main :: IO ()
 main = do
-    -- gcc gd32f350k8u6 [ di4rsm
-    --                  ]
+    gcc gd32f350k8u6 [ di4rsm
+                     ]
 
-    -- gcc gd32f330k8u6 [ relay12
-    --                  , dimmerDC12
-    --                  , dimmerAC12
-    --                  , mix6x12
-    --                  , di4
-    --                  , smartBottom1
-    --                  , smartBottom2
-    --                  , smartTopA4P
-    --                  , smartTopG6
-    --                  , smartTopG4
-    --                  , smartTopG2
-    --                  , smartTopA6P
-    --                  , smartTopA4T
-    --                  , smartTopA6T
-    --                  , smartTopG4D
-    --                  , doppler1
-    --                  , doppler5
-    --                  ]
+    gcc gd32f330k8u6 [ relay12
+                     , dimmerDC12
+                     , dimmerAC12
+                     , mix6x12
+                     , di4
+                     , di4la
+                     , smartBottom1
+                     , smartBottom2
+                     , smartTopA4P
+                     , smartTopG6
+                     , smartTopG4
+                     , smartTopG2
+                     , smartTopA6P
+                     , smartTopA4T
+                     , smartTopA6T
+                     , smartTopG4D
+                     , doppler1
+                     , doppler5
+                     ]
 
-    -- gcc gd32f450vgt6 [ server
-    --                  , rsHub4
-    --                  ]
-
-    gcc gd32f450vgt6 [ rsHub4 ]
+    gcc gd32f450vgt6 [ server
+                     , rsHub4
+                     ]
