c436c32e631542aa26e39fb5231c04124df09fd8
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-11-15 13:45:39 +0300

    fix

--- src/Feature/RS485/RBUS/Data.hs
+++ src/Feature/RS485/RBUS/Data.hs
@@ -40,7 +40,6 @@ data RBUS = forall t. (LazyTransport t, Transport t) => RBUS
      , msgConfirm       :: Values  255 IBool
      , msgBuff          :: Buffer 1024 Uint16
      , msgIndex         :: Value       Uint16
-     , txBuff           :: Buffer  255 Uint16
      , rsBuff           :: Buffer  253 Uint8
      , rsSize           :: Value       Uint8
      , rxLock           :: Value       IBool
