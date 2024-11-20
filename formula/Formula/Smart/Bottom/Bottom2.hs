5f0d74e77dfc1042e05332632e1ec32ce5515224
Evgeny Gazdovsky <gazdovsky@gmail.com>
2024-11-20 12:07:25 +0300

    up

--- formula/Formula/Smart/Bottom/Bottom2.hs
+++ formula/Formula/Smart/Bottom/Bottom2.hs
@@ -22,6 +22,7 @@ smartBottom2 =  Formula { name            = "smart_bottom_2"
                         , model           = deviceTypeSmartBottom2
                         , version         = (4, 6)
                         , shouldInit      = true
+                        , mcu             = gd32f330k8u6
                         , quartzFrequency =  8_000_000
                         , systemFrequency = 84_000_000
                         , implementation  = bottom2 (rbus $ rs485 uart_1 out_pa_4)
