f1cfa8cf69922bbc627159fdfbddb7e71b5455fe
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2024-12-02 12:58:29 +0300

    dimmer ac up version

--- formula/Formula/DimmerAC12.hs
+++ formula/Formula/DimmerAC12.hs
@@ -18,7 +18,7 @@ import           Transport.RS485.RBUS
 dimmerAC12 :: Formula GD32F3x0
 dimmerAC12 = Formula { name            = "dimmerAC12"
                      , model           = deviceTypeDim12AcRs
-                     , version         = (2, 6)
+                     , version         = (2, 7)
                      , shouldInit      = true
                      , mcu             = gd32f330k8u6
                      , quartzFrequency =  8_000_000
