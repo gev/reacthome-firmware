19b17e134435bb296073895dfd7f0219faf76ca0
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-05-20 18:52:27 +0300

    new init

--- src/Feature/SRC4392.hs
+++ src/Feature/SRC4392.hs
@@ -2,25 +2,33 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs            #-}
 {-# LANGUAGE NamedFieldPuns   #-}
+{-# LANGUAGE RecordWildCards  #-}
 
 module Feature.SRC4392 where
 
+
 import           Control.Monad.RWS
 import           Core.Context
-import qualified Core.Domain         as D
+import qualified Core.Domain       as D
+import           Core.Task
 import           Data.Buffer
 import           Data.Matrix
 import           Data.Value
-import qualified Interface.I2C       as I
+import qualified Interface.I2C     as I
 import           Interface.MCU
 import           Ivory.Language
-import           Ivory.Language.Uint 
+import           Ivory.Stdlib
+import Core.Handler
+
+
+
 
 data SRC4392 = forall i. I.I2C i 2 => SRC4392
     { i2c      :: i 2
-    , address  :: Value Uint8
+    , address  :: Uint8
     , config   :: Matrix 10 2 Uint8
     , tempBuff :: Buffer 2 Uint8
+    , count    :: Value Uint8
     }
 
 mkSRC4392 :: (MonadState Context m, MonadReader (D.Domain p c) m, I.I2C i 2)
@@ -28,8 +36,8 @@ mkSRC4392 :: (MonadState Context m, MonadReader (D.Domain p c) m, I.I2C i 2)
 mkSRC4392 i2c' = do
     mcu      <- asks D.mcu
     i2c      <- i2c' $ peripherals mcu
-    address  <- value  "src4392_address" 0xc0
     tempBuff <- buffer "src4392"
+    count    <- value "src4392_count" 0
     config   <- matrix "src4392_config" [[0x7f, 0x00],
                                          [0x01, 0x3F],
                                          [0x03, 0x31],
@@ -41,14 +49,17 @@ mkSRC4392 i2c' = do
                                          [0x2d, 0x02],
                                          [0x2f, 0x00]]
 
-    let src4392 = SRC4392 {i2c, address, config, tempBuff}
+    let src4392 = SRC4392 {i2c, address = 0xe0, config, tempBuff, count}
 
-    addInit "src4392" $ do
-        address' <- deref address
-        arrayMap $ \jx -> do
-            arrayMap $ \ix -> store (tempBuff ! toIx ix) =<< deref (config ! jx ! ix)
-            I.transmit i2c address' tempBuff
+    addTask $ delay 10 "src4392_init" $ initSrc4392 src4392
+
+    addHandler $ I.HandleI2C i2c $ \_ _ -> pure ()
 
     pure src4392
 
-
+initSrc4392 SRC4392{..} = do
+    count' <- deref count
+    when (count' <? 10) $ do
+        arrayMap $ \ix -> store (tempBuff ! ix) =<< deref (config ! toIx count' ! ix)
+        I.transmit i2c address tempBuff
+        store count $ count' + 1
