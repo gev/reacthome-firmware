03b74ddac3b1d22818b701476f25538e3f36d087
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-06-03 17:37:02 +0300

    fix src4392

--- src/Feature/SRC4392.hs
+++ src/Feature/SRC4392.hs
@@ -31,16 +31,16 @@ data SRC4392 = forall i o. (I.I2C i 2, Output o) => SRC4392
     , address  :: Uint8
     , config   :: Matrix 10 2 Uint8
     , count    :: Value Uint8
-    , shutdown :: o
+    , mute     :: o
     }
 
 mkSRC4392 :: (MonadState Context m, MonadReader (D.Domain p c) m, I.I2C i 2, Output o, Pull p u)
           => (p -> m (i 2)) -> (p -> u -> m o) -> m SRC4392
-mkSRC4392 i2c' shutdown'= do
+mkSRC4392 i2c' mute' = do
     mcu      <- asks D.mcu
     let peripherals' = peripherals mcu
     i2c      <- i2c' peripherals'
-    shutdown <- shutdown' peripherals' $ pullNone peripherals'
+    mute     <- mute' peripherals' $ pullNone peripherals'
     count    <- value "src4392_count" 0
     config   <- matrix "src4392_config" [[0x7f, 0x00],
                                          [0x01, 0x3F],
@@ -53,9 +53,9 @@ mkSRC4392 i2c' shutdown'= do
                                          [0x2d, 0x02],
                                          [0x2f, 0x00]]
 
-    let src4392 = SRC4392 {i2c, address = 0xe0, config, count, shutdown}
+    let src4392 = SRC4392 {i2c, address = 0xe0, config, count, mute}
 
-    addInit "src4392_init" $ reset shutdown
+    addInit "src4392_init" $ set mute
     addTask $ delay 10 "src4392_init" $ initSrc4392 src4392
 
     addHandler $ I.HandleI2C i2c $ \_ _ -> pure ()
@@ -64,6 +64,6 @@ mkSRC4392 i2c' shutdown'= do
 
 initSrc4392 SRC4392{..} = do
     count' <- deref count
-    flip (ifte_ (count' <? arrayLen config)) (set shutdown) $ do
+    flip (ifte_ (count' <? arrayLen config)) (reset mute) $ do
         I.transmit i2c address $ config ! toIx count'
         store count $ count' + 1
