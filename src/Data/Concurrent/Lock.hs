3eadde32bc0d4df511d52c4c4e4e3e649408c2bc
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-06-07 23:09:23 +0300

    disable/enable IRQs

--- src/Data/Concurrent/Lock.hs
+++ src/Data/Concurrent/Lock.hs
@@ -1,12 +1,13 @@
 module Data.Concurrent.Lock where
 
 import           Ivory.Language
+import           Support.CMSIS.CoreCMFunc (disableIRQ, enableIRQ)
 
 lock :: Ivory eff ()
-lock = pure ()
+lock = disableIRQ
 
 unlock :: Ivory eff ()
-unlock = pure ()
+unlock = enableIRQ
 
 atomically :: Ivory eff () -> Ivory eff ()
 atomically run = lock >> run >> unlock
