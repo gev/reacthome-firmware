d731ba8df0265111fa3d36a99df221ccf48035c7
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-07-26 18:37:30 +0300

    main up

--- app/Main.hs
+++ app/Main.hs
@@ -26,6 +26,7 @@ import           Formula.Smart.Top.TopG4
 import           Formula.Smart.Top.TopG4D
 import           Formula.Smart.Top.TopA4TD
 import           Formula.Smart.Top.TopG6
+import           Formula.Smart.Top.TopG6I
 import           Formula.Soundbox
 import           Formula.Lanamp
 import           Formula.MixH 
@@ -34,31 +35,32 @@ import           Formula.MixH
 main :: IO ()
 main = do
     mapM_ gcc [ di4
-              , di4la
-              , di4rsm
-              , relay12
-              , dimmerDC12
-              , dimmerAC12
-              , dimmerAC1
-              , doppler1
-              , doppler5
-              , mix6x12
-              , mixH
-              , smartBottom1
-              , smartBottom2
-              , smartTopA4P
-              , smartTopA4T
-              , smartTopA4TD
-              , smartTopA6P
-              , smartTopA6T
-              , smartTopG2
-              , smartTopG4
-              , smartTopG4D
+              -- , di4la
+              -- , di4rsm
+              -- , relay12
+              -- , dimmerDC12
+              -- , dimmerAC12
+              -- , dimmerAC1
+              -- , doppler1
+              -- , doppler5
+              -- , mix6x12
+              -- , mixH
+              -- , smartBottom1
+              -- , smartBottom2
+              -- , smartTopA4P
+              -- , smartTopA4T
+              -- , smartTopA4TD
+              -- , smartTopA6P
+              -- , smartTopA6T
+              -- , smartTopG2
+              -- , smartTopG4
+              -- , smartTopG4D
               , smartTopG6
+              , smartTopG6I
               ]
-    mapM_ gcc [ 
-                lanamp              
-              , rsHub4
-              , server
-              , soundbox
-              ]
+    -- mapM_ gcc [ 
+    --             lanamp              
+    --           , rsHub4
+    --           , server
+    --           , soundbox
+    --           ]
