18ccb3774bf2bb85fbc44eb49f72c95632f58d10
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-09-09 12:51:35 +0300

    add test pcb for g6i

--- app/Main.hs
+++ app/Main.hs
@@ -27,6 +27,7 @@ import           Formula.Smart.Top.TopG4D
 import           Formula.Smart.Top.TopA4TD
 import           Formula.Smart.Top.TopG6
 import           Formula.Smart.Top.TopG6I
+import           Formula.Smart.Top.TopG6IT
 import           Formula.Smart.Top.TopG6Test
 import           Formula.Soundbox
 import           Formula.MixH 
@@ -35,31 +36,32 @@ import           Formula.MixH
 main :: IO ()
 main = do
     mapM_ gcc [ di4
-              , di4la
-              , di4rsm
-              , relay12
-              , dimmerDC12
-              , dimmerAC12
-              , dimmerAC1
-              , doppler1
-              , doppler5
-              , mix6x12
-              , mixH
-              , smartBottom1
-              , smartBottom2
-              , smartTopA4P
-              , smartTopA4T
-              , smartTopA4TD
-              , smartTopA6P
-              , smartTopA6T
-              , smartTopG2
-              , smartTopG4
-              , smartTopG4D
-              , smartTopG6
+              -- , di4la
+              -- , di4rsm
+              -- , relay12
+              -- , dimmerDC12
+              -- , dimmerAC12
+              -- , dimmerAC1
+              -- , doppler1
+              -- , doppler5
+              -- , mix6x12
+              -- , mixH
+              -- , smartBottom1
+              -- , smartBottom2
+              -- , smartTopA4P
+              -- , smartTopA4T
+              -- , smartTopA4TD
+              -- , smartTopA6P
+              -- , smartTopA6T
+              -- , smartTopG2
+              -- , smartTopG4
+              -- , smartTopG4D
+              -- , smartTopG6
               , smartTopG6I
-              , smartTopG6Test
+              , smartTopG6IT
+              -- , smartTopG6Test
               ]
-    mapM_ gcc [ rsHub4
-              , server
-              , soundbox
-              ]
+    -- mapM_ gcc [ rsHub4
+    --           -- , server
+    --           , soundbox
+    --           ]
