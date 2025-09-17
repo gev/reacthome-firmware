ee6c98714fdb05fb9fee23bcb7c8929e0dc193ff
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-09-17 23:50:53 +0300

    stylish

--- formula/Formula/Smart/Top/TopG6I.hs
+++ formula/Formula/Smart/Top/TopG6I.hs
@@ -17,24 +17,24 @@ import           Transport.UART.RBUS
 
 smartTopG6I :: Formula GD32F3x0
 smartTopG6I =  Formula { name           = "smart_top_g6i"
-                      , model           = deviceTypeSmartTopG6
-                      , version         = (4, 8)
-                      , shouldInit      = false
-                      , mcu             = gd32f330k8u6
-                      , quartzFrequency =  8_000_000
-                      , systemFrequency = 84_000_000
-                      , implementation  = topG6I (rbusTop uart_1)
-                                                 (touches 10
-                                                       $  touch_pa4
-                                                       :> touch_pb2
-                                                       :> touch_pa5
-                                                       :> touch_pb1
-                                                       :> touch_pa7
-                                                       :> touch_pb0
-                                                       :> Nil
-                                                 )
-                                                 (vibro out_pb_5)
-                                                 (sht21 i2c_0)
-                                                 npx_pwm_0
-                                                 etc
-                      }
+                       , model           = deviceTypeSmartTopG6
+                       , version         = (4, 8)
+                       , shouldInit      = false
+                       , mcu             = gd32f330k8u6
+                       , quartzFrequency =  8_000_000
+                       , systemFrequency = 84_000_000
+                       , implementation  = topG6I (rbusTop uart_1)
+                                                  (touches 10
+                                                        $  touch_pa4
+                                                        :> touch_pb2
+                                                        :> touch_pa5
+                                                        :> touch_pb1
+                                                        :> touch_pa7
+                                                        :> touch_pb0
+                                                        :> Nil
+                                                  )
+                                                  (vibro out_pb_5)
+                                                  (sht21 i2c_0)
+                                                  npx_pwm_0
+                                                  etc
+                       }
