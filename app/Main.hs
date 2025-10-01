a790ff39113454f320e038695d35c22c869dbe63
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-10-01 15:59:16 +0300

    all firmware

--- app/Main.hs
+++ app/Main.hs
@@ -11,34 +11,35 @@ main = do
     mapM_
         gcc
         [ di4
-        , -- , di4la
-          -- , di4rsm
-          -- , relay12
-          -- , dimmerDC12
-          -- , dimmerAC12
-          -- , dimmerAC1
-          -- , doppler1
-          -- , doppler5
-          -- , mix6x12
-          -- , mixH
-          -- , smartBottom1
-          -- , smartBottom2
-          -- , smartTopA4P
-          -- , smartTopA4T
-          -- , smartTopA4TD
-          -- , smartTopA6P
-          -- , smartTopA6T
-          -- , smartTopG2
-          -- , smartTopG4
-          -- , smartTopG4D
-          -- , smartTopG6
-          smartTopG6I
-        , smartTopG6IT
+        , di4la
+        , di4rsm
+        , relay12
+        , dimmerDC12
+        , dimmerAC12
+        , dimmerAC1
+        , doppler1
+        , doppler5
         , leakSensor
-        -- , smartTopG6Test
+        , mix6x12
+        , mixH
+        , smartBottom1
+        , smartBottom2
+        , smartTopA4P
+        , smartTopA4T
+        , smartTopA4TD
+        , smartTopA6P
+        , smartTopA6T
+        , smartTopG2
+        , smartTopG4
+        , smartTopG4D
+        , smartTopG6
+        , smartTopG6I
+        , smartTopG6IT
+        , smartTopG6Test
         ]
-
--- mapM_ gcc [ rsHub4
---           -- , server
---           , soundbox
---           ]
+    mapM_
+        gcc
+        [ rsHub4
+        , server
+        , soundbox
+        ]
