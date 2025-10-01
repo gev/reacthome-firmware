6e585cc6410c5b3dfee7e86082939587d707ca75
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-10-01 16:06:57 +0300

    add imports

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,34 @@ module Main where
 
 import Build.Compiler.GCC
 import Formula.DI4
+import Formula.DI4LA
+import Formula.DI4RSM
+import Formula.DimmerAC1
+import Formula.DimmerAC12
+import Formula.DimmerDC12
+import Formula.Doppler1
+import Formula.Doppler5
 import Formula.LeakSensor
+import Formula.Mix6x12
+import Formula.MixH
+import Formula.Relay12
+import Formula.RsHub4
+import Formula.Server
+import Formula.Smart.Bottom.Bottom1
+import Formula.Smart.Bottom.Bottom2
+import Formula.Smart.Top.TopA4P
+import Formula.Smart.Top.TopA4T
+import Formula.Smart.Top.TopA4TD
+import Formula.Smart.Top.TopA6P
+import Formula.Smart.Top.TopA6T
+import Formula.Smart.Top.TopG2
+import Formula.Smart.Top.TopG4
+import Formula.Smart.Top.TopG4D
+import Formula.Smart.Top.TopG6
 import Formula.Smart.Top.TopG6I
 import Formula.Smart.Top.TopG6IT
+import Formula.Smart.Top.TopG6Test
+import Formula.Soundbox
 
 main :: IO ()
 main = do
