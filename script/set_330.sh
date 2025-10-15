fabe39695d91cdcdd529ead7a6bd73a9a3960278
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-10-15 16:36:00 +0300

    up

--- script/set_330.sh
+++ script/set_330.sh
@@ -1,8 +1,9 @@
-echo r > jlink/gd32f330/FlashMCU.jlink
-echo h >> jlink/gd32f330/FlashMCU.jlink
-echo loadbin $1 0x8000000 >> jlink/gd32f330/FlashMCU.jlink
-echo g >> jlink/gd32f330/FlashMCU.jlink
-echo verifybin $1 0x8000000 >> jlink/gd32f330/FlashMCU.jlink
-echo r >> jlink/gd32f330/FlashMCU.jlink
-echo q >> jlink/gd32f330/FlashMCU.jlink
+FILE="jlink/gd32f330/FlashMCU.jlink"
 
+echo r > $FILE
+echo h >> $FILE
+echo loadbin $1 0x8000000 >> $FILE
+echo g >> $FILE
+echo verifybin $1 0x8000000 >> $FILE
+echo r >> $FILE
+echo q >> $FILE
