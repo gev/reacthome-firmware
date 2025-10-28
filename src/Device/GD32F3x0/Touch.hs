78af550b2ebcc7be944c68057bb5bff68384c670
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-10-28 19:43:44 +0300

    Revert "up"
    This reverts commit f0842e54a7f68869a8af387932174eea2887b948.

--- src/Device/GD32F3x0/Touch.hs
+++ src/Device/GD32F3x0/Touch.hs
@@ -23,13 +23,10 @@ data Touch = Touch
     , timer :: Timer
     , threshold :: IFloat
     , sum :: Value IFloat
-    , min :: Value IFloat
-    , max :: Value IFloat
     , avg :: Value IFloat
     , ix :: Value (Ix Samples)
     , moments :: Values Samples Uint16
     , variance :: Value IFloat
-    , variance_ :: Value IFloat
     , stateTouch :: Value IBool
     , start :: Value IBool
     , debugVal :: Value IFloat
@@ -48,11 +45,8 @@ mkTouch port pin rcuPin threshold = do
     let name = symbol port <> "_" <> symbol pin
 
     sum <- value ("touch_sum" <> name) 0
-    min <- value ("touch_min" <> name) 0xffff_ffff
-    max <- value ("touch_max" <> name) 0
     avg <- value ("touch_avg" <> name) 0
     variance <- value ("touch_variance" <> name) 0
-    variance_ <- value ("touch_variance_" <> name) 0
     ix <- value ("touch_ix" <> name) 0
     moments <- values_ ("touch_moments" <> name)
     stateTouch <- value ("touch_state_touch" <> name) false
@@ -71,12 +65,9 @@ mkTouch port pin rcuPin threshold = do
                 , timer
                 , threshold
                 , sum
-                , min
-                , max
                 , avg
                 , ix
                 , variance
-                , variance_
                 , moments
                 , stateTouch
                 , start
@@ -121,7 +112,6 @@ runMeasurement Touch{..} = do
             store (moments ! ix') moment
 
             let moment' = safeCast moment
-
             sum' <- deref sum
             store sum $ sum' + moment'
 
@@ -130,62 +120,33 @@ runMeasurement Touch{..} = do
 
             when (ix'' ==? 0) do
                 let n = arrayLen moments
-                
                 avg' <- deref avg
-                avg_ <- (/ n) <$> deref sum
-                store sum 0
-
                 var <- local izero
-                var_ <- local izero
-
 
                 arrayMap \kx -> do
                     val <- safeCast <$> deref (moments ! kx)
-
                     var' <- deref var
                     let d = val - avg'
                     store var $ var' + (d * d)
 
-                    var_' <- deref var_
-                    let d_ = val - avg_
-                    store var_ $ var_' + abs d_
-                
                 var' <- (/ avg') . (/ n) <$> deref var
-                var_'' <- (/ n) <$> deref var_
-
-
-                -- store debugVal $ var_' * 10000
-
                 variance' <- deref variance
                 store variance $ average 0.1 variance' var'
                 variance'' <- deref variance
 
-                variance_' <- deref variance_
-                store variance_ $ average 0.01 variance_' var_''
-                variance_'' <- deref variance_
+                store debugVal $ variance'' * 100
 
-                min' <- deref min
-                when (min' >? variance_'') do
-                    store min variance_''
-
-                max' <- deref max
-                when (max' <? variance_'') do
-                    store max variance_''
-
-                min'' <- deref min
-                max'' <- deref max
-
-                store debugVal $ (variance_'' - min'') / (max'' - min'') * 1000
-
-                -- when (variance'' >? 0.6) do
-                --     store stateTouch true
-                --     store debugVal $ variance'' * 100
-                -- when (variance'' <? 0.25) do
-                --     store stateTouch false
-                --     store debugVal $ (-variance'') * 100
-                -- when (variance_'' / max'' <? 0.5) do
-                --     avg' <- deref avg
-                --     store avg $ average 0.001 avg' avg_
+                when (variance'' >? 0.6) do
+                    store stateTouch true
+                    store debugVal $ variance'' * 100
+                when (variance'' <? 0.25) do
+                    store stateTouch false
+                    store debugVal $ (-variance'') * 100
+                when (variance'' <? 0.2) do
+                    avg' <- deref avg
+                    val <- (/ n) <$> deref sum
+                    store avg $ average 0.001 avg' val
+                store sum 0
 
         do
             avg' <- deref avg
