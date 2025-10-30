b09c8965454ecd3a22f06b46aa19c959361227ee
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-10-30 20:25:52 +0300

    up version

--- formula/Formula/Smart/Top/TopG6v9.hs
+++ formula/Formula/Smart/Top/TopG6v9.hs
@@ -18,7 +18,7 @@ smartTopG6v9 =
       Formula
             { name = "smart_top_g6_v9"
             , model = deviceTypeSmartTopG6
-            , version = (4, 9)
+            , version = (4, 10)
             , shouldInit = false
             , mcu = gd32f330k8u6
             , quartzFrequency = 8_000_000
