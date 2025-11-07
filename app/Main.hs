ffd259e42e89b569c2764af54335b7429ff11189
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-11-07 17:45:32 +0300

    add all firmware to main

--- app/Main.hs
+++ app/Main.hs
@@ -25,6 +25,7 @@ import Formula.Smart.Top.TopA4Tv5
 import Formula.Smart.Top.TopA6P
 import Formula.Smart.Top.TopA6T
 import Formula.Smart.Top.TopA6Tv5
+import Formula.Smart.Top.TopClimate
 import Formula.Smart.Top.TopG2
 import Formula.Smart.Top.TopG2v9
 import Formula.Smart.Top.TopG4
@@ -32,51 +33,50 @@ import Formula.Smart.Top.TopG4D
 import Formula.Smart.Top.TopG4Dv15
 import Formula.Smart.Top.TopG4v9
 import Formula.Smart.Top.TopG6
+import Formula.Smart.Top.TopG6v7
 import Formula.Smart.Top.TopG6v9
 import Formula.Soundbox
-import Formula.Smart.Top.TopG6v7
 
 main :: IO ()
 main = do
-  mapM_
-    gcc
-    [ di4
-    , -- , di4la
-      -- , di4rsm
-      -- , relay12
-      -- , dimmerDC12
-      -- , dimmerAC12
-      -- , dimmerAC1
-      -- , doppler1
-      -- , doppler5
-      -- , leakSensor
-      -- , mix6x12
-      -- , mixH
-      -- , smartBottom1
-      -- , smartBottom2
-      -- , smartTopA4P
-      -- , smartTopA4T
-      smartTopA4Tv5
-    , -- , smartTopA4TD
-      smartTopA4TDv5
-    , -- , smartTopA6P
-      -- , smartTopA6T
-      smartTopA6Tv5
-    , -- , smartTopG2
-      -- , smartTopG4
-      -- , smartTopG4D
-     smartTopG4Dv15
-    , -- , smartTopG6
-      smartTopG2v9
-    , smartTopG4v9
-    , smartTopG6v7
-    , smartTopG6v9
-    -- , smartTopG6IT
-    ]
-
--- mapM_
---     gcc
---     [ rsHub4
---     , server
---     , soundbox
---     ]
+    mapM_
+        gcc
+        [ di4
+        , di4la
+        , di4rsm
+        , relay12
+        , dimmerDC12
+        , dimmerAC12
+        , dimmerAC1
+        , doppler1
+        , doppler5
+        , leakSensor
+        , mix6x12
+        , mixH
+        , smartBottom1
+        , smartBottom2
+        , smartTopA4P
+        , smartTopA4T
+        , smartTopA4Tv5
+        , smartTopA4TD
+        , smartTopA4TDv5
+        , smartTopA6P
+        , smartTopA6T
+        , smartTopA6Tv5
+        , smartTopClimate
+        , smartTopG2
+        , smartTopG4
+        , smartTopG4D
+        , smartTopG4Dv15
+        , smartTopG6
+        , smartTopG2v9
+        , smartTopG4v9
+        , smartTopG6v7
+        , smartTopG6v9
+        ]
+    mapM_
+        gcc
+        [ rsHub4
+        , server
+        , soundbox
+        ]
