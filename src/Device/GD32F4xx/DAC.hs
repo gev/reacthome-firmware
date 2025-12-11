b165add99426f74b442d9b751b3dba5d61641df3
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-12-11 12:52:00 +0300

    fix dac

--- src/Device/GD32F4xx/DAC.hs
+++ src/Device/GD32F4xx/DAC.hs
@@ -8,20 +8,20 @@ import Ivory.Language
 import Support.Device.GD32F4xx.DAC
 import Support.Device.GD32F4xx.GPIO (GPIO_PUPD, gpio_pupd_none)
 import Support.Device.GD32F4xx.RCU
+import Ivory.Support
 
 newtype DAC = DAC {dac :: DAC_PERIPH}
 
 mkDAC :: (MonadState Context m) => (GPIO_PUPD -> Port) -> DAC_PERIPH -> m DAC
 mkDAC mkPin dac = do
     let dac' = DAC{dac}
-    addInit "dac" $ initDAC dac'
+    addInit ("dac_" <> symbol dac) $ initDAC dac'
     initPort (mkPin gpio_pupd_none)
     pure dac'
 
 initDAC :: DAC -> Ivory eff ()
 initDAC DAC{..} = do
     enablePeriphClock rcu_dac
-    deinitDAC
     disableTriggerDAC dac
     configWaveModeDAC dac dac_wave_disable
     enableDAC dac
