2c58c29505b19f40abcaec67695740741d64f569
Evgeny Gazdovsky <gazdovsky@gmail.com>
2025-12-24 01:56:31 +0300

    stylish

--- src/Implementation/MixV.hs
+++ src/Implementation/MixV.hs
@@ -5,6 +5,7 @@ import Core.Actions
 import Core.Controller
 import Core.Domain qualified as D
 import Data.Buffer
+import Data.Serialize (unpack)
 import Data.Value
 import Endpoint.AOutputs qualified as EAO
 import Endpoint.Dimmers qualified as EDIM
@@ -17,17 +18,17 @@ import GHC.TypeLits
 import Ivory.Language
 import Ivory.Language.Proxy (NatType, aNat)
 import Ivory.Stdlib
-import Data.Serialize (unpack)
 
 data Mix ni nd no nr = Mix
     { dinputs :: DInputs ni
     , aoutputs :: AO.AOutputs no
     , dimmers :: DIM.Dimmers nd
-    , relays  :: REL.Relays nr
+    , relays :: REL.Relays nr
     , shouldInit :: Value IBool
     }
 
-mix :: forall no ni nd nr m p c t.
+mix ::
+    forall no ni nd nr m p c t.
     (KnownNat no, Monad m, MonadReader (D.Domain p c) m) =>
     m t ->
     (Bool -> t -> m (DInputs ni)) ->
@@ -57,17 +58,19 @@ instance (KnownNat ni, KnownNat nd, KnownNat no, KnownNat nr) => Controller (Mix
             , action ==? actionGetState ==> onGetState mix
             ]
 
-onInit :: forall l ni no nd nr s t. (KnownNat ni, KnownNat nd, KnownNat no, KnownNat nr, KnownNat l) 
-       => Mix ni nd no nr 
-       -> Buffer l Uint8 
-       -> Uint8 
-       -> Ivory (ProcEffects s t) ()
+onInit ::
+    forall l ni no nd nr s t.
+    (KnownNat ni, KnownNat nd, KnownNat no, KnownNat nr, KnownNat l) =>
+    Mix ni nd no nr ->
+    Buffer l Uint8 ->
+    Uint8 ->
+    Ivory (ProcEffects s t) ()
 onInit Mix{..} buff size = do
     let aoutputsN = fromIntegral $ natVal (aNat :: NatType no)
     let dimmersN = fromIntegral $ natVal (aNat :: NatType nd)
     let relaysN = fromIntegral $ natVal (aNat :: NatType nr)
     let sizeBuff = 1 + aoutputsN + (dimmersN * 3) + (relaysN * 5 + relaysN * 6)
-    when (size >=? sizeBuff)  do
+    when (size >=? sizeBuff) do
         offset <- local $ ival 1
 
         let aos = EAO.aoutputs $ AO.getAOutputs aoutputs
@@ -84,21 +87,22 @@ onInit Mix{..} buff size = do
             offset' <- deref offset
             group <- unpack buff offset'
             mode <- unpack buff (offset' + 1)
-            value <- unpack buff (offset' + 2)  :: Ivory eff Uint8
+            value <- unpack buff (offset' + 2) :: Ivory eff Uint8
             EDIM.initialize d group mode (safeCast value / 255) 0
             EDIM.syncDimmerGroup ds d ix
             store offset $ offset' + 3
-        
+
         REL.initGroups relays buff offset
         REL.initRelays relays buff offset
 
         store shouldInit false
 
-        pure()
+        pure ()
 
-onGetState :: (KnownNat ni, KnownNat nd, KnownNat no, KnownNat nr) 
-           => Mix ni nd no nr
-           -> Ivory eff ()
+onGetState ::
+    (KnownNat ni, KnownNat nd, KnownNat no, KnownNat nr) =>
+    Mix ni nd no nr ->
+    Ivory eff ()
 onGetState Mix{..} = do
     forceSyncDInputs dinputs
     initialized <- iNot <$> deref shouldInit
@@ -107,28 +111,30 @@ onGetState Mix{..} = do
         DIM.forceSync dimmers
         REL.forceSyncRelays relays
 
-onDo :: forall l ni no nd nr s t. (KnownNat l, KnownNat ni, KnownNat nd, KnownNat nr, KnownNat no) 
-     => Mix ni nd no nr
-     -> Buffer l Uint8 
-     -> Uint8 
-     -> Ivory (ProcEffects s t) ()
-onDo Mix{..} buff size = do
-    REL.onDo relays buff size
+onDo ::
+    forall l ni no nd nr s t.
+    (KnownNat l, KnownNat ni, KnownNat nd, KnownNat nr, KnownNat no) =>
+    Mix ni nd no nr ->
+    Buffer l Uint8 ->
+    Uint8 ->
+    Ivory (ProcEffects s t) ()
+onDo Mix{..} = do
+    REL.onDo relays
 
-
-onDim :: (KnownNat l, KnownNat nd)
-      => Mix ni nd no nr
-      -> Buffer l Uint8 
-      -> Uint8 
-      -> Ivory (ProcEffects s t) ()
+onDim ::
+    (KnownNat l, KnownNat nd) =>
+    Mix ni nd no nr ->
+    Buffer l Uint8 ->
+    Uint8 ->
+    Ivory (ProcEffects s t) ()
 onDim Mix{..} = do
     DIM.onDim dimmers
-    
 
-onAo :: (KnownNat l, KnownNat no)
-      => Mix ni nd no nr
-      -> Buffer l Uint8 
-      -> Uint8 
-      -> Ivory (ProcEffects s t) ()
-onAo Mix{..} = do 
-    AO.onAo aoutputs 
+onAo ::
+    (KnownNat l, KnownNat no) =>
+    Mix ni nd no nr ->
+    Buffer l Uint8 ->
+    Uint8 ->
+    Ivory (ProcEffects s t) ()
+onAo Mix{..} = do
+    AO.onAo aoutputs
