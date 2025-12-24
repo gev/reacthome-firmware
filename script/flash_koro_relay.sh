917382926ea979d0ea0338892db07ddb1179300b
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2025-12-24 14:59:28 +0300

    up script for flash relay

--- script/flash_koro_relay.sh
+++ script/flash_koro_relay.sh
@@ -1,1 +1,1 @@
-JLinkExe ip tunnel:69651227::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/gd32f330/FlashRelay.jlink
+JLinkExe ip tunnel:69667069::jlink-europe.segger.com -Device GD32F330K8 -If SWD -Speed 1000 jlink/gd32f330/FlashRelay.jlink
