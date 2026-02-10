96bea4ca44c176d1c3e972949dec818471325ac6
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-02-10 17:48:54 +0300

    add nvic_vector_table_set for 330

--- src/Support/Device/GD32F3x0/Misc.hs
+++ src/Support/Device/GD32F3x0/Misc.hs
@@ -1,7 +1,10 @@
 {-# HLINT ignore "Use camelCase" #-}
 
 module Support.Device.GD32F3x0.Misc (
+    NVIC_VECTTAB,
+    nvic_vecttab_flash,
     enableIrqNvic,
+    setVectorTableNvic,
     inclMisc,
 ) where
 
@@ -9,11 +12,29 @@ import Ivory.Language
 import Ivory.Support.Device.GD32F3x0
 import Support.Device.GD32F3x0.IRQ
 
+newtype NVIC_VECTTAB = NVIC_VECTTAB Uint32
+    deriving (IvoryExpr, IvoryInit, IvoryStore, IvoryType, IvoryVar)
+
+nvic_vecttab_flash = NVIC_VECTTAB $ ext "NVIC_VECTTAB_FLASH"
+
+
 enableIrqNvic :: IRQn -> Uint8 -> Uint8 -> Ivory eff ()
 enableIrqNvic = call_ nvic_irq_enable
 
 nvic_irq_enable :: Def ('[IRQn, Uint8, Uint8] :-> ())
 nvic_irq_enable = fun "nvic_irq_enable"
 
+
+setVectorTableNvic :: NVIC_VECTTAB -> Uint32 -> Ivory eff ()
+setVectorTableNvic = call_ nvic_vector_table_set
+
+nvic_vector_table_set :: Def ('[NVIC_VECTTAB, Uint32] :-> ())
+nvic_vector_table_set = fun "nvic_vector_table_set"
+
+
 inclMisc :: ModuleDef
-inclMisc = incl nvic_irq_enable
+inclMisc = do 
+    inclSym nvic_vecttab_flash
+
+    incl nvic_irq_enable
+    incl nvic_vector_table_set
