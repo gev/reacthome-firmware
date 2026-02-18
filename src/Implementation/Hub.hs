f2ee4db6c355a62d5d6751f328366091cb847658
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-02-19 01:11:59 +0300

    add get state for hub

--- src/Implementation/Hub.hs
+++ src/Implementation/Hub.hs
@@ -1,21 +1,30 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Implementation.Hub where
 
 import Control.Monad.Reader (MonadReader, asks)
+import Control.Monad.State
 import Core.Actions
+import Core.Context
 import Core.Controller (Controller, handle)
 import Core.Domain qualified as D
+import Core.Task
+import Core.Transport
 import Data.Buffer
 import Data.Fixed
 import Data.Serialize
+import Data.Type.Bool
+import Data.Type.Equality
 import Data.Value
 import Endpoint.ALED qualified as E
+import Endpoint.DInputs qualified as DI
 import Endpoint.Dimmers as Dim (
     dimmers,
     initialize,
     syncDimmerGroup,
  )
+import Endpoint.Dimmers qualified as EDim
 import Feature.ALED
-import Feature.DInputs (DInputs, forceSyncDInputs)
+import Feature.DInputs (DInputs, forceSyncDInputs, getDInputs)
 import Feature.DS18B20 (DS18B20)
 import Feature.Dimmers (
     Dimmers (getDimmers),
@@ -35,7 +44,12 @@ import Feature.RS485.RBUS (
 import Feature.RS485.RBUS.Data (RBUS (..))
 import GHC.TypeNats
 import Ivory.Language
+import Ivory.Language.Proxy
 import Ivory.Stdlib
+import Support.Cast (castFloatToUint8)
+
+type ToSizeInBytes n = Div n 8 + If (Mod n 8 == 0) 0 1
+type SizeSyncStateBuff ni nd = 1 + ToSizeInBytes ni + nd
 
 data Hub ni nd nr = Hub
     { rbus :: List nr RBUS
@@ -44,10 +58,24 @@ data Hub ni nd nr = Hub
     , indicator :: Indicator 20
     , aled :: ALED 10 100 2400
     , shouldInit :: Value IBool
+    , syncStateBuff :: Buffer (SizeSyncStateBuff ni nd) Uint8
+    , transmit ::
+        forall n.
+        (KnownNat n) =>
+        Buffer n Uint8 ->
+        forall s t.
+        Ivory (ProcEffects s t) ()
     }
 
 hub ::
-    (MonadReader (D.Domain p c) m) =>
+    ( MonadState Context m
+    , MonadReader (D.Domain p c) m
+    , KnownNat ni
+    , KnownNat nd
+    , KnownNat (SizeSyncStateBuff ni nd)
+    , KnownNat (ToSizeInBytes ni)
+    , Transport t
+    ) =>
     m t ->
     (t -> m (List nr RBUS)) ->
     (t -> m (Dimmers nd)) ->
@@ -64,10 +92,25 @@ hub transport' rbus' dimmers' dinputs' ds18b20' indicator' aled' = do
     indicator <- indicator' transport
     aled <- aled' transport
     shouldInit <- asks D.shouldInit
+    syncStateBuff <- buffer "hub_sync_channels"
     ds18b20' transport
-    pure Hub{rbus, dimmers, dinputs, indicator, shouldInit, aled}
 
-instance (KnownNat ni, KnownNat nd, KnownNat nr) => Controller (Hub ni nd nr) where
+    let hub =
+            Hub
+                { rbus
+                , dimmers
+                , dinputs
+                , indicator
+                , shouldInit
+                , aled
+                , syncStateBuff
+                , transmit = transmitBuffer transport
+                }
+
+    addTask $ delay 5_000 "sync_channels" $ syncChannels hub
+
+    pure hub
+instance (KnownNat ni, KnownNat nd, KnownNat nr, KnownNat (SizeSyncStateBuff ni nd), KnownNat (ToSizeInBytes ni)) => Controller (Hub ni nd nr) where
     handle s@Hub{..} buff size = do
         action <- deref $ buff ! 0
         cond_
@@ -78,7 +121,7 @@ instance (KnownNat ni, KnownNat nd, KnownNat nr) => Controller (Hub ni nd nr) wh
             , action ==? actionRs485Transmit ==> transmitRB485 rbus buff size
             , action ==? actionFindMe ==> onFindMe indicator buff size
             , action ==? actionInitialize ==> onInit s buff size
-            , action ==? actionGetState ==> onGetState s
+            , action ==? actionGetState ==> syncChannels s
             , action ==? actionALedOn ==> onALedOn aled buff size
             , action ==? actionALedOff ==> onALedOff aled buff size
             , action ==? actionALedColorAnimationPlay ==> onALedColorAnimationPlay aled buff size
@@ -129,6 +172,46 @@ onInit Hub{..} buff size = do
             store offset $ offset' + 1
 
         store shouldInit false
+
+syncChannels ::
+    forall ni nd nr s t.
+    ( KnownNat ni
+    , KnownNat nd
+    , KnownNat (SizeSyncStateBuff ni nd)
+    , KnownNat (ToSizeInBytes ni)
+    ) =>
+    Hub ni nd nr ->
+    Ivory (ProcEffects s t) ()
+syncChannels Hub{..} = do
+    shouldInit' <- deref shouldInit
+    when (iNot shouldInit') do
+        arrayMap \ix -> store (syncStateBuff ! ix) 0
+        pack syncStateBuff 0 actionGetState
+
+        offsetByte <- local $ ival 1
+        offsetByte' <- deref offsetByte
+
+        arrayMap \ix -> do
+            let di' = DI.dinputs (getDInputs dinputs) ! ix
+            diState <- deref $ di' ~> DI.state
+            when diState do
+                let ixByte = toIx $ offsetByte' + (fromIx ix `iDiv` 8)
+                let numBit = castDefault $ fromIx ix .% 8
+                let bitMask = 1 `iShiftL` numBit
+                buffByte <- deref $ syncStateBuff ! ixByte
+                pack syncStateBuff ixByte (buffByte .| bitMask)
+
+        let numByteDI = fromIntegral $ natVal (aNat :: NatType (ToSizeInBytes ni))
+        store offsetByte $ offsetByte' + numByteDI
+
+        offsetByte'' <- deref offsetByte
+        arrayMap \ix -> do
+            let dimmer = EDim.dimmers (getDimmers dimmers) ! ix
+            dimmerBrightness <- castFloatToUint8 . (* 255) =<< deref (dimmer ~> EDim.brightness)
+            let ixBuff = toIx . (+ offsetByte'') $ fromIx ix
+            pack syncStateBuff ixBuff dimmerBrightness
+
+        transmit syncStateBuff
 
 onGetState Hub{..} = do
     forceSyncDInputs dinputs
