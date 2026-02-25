dd17e741ee980343780c921be0b246c0af2c81a2
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-02-25 17:03:58 +0300

    add get state for di devices

--- src/Implementation/DILA.hs
+++ src/Implementation/DILA.hs
@@ -1,21 +1,39 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Implementation.DILA where
 
+import Control.Monad.State
 import Core.Actions
+import Core.Context
 import Core.Controller
+import Core.Task
+import Data.Buffer
+import Data.Serialize
+import Data.Type.Bool
+import Data.Type.Equality
+import Endpoint.DInputs qualified as D
 import Feature.ALED
-import Feature.DInputs (DInputs, forceSyncDInputs)
+import Feature.DInputs (DInputs (transmit), forceSyncDInputs, getDInputs)
 import Feature.DS18B20
 import GHC.TypeNats
 import Ivory.Language
 import Ivory.Stdlib
 
+type ToSizeInBytes n = Div n 8 + If (Mod n 8 == 0) 0 1
+type SizeSyncStateBuff n = 1 + ToSizeInBytes n
+
 data DILA n = DILA
     { dinputs :: DInputs n
     , aled :: ALED 10 100 2040
+    , syncStateBuff :: Buffer (SizeSyncStateBuff n) Uint8
     }
 
 dila ::
-    (Monad m) =>
+    ( Monad m
+    , KnownNat n
+    , MonadState Context m
+    , KnownNat (SizeSyncStateBuff n)
+    ) =>
     m t ->
     (Bool -> t -> m (DInputs n)) ->
     (t -> m DS18B20) ->
@@ -26,17 +44,23 @@ dila transport' dinputs' ds18b20 aled' = do
     ds18b20 transport
     dinputs <- dinputs' True transport
     aled <- aled' transport
-    pure DILA{dinputs, aled}
+    syncStateBuff <- buffer "sync_channels"
+
+    let dila = DILA{dinputs, aled, syncStateBuff}
+
+    addTask $ delay 5_000 "sync_channels" $ syncChannels dila
+
+    pure dila
 
 onGetState DILA{..} _ _ = do
     forceSyncDInputs dinputs
     forceSyncAled aled
 
-instance (KnownNat n) => Controller (DILA n) where
+instance (KnownNat n, KnownNat (SizeSyncStateBuff n)) => Controller (DILA n) where
     handle d@DILA{..} buff size = do
         action <- deref $ buff ! 0
         cond_
-            [ action ==? actionGetState ==> onGetState d buff size
+            [ action ==? actionGetState ==> syncChannels d
             , action ==? actionInitialize ==> onInitialize aled buff size
             , action ==? actionALedOn ==> onALedOn aled buff size
             , action ==? actionALedOff ==> onALedOff aled buff size
@@ -48,3 +72,25 @@ instance (KnownNat n) => Controller (DILA n) where
             , action ==? actionALedBrightness ==> onALedBrightness aled buff size
             , action ==? actionALedConfigGroup ==> onALedConfigGroup aled buff size
             ]
+
+syncChannels ::
+    forall n s t.
+    ( KnownNat n
+    , KnownNat (SizeSyncStateBuff n)
+    ) =>
+    DILA n ->
+    Ivory (ProcEffects s t) ()
+syncChannels DILA{..} = do
+    arrayMap \ix -> store (syncStateBuff ! ix) 0
+    pack syncStateBuff 0 actionGetState
+    let offset = 1
+    arrayMap \ix -> do
+        let relay' = D.dinputs (getDInputs dinputs) ! ix
+        diState <- deref $ relay' ~> D.state
+        when diState do
+            let ixByte = toIx $ offset + (fromIx ix `iDiv` 8)
+            let numBit = castDefault $ fromIx ix .% 8
+            byteFromBuff <- deref $ syncStateBuff ! ixByte
+            let newByte = byteFromBuff .| (1 `iShiftL` numBit)
+            pack syncStateBuff ixByte newByte
+    transmit dinputs syncStateBuff
