46de8ac31047b615eef92033405dadd00ff94d75
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-03-30 13:45:39 +0300

    replace formula with dfu in bottom climate

--- formula/Formula/Smart/Bottom/BottomClimate.hs
+++ formula/Formula/Smart/Bottom/BottomClimate.hs
@@ -2,22 +2,22 @@
 
 module Formula.Smart.Bottom.BottomClimate where
 
-import Core.Formula
+import Core.Formula.DFU
 import Core.Models
 import Data.Fixed
 import Device.GD32F3x0
 import Feature.ALED (aled)
 import Feature.DInputs
 import Feature.DS18B20
+import Feature.Sht21
 import Implementation.Smart.BottomClimate (bottomClimate)
 import Interface.RS485
 import Ivory.Language
 import Transport.RS485.RBUS
-import Feature.Sht21
 
-smartBottomClimate :: Formula GD32F3x0
+smartBottomClimate :: DFU GD32F3x0
 smartBottomClimate =
-    Formula
+    DFU
         { name = "smart_bottom_climate"
         , model = deviceTypeSmartBottomClimate
         , version = (4, 12)
@@ -25,6 +25,7 @@ smartBottomClimate =
         , mcu = gd32f330k8u6
         , quartzFrequency = 8_000_000
         , systemFrequency = 84_000_000
+        , transport = rbus $ rs485 uart_1 out_pa_4
         , implementation =
             bottomClimate
                 ( dinputs $
@@ -37,5 +38,4 @@ smartBottomClimate =
                 (ds18b20 ow_0 od_pa_15)
                 (sht21 i2c_0)
                 (aled npx_pwm_3 etc)
-                (rbus $ rs485 uart_1 out_pa_4)
         }
