414ad2b67616d9a5a4c5953b623d3ce3cc349336
DmitryTyazhlov <dmitrytyazhlov13@gmail.com>
2026-04-09 14:40:24 +0300

    add comment for actions FE

--- src/Core/Actions.hs
+++ src/Core/Actions.hs
@@ -65,4 +65,5 @@ actionFindMe = 0xFA :: Uint8
 actionBootload = 0xFB :: Uint8
 actionMacAddress = 0xFC :: Uint8
 actionIpAddress = 0xFD :: Uint8
+-- 0xFE is used for PLC device
 actionError = 0xFF :: Uint8
